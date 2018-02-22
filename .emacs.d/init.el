c23313ec89f3c02855b60390fcf1c25c41a0fb56
Edmund <eman0088@gmail.com>
2018-02-22 11:17:28 -0600

    Rewrite emacs
    Switching to the dark side.
    As I ironically write this commit in vim.

---
+++ .emacs.d/init.el
@@ -0,0 +1,22 @@
+(setq inhibit-startup-message t)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/configuration.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (org-bullets which-key try use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
