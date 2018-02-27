37c84f49d3f96137d3aaaf382bd0c84eba2cb058
Edmund Miller <eman0088@gmail.com>
2018-02-27 11:13:22 -0600

    Update Emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,5 @@
+;;; Edmund Miller init.el
+
 (setq inhibit-startup-message t)
 
 (require 'package)
@@ -13,10 +15,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (org-bullets which-key try use-package))))
+ '(package-selected-packages
+   (quote
+    (nlinum- org avy evil-magit org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+;;; init.el ends here
