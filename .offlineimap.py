76432c4fe0c835d7fb03af064a22f20c99b4ea7a
Edmund Miller <eman0088@gmail.com>
2018-03-15 18:39:17 -0500

    Update Email on Emacs

---
+++ .offlineimap.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from subprocess import check_output
+from re import sub
+
+def get_pass(entry):
+    return check_output("/usr/bin/pass " + entry, shell=True).splitlines()[0] 
+
+    # data = check_output("/usr/bin/pass gmail.com/" + account, shell=True).splitlines()
+    # password = data[0]
+    # tmp = [x for x in data if x.startswith('address:')]
+    # user = ""
+    # if len(tmp) > 0:
+    #     user = tmp[0].split(":", 1)[1]
+
+    # return {"password": password, "user": user}
+
+def folder_filter(name):
+    return not (name in ['INBOX',
+                         '[Gmail]/Spam',
+                         '[Gmail]/Important',
+                         '[Gmail]/Starred'] or
+                name.startswith('[Airmail]'))
+
+def nametrans(name):
+    return sub('^(Starred|Sent Mail|Drafts|Trash|All Mail|Spam)$', '[Gmail]/\\1', name)
+
+def nametrans_reverse(name):
+    return sub('^(\[Gmail\]/)', '', name)
+
+# eval = get_pass("gmail.com/user")
+# eval = get_pass("gmail.com/password")
