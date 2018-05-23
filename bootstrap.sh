18e226a5bc6787054608f14a523509725bd62ca8
Edmund Miller <eman0088@gmail.com>
2018-05-22 20:42:03 -0500

    Update bootstrap
    Moving to Henrik's over-engineered method

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,34 +1,41 @@
 #!/usr/bin/env bash
-# Zsh
-sudo apt install zsh curl
-chsh -s $(which zsh)
-echo $SHELL
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# Clone dots files
-git clone --bare https://github.com/Emiller88/dotfiles.git $HOME/.cfg
-# Emacs
-git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
-bash emacs27.sh
-git submodule update --init
-cd ~/.emacs.d/
-make install compile all
-# Set up link to keep home dir clean
-function config { /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@ }
-mkdir -p .config-backup
-config checkout
-if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
-config checkout
-config config status.showUntrackedFiles no
 
-# Install Vim plug
-# https://github.com/junegunn/vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# Termite
-# Uncomment if Termite is desired
-# bash termite-install.sh
+# dotfiles bootstrapper
+#
+# Installs zsh & git, then clones this repo to ~/.dotfiles. This script is
+# intended to be run remotely, via curl:
+#
+# bash <(curl -s https://raw.githubusercontent.com/hlissner/dotfiles/master/bootstrap.sh)
+
+export DOTFILES=~/.dotfiles
+set -e
+
+_msg() { printf "\r\033[2K\033[0;32m[ .. ] %s\033[0m\n" "$*"; }
+_uncallable() { ! command -v "$1" >/dev/null; }
+
+if _uncallable zsh || _uncallable git; then
+  # NOTE Macos has both already
+  _msg "Installing git and/or zsh"
+  if [[ -f /etc/debian_version ]]; then
+    sudo apt-get update && sudo apt-get install -y git zsh
+  elif [[ -f /etc/arch-release ]]; then
+    sudo pacman --needed --noconfirm -S git zsh
+  fi
+fi
+
+if [[ ! -d ~/.dotfiles ]]; then
+  _msg "Deploying dotfiles repository..."
+  if [[ $USER == emiller ]]; then
+    dfrepo=git@github.com:emiller88/dotfiles.git
+  else
+    dfrepo=https://github.com/emiller88/dotfiles
+  fi
+  git clone --recursive "$dfrepo" "$DOTFILES"
+fi
+
+#
+_msg
+_msg "And done!"
+_msg
+_msg "Use $DOTFILES/deploy to install your dotfiles, I suggest you begin"
+_msg "with 'base/arch' or 'base/debian'."
