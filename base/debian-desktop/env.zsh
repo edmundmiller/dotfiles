80b41d1956443ed2a417b6447209576b1f502a52
Edmund Miller <eman0088@gmail.com>
2018-05-25 23:10:46 -0500

    Add Debian
    First try

---
+++ base/debian-desktop/env.zsh
@@ -0,0 +1,13 @@
+export BROWSER=firefox
+export GNUPGHOME="$XDG_CONFIG_HOME/gpg"
+
+_source "$XDG_RUNTIME_DIR/ssh-agent-env" >/dev/null
+if [[ -z $SSH_AGENT_PID ]] || ! ps $SSH_AGENT_PID >/dev/null; then
+  ssh-agent -s >"$XDG_RUNTIME_DIR/ssh-agent-env"
+  _source "$XDG_RUNTIME_DIR/ssh-agent-env" >/dev/null
+fi
+
+_is_running gpg-agent || {
+  gpg-agent --pinentry-program /usr/bin/pinentry-gtk-2 --daemon >"$XDG_RUNTIME_DIR/gpg-agent-env"
+}
+_source "$XDG_RUNTIME_DIR/gpg-agent-env" >/dev/null
---
+++ base/debian-desktop/scripts/install-steam.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo add-apt-repository multiverse
+sudo apt-get install steam
