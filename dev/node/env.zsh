40352582eec8a66d883a44fcbb1004562600f1e3
Edmund Miller <edmund.a.miller@gmail.com>
2018-12-18 16:44:50 -0600

    Add yarn global to path

--- dev/node/env.zsh
+++ dev/node/env.zsh
@@ -4,3 +4,6 @@ export NODE_REPL_HISTORY=$XDG_CACHE_HOME/node/repl_history
 export NODENV_ROOT=$XDG_DATA_HOME/nodenv
 path=( $NODENV_ROOT/bin $path )
 _cache nodenv init - --no-rehash
+
+# yarn
+export PATH="$(yarn global bin):$PATH"
