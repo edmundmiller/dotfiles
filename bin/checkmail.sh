a3a969e95fd516539ba22ee7c504e08519fae7eb
Edmund Miller <edmund.a.miller@gmail.com>
2019-01-23 12:43:26 -0600

    Move email to gmailieer

--- bin/checkmail.sh
+++ bin/checkmail.sh
@@ -3,9 +3,22 @@
 STATE=$(nmcli networking connectivity)
 
 if [ $STATE = 'full' ]; then
+  echo "Sending mail"
 	~/.dotfiles/bin/msmtp-runqueue.sh
-	mbsync -Vanf
+  echo "Syncing gmail"
+  cd /home/emiller/.mail/gmail/
+  gmi sync
+  echo "Syncing eman"
+  cd /home/emiller/.mail/eman/
+  gmi sync
+  echo "Syncing olypsis"
+  cd /home/emiller/.mail/olypsis/
+  gmi sync
+  echo "Checking utd"
+	mbsync -V utd
+  echo "Running notmuch new"
 	notmuch new
+	notmuch tag +inbox -- to:eam150030@utdallas.edu AND not tag:inbox
 	exit 0
 fi
 echo "No internet connection."
