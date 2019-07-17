9ca1e4a10e81b7e15b16f74c0d1e99d8fb713548
Edmund Miller <edmund.a.miller@protonmail.com>
2019-07-17 18:31:26 -0500

    refactor: move a bunch of files around

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,28 +1,38 @@
-# modules/shell.nix
 { config, pkgs, libs, ... }:
 
-let zgen = builtins.fetchTarball "https://github.com/tarjoilija/zgen/archive/master.tar.gz";
-in {
-    home.sessionVariables = {
+let zgen = builtins.fetchGit "https://github.com/tarjoilija/zgen";
+in
+{
+  environment = {
+    variables = {
       ZDOTDIR = "$XDG_CONFIG_HOME/zsh";
       ZSH_CACHE = "$XDG_CACHE_HOME/zsh";
       ZGEN_DIR  = "$XDG_CACHE_HOME/zgen";
       ZGEN_SOURCE = "${zgen}/zgen.zsh";
     };
 
-    home.packages = with pkgs; [
+    systemPackages = with pkgs; [
       zsh
       nix-zsh-completions
       fasd
       exa
       fd
       tmux
+      htop
+      tree
     ];
+  };
 
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     enableGlobalCompInit = false; # I'll do it myself
     promptInit = "";
+  };
+
+  home-manager.users.emiller.xdg.configFile = {
+    # link recursively so other modules can link files in this folder,
+    # particularly in zsh/rc.d/*.zsh
+    "zsh" = { source = <config/zsh>; recursive = true; };
   };
 }
