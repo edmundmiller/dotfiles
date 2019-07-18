a0a928a70fbba60130ca9e194b142cd985269389
Edmund Miller <edmund.a.miller@protonmail.com>
2019-07-18 10:32:02 -0500

    feat: fix emacs aliases

--- modules/editors/emacs.nix
+++ modules/editors/emacs.nix
@@ -9,6 +9,8 @@
     sqlite                          # :tools (lookup +docsets)
     texlive.combined.scheme-medium  # :lang org -- for latex previews
     ccls                            # :lang (cc +lsp)
+    imagemagick
+    pandoc
   ];
 
   fonts.fonts = [ pkgs.emacs-all-the-icons-fonts ];
@@ -18,4 +20,5 @@
     "zsh/rc.d/env.emacs.zsh".source = <config/emacs/env.zsh>;
   };
   # TODO override https://nixos.org/nixos/manual/index.html#sec-customising-packages
+  # TODO add Doom automagically
 }
