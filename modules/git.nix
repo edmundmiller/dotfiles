b13612c107c0a793e8697e1b062fd9018e3a8bf7
Edmund Miller <edmund.a.miller@protonmail.com>
2019-07-18 10:32:25 -0500

    feat: add git packages

--- modules/git.nix
+++ modules/git.nix
@@ -5,6 +5,10 @@ let
   protonmail = "edmund.a.miller@protonmail.com";
 in {
   environment.systemPackages = with pkgs; [
+    gitAndTools.diff-so-fancy
+    gitAndTools.git-hub
+    gitAndTools.gitflow
+    gitAndTools.hub
   ];
   home-manager.users.emiller = {
     programs = {
@@ -15,23 +19,6 @@ in {
         userEmail = "${protonmail}";
         signing.key = "BC10AA9D";
         signing.signByDefault = true;
-        aliases = {
-          amend = "commit --amend";
-          exec = "!exec ";
-          lg =
-          "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --";
-          ls = "ls-files";
-          orphan = "checkout --orphan";
-          unadd = "reset HEAD";
-          undo-commit = ''
-            reset --soft "HEAD^"
-                      mr = !sh -c 'git fetch $1 merge-requests/$2/head:mr-$1-$2 && git checkout mr-$1-$2' -'';
-          # data analysis
-          ranked-authors = "!git authors | sort | uniq -c | sort -n";
-          emails = ''!git log --format="%aE" | sort -u'';
-          email-domains =
-          ''!git log --format="%aE" | awk -F'@' '{print $2}' | sort -u'';
-        };
         extraConfig = ''
           [github]
             user = emiller88
@@ -48,8 +35,8 @@ in {
       };
     };
 
-    xdg.configFile = {
-      "zsh/rc.d/aliases.git.zsh".source = <config/git/aliases.zsh>;
-    };
+    # xdg.configFile = {
+    #   "zsh/rc.d/aliases.git.zsh".source = <config/git/aliases.zsh>;
+    # };
   };
 }
