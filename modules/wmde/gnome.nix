b061527a6b282a46b8f87a7d83df8bc0b6bd3db4
Edmund Miller <edmund.a.miller@protonmail.com>
2019-07-27 16:53:15 -0500

    feat(gnome): add gtk theme

--- modules/wmde/gnome.nix
+++ modules/wmde/gnome.nix
@@ -19,9 +19,23 @@
         gdm.enable = true;
         gdm.wayland = false;
       };
-      desktopManager = {
-        gnome3 = { enable = true; };
+      desktopManager.gnome3.enable = true;
+    };
+  };
+
+  home-manager.users.emiller = {
+    gtk = {
+      enable = true;
+      theme = {
+        package = pkgs.sierra-gtk-theme;
+        name = "Sierra-compact-dark";
       };
+      iconTheme = {
+        package = pkgs.paper-icon-theme;
+        name = "Paper-Mono-Dark";
+      };
+      # Give Termite some internal spacing.
+      gtk3.extraCss = ".termite {padding: 20px;}";
     };
   };
 }
