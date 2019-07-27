bdf8c5b203921fa34ef575437d52ed47609b1727
Edmund Miller <edmund.a.miller@protonmail.com>
2019-07-27 17:51:08 -0500

    feat(dev): add clojure

---
+++ modules/dev/clojure.nix
@@ -0,0 +1,17 @@
+{ config, lib, pkgs, ... }:
+
+{
+  nixpkgs.config = {
+  };
+
+  environment.systemPackages = with pkgs; [
+    clojure
+    joker
+    leiningen
+  ];
+
+  home-manager.users.emiller.xdg.configFile = {
+    "zsh/rc.d/aliases.clojure.zsh".source = <config/clojure/aliases.zsh>;
+    "zsh/rc.d/env.clojure.zsh".source = <config/clojure/env.zsh>;
+  };
+}
