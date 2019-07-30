54689c88662015a4683ba44dc5b9b7bdff3c5987
Edmund Miller <edmund.a.miller@protonmail.com>
2019-07-29 21:22:34 -0500

    feat(bspwm): add redshift screenlock and dunst

--- modules/wmde/bspwm.nix
+++ modules/wmde/bspwm.nix
@@ -1,6 +1,8 @@
 { config, lib, pkgs, ... }:
 
 {
+  imports = [ ./polybar.nix ./dunst.nix ];
+
   services.xserver.windowManager.bspwm = { enable = true; };
 
   home-manager.users.emiller = {
@@ -11,6 +13,7 @@
         rofi.ssh-client: mosh
         rofi.ssh-command: {terminal} -e "{ssh-client} {host}"
         rofi.combi-modi: window,drun,ssh
+        rofi.font: Iosevka 18
       '';
       terminal = "termite";
       theme = "Arc-Dark";
@@ -159,6 +162,18 @@
     #   };
     #   script = "polybar bottom &";
     # };
+    services.screen-locker = {
+      enable = true;
+    };
+    services.redshift = {
+      enable = true;
+      latitude = "32.78306";
+      longitude = "-96.80667";
+    };
+    services.gnome-keyring = {
+      enable = true;
+      components = [ "secrets" ];
+    };
 
     xdg.configFile = {
       "bspwm/bspwmrc".source = <config/bspwm/bspwmrc>;
