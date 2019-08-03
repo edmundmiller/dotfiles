dcba78dd4845bccbcf6d6958c1f72e6a46eff177
Edmund Miller <edmund.a.miller@protonmail.com>
2019-08-03 10:00:50 -0500

    fix(gpg): add defaultCacheTtl to agent

--- modules/shell/gpg.nix
+++ modules/shell/gpg.nix
@@ -1,5 +1,16 @@
 { config, lib, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ pinentry_emacs pinentry];
+  environment.systemPackages = with pkgs; [ pinentry_emacs pinentry ];
+
+  home-manager.users.emiller = {
+    programs.gpg = {
+      enable = true;
+    };
+    services.gpg-agent = {
+      enable = true;
+      defaultCacheTtl = 28800;
+      maxCacheTtl = 28800;
+    };
+  };
 }
