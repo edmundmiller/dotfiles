5b5df021ff851ae512475415c8d13a0ecdbfbec3
Edmund Miller <edmund.a.miller@protonmail.com>
2019-08-03 17:29:21 -0500

    feat(dev): update root file

--- modules/dev.nix
+++ modules/dev.nix
@@ -3,12 +3,19 @@
 {
   imports = [
     ./editors/emacs.nix
+    ./shell/direnv.nix
     ./shell/git.nix
     ./shell/mail.nix
     ./shell/ncmpcpp+mpd.nix
     # ./shell/tmux.nix
     ./shell/gpg.nix
     ./shell/zsh.nix
+    ./shell/termite.nix
+
+    ./dev/node.nix
+    ./dev/python.nix
+    ./dev/clojure.nix
+    ./dev/zsh.nix
 
     ./misc/docker.nix
     ./misc/virtualbox.nix
