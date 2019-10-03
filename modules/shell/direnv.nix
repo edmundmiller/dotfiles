6d242c5fa2c05c10cca707d649656b72636aecd8
Edmund Miller <edmund.a.miller@protonmail.com>
2019-10-02 22:41:07 -0500

    feat: add lorri

--- modules/shell/direnv.nix
+++ modules/shell/direnv.nix
@@ -1,7 +1,20 @@
 { config, lib, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ direnv ];
+  nixpkgs.overlays = [
+    (self: super: {
+      lorri = let
+        src = (super.fetchFromGitHub {
+          owner = "target";
+          repo = "lorri";
+          rev = "38eae3d487526ece9d1b8c9bb0d27fb45cf60816";
+          sha256 = "11k9lxg9cv6dlxj4haydvw4dhcfyszwvx7jx9p24jadqsy9jmbj4";
+        });
+      in import src { inherit src; };
+    })
+  ];
+
+  environment.systemPackages = with pkgs; [ direnv lorri ];
 
   programs = {
     bash.interactiveShellInit = ''
