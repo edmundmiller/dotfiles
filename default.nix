cd7917cab6ff79618cbfba7002a59fcc7dc00f7e
Edmund Miller <edmund.a.miller@protonmail.com>
2019-10-04 22:59:47 -0500

    feat: add nix commands

--- default.nix
+++ default.nix
@@ -36,6 +36,7 @@
       unzip
       bc
       (ripgrep.override { withPCRE2 = true; })
+      bashmount
     ];
     variables = {
       XDG_CONFIG_HOME = "$HOME/.config";
@@ -49,6 +50,13 @@
       q = "exit";
       clr = "clear";
       sudo = "sudo ";
+      nix-env = "NIXPKGS_ALLOW_UNFREE=1 nix-env";
+      ne = "nix-env";
+      nu =
+      "sudo nix-channel --update && sudo nixos-rebuild -I config=$HOME/.dotfiles/config switch";
+      nre = "sudo nixos-rebuild -I config=$HOME/.dotfiles/config";
+      ngc = "nix-collect-garbage -d && sudo nix-collect-garbage -d";
+      ns = "nix-shell";
     };
   };
 
