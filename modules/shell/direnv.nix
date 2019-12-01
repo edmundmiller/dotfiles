24bf381d3406389c269c97e80bab42b67bbf178c
Edmund Miller <edmund.a.miller@protonmail.com>
2019-12-01 00:53:36 -0600

    fix(direnv): remove interactive shell starts

--- modules/shell/direnv.nix
+++ modules/shell/direnv.nix
@@ -16,18 +16,6 @@
 
   environment.systemPackages = with pkgs; [ direnv lorri ];
 
-  programs = {
-    bash.interactiveShellInit = ''
-      eval "$(${pkgs.direnv}/bin/direnv hook bash)"
-    '';
-    zsh.interactiveShellInit = ''
-      eval "$(${pkgs.direnv}/bin/direnv hook zsh)"
-    '';
-    fish.interactiveShellInit = ''
-      eval (${pkgs.direnv}/bin/direnv hook fish)
-    '';
-  };
-
   home-manager.users.emiller = {
     xdg.configFile = {
       "zsh/rc.d/aliases.direnv.zsh".source = <config/direnv/aliases.zsh>;
