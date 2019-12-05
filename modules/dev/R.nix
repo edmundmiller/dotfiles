e8232246940837d07a35fa9321aaa5259d5182a6
Edmund Miller <edmund.a.miller@protonmail.com>
2019-12-05 16:33:37 -0600

    feat(R): fix the R install with packages

--- modules/dev/R.nix
+++ modules/dev/R.nix
@@ -1,7 +1,10 @@
 { config, lib, pkgs, ... }:
-
-{
-  environment.systemPackages = with pkgs; [ R ];
+with pkgs;
+let
+  R-with-my-packages =
+  rWrapper.override { packages = with rPackages; [ languageR lintr styler ]; };
+in {
+  environment.systemPackages = with pkgs; [ R-with-my-packages ];
 
   home-manager.users.emiller.xdg.configFile = {
     # "zsh/rc.d/aliases.R.zsh".source = <config/R/aliases.zsh>;
