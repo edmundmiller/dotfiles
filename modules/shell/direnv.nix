834f3eb0bd2505c151d2d4abb768b690a4fe5a71
Edmund Miller <edmund.a.miller@protonmail.com>
2019-12-07 14:09:06 -0600

    feat(direnv): install lorri from unstable

--- modules/shell/direnv.nix
+++ modules/shell/direnv.nix
@@ -1,20 +1,10 @@
 { config, lib, pkgs, ... }:
 
 {
-  nixpkgs.overlays = [
-    (self: super: {
-      lorri = let
-        src = (super.fetchFromGitHub {
-          owner = "target";
-          repo = "lorri";
-          rev = "38eae3d487526ece9d1b8c9bb0d27fb45cf60816";
-          sha256 = "11k9lxg9cv6dlxj4haydvw4dhcfyszwvx7jx9p24jadqsy9jmbj4";
-        });
-      in import src { inherit src; };
-    })
+  environment.systemPackages = with pkgs; [
+    direnv
+    (import <nixpkgs-unstable> { }).lorri
   ];
-
-  environment.systemPackages = with pkgs; [ direnv lorri ];
 
   home-manager.users.emiller = {
     xdg.configFile = {
