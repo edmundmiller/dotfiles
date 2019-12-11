52194e24a037120ee4968bdfc9a5f7756c7b5ac6
Edmund Miller <edmund.a.miller@protonmail.com>
2019-12-11 09:56:21 -0600

    fix(pass): move password-store

--- modules/shell/pass.nix
+++ modules/shell/pass.nix
@@ -7,5 +7,8 @@
       (exts: [ exts.pass-otp exts.pass-genphrase exts.pass-import ]))
       (lib.mkIf (config.services.xserver.enable) rofi-pass)
     ];
+
+    # HACK Have to symlink to ~/.password-store for mail
+    variables.PASSWORD_STORE_DIR = "$HOME/.secrets/password-store";
   };
 }
