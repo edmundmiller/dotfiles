cb5d43dbc0388cf802296207db61b6443e29a1ff
Edmund Miller <edmund.a.miller@protonmail.com>
2019-12-16 17:23:43 -0600

    fix(borg): remove default enable
    team zfs

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -2,7 +2,7 @@
 
 {
   imports =
-  [ ./borg.nix ./keybase.nix ./pia.nix ./transmission.nix ./syncthing.nix ];
+  [ ./keybase.nix ./pia.nix ./transmission.nix ./syncthing.nix ];
 
   # services.autorandr = {
   #   enable = true;
