bb5160c21a2de1832984c4d3b3a24e6932e18061
Edmund Miller <edmund.a.miller@protonmail.com>
2019-12-23 23:19:00 -0600

    feat(node): init configuration

---
+++ configuration.node.nix
@@ -0,0 +1,34 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ./.
+
+    ./modules/base.nix
+
+    ./modules/services/ssh.nix
+    ./modules/services/syncthing.nix
+  ];
+
+  networking.hostName = "node";
+  # networking.wireless.enable = true;
+  networking.networkmanager.enable = true;
+
+  system.autoUpgrade = {
+    enable = true;
+    channel = "https://nixos.org/channels/nixos-19.09";
+  };
+
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  nix.gc = {
+    automatic = true;
+    options = "--delete-older-than 15d";
+  };
+
+  users.users.emiller.extraGroups = [ "networkmanager" ];
+}
