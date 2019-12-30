b9bfb5578fae0e0bd9ed9b20da7140306245d558
Edmund Miller <edmund.a.miller@protonmail.com>
2019-12-30 12:38:33 -0600

    fix(omen): missing luks

--- configuration.omen.nix
+++ configuration.omen.nix
@@ -60,40 +60,42 @@
     # Required to open the EFI partition and Yubikey
     kernelModules = [ "vfat" "nls_cp437" "nls_iso8859-1" "usbhid" ];
 
-    # luks = {
-    #   # Update if necessary
-    #   cryptoModules = [ "aes" "xts" "sha512" ];
+    luks = {
+      #   # Update if necessary
+      #   cryptoModules = [ "aes" "xts" "sha512" ];
 
-    # Support for Yubikey PBA
-    yubikeySupport = true;
+      # Support for Yubikey PBA
+      yubikeySupport = true;
 
-    devices = [
-      {
-        name = "root";
-        device = "/dev/nvme0n1p5";
-        preLVM = true;
-        allowDiscards = true;
-      }
-      {
-        name = "encrypted";
-        device = "/dev/sda1"; # Be sure to update this to the correct volume
+      devices = [
+        {
+          name = "root";
+          device = "/dev/nvme0n1p5";
+          preLVM = true;
+          allowDiscards = true;
+        }
+        {
+          name = "encrypted";
+          device = "/dev/sda1"; # Be sure to update this to the correct volume
 
-        yubikey = {
-          slot = 2;
-          twoFactor = true; # Set to false for 1FA
-          gracePeriod = 30; # Time in seconds to wait for Yubikey to be inserted
-          keyLength = 64; # Set to $KEY_LENGTH/8
-          saltLength = 16; # Set to $SALT_LENGTH
+          yubikey = {
+            slot = 2;
+            twoFactor = true; # Set to false for 1FA
+            gracePeriod =
+              30; # Time in seconds to wait for Yubikey to be inserted
+            keyLength = 64; # Set to $KEY_LENGTH/8
+            saltLength = 16; # Set to $SALT_LENGTH
 
-          storage = {
-            device =
-              "/dev/nvme0n1p1"; # Be sure to update this to the correct volume
-            fsType = "vfat";
-            path = "/crypt-storage/default";
+            storage = {
+              device =
+                "/dev/nvme0n1p1"; # Be sure to update this to the correct volume
+              fsType = "vfat";
+              path = "/crypt-storage/default";
+            };
           };
-        };
-      }
-    ];
+        }
+      ];
+    };
   };
 
   # Monitor backlight control
