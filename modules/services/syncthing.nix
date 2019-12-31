a259faa11f448f97411d3f5a9de3c7c3fb8452b4
Edmund Miller <edmund.a.miller@protonmail.com>
2019-12-31 11:16:41 -0600

    fix(syncthing): bump omen id

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -13,27 +13,27 @@
       devices = {
         omen = {
           id =
-          "5D6ELCY-YK2GOPP-BNJLVOH-F4OGXUD-D3Z5RCQ-LVGIFSS-MLS6LV5-DYLNYAK";
+            "JZCOYRI-OVUFHZ5-TV3WQ7D-L7PV3JA-6JDYG6D-TOJ47KK-EJMX4TR-XQAL4AY";
           name = "omen";
         };
         oneplus = {
           id =
-          "6EYD6V4-4KPXAVK-PR3GGLT-AYIWQ72-BUWQAUJ-PA7KAH7-QVGXA7Q-SKSL7AD";
+            "6EYD6V4-4KPXAVK-PR3GGLT-AYIWQ72-BUWQAUJ-PA7KAH7-QVGXA7Q-SKSL7AD";
           name = "oneplus";
         };
         meshify = {
           id =
-          "BNE2NYW-PLPCOLI-Z2T6Y5X-YICNTZO-RLFCSMN-VJ4QFPD-XJILLSQ-34XERAQ";
+            "BNE2NYW-PLPCOLI-Z2T6Y5X-YICNTZO-RLFCSMN-VJ4QFPD-XJILLSQ-34XERAQ";
           name = "meshify";
         };
-        envy = {
-          id = "FIXME";
-          name = "envy";
-        };
+        # envy = {
+        #   id = "FIXME";
+        #   name = "envy";
+        # };
       };
       folders = {
         sync = {
-          devices = [ "omen" "oneplus" "meshify" "envy" ];
+          devices = [ "omen" "oneplus" "meshify" ];
           id = "sync";
           path = "/home/emiller/sync";
         };
