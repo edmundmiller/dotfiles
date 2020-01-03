3ba0eb7e3964f541418e0d8886f465fcd16d9a6b
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-03 16:46:19 -0600

    feat(themes): init functional theme

---
+++ themes/functional/default.nix
@@ -0,0 +1,88 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ../. ]; # Load framework for themes
+
+  environment.systemPackages = with pkgs; [
+    sierra-gtk-theme
+    paper-icon-theme # for rofi
+  ];
+
+  fonts.fonts = [ pkgs.nerdfonts ];
+
+  services.compton = {
+    activeOpacity = "0.96";
+    inactiveOpacity = "0.75";
+    opacityRules = [
+      "100:class_g = 'Firefox'"
+      "100:class_g = 'Gimp'"
+      "100:class_g = 'feh'"
+      "100:class_g = 'mpv'"
+    ];
+    settings = {
+      blur-background = true;
+      blur-background-frame = true;
+      blur-background-fixed = true;
+      blur-kern = "7x7box";
+      blur-strength = 340;
+      blur-background-exclude =
+        [ "window_type = 'dock'" "window_type = 'desktop'" ];
+    };
+  };
+
+  services.xserver.displayManager.lightdm = {
+    background = "${./wallpaper.orange.png}";
+    greeters.mini.extraConfig = ''
+      text-color = "#bbc2cf"
+      password-background-color = "#29323d"
+      window-color = "#0b1117"
+      border-color = "#0b1117"
+    '';
+  };
+
+  home-manager.users.emiller = {
+    home.file.".background-image".source = ./wallpaper.png;
+
+    xdg.configFile = {
+      "xtheme/90-theme".source = ./Xresources;
+      "dunst/dunstrc".source = ./dunstrc;
+      "bspwm/rc.d/theme".source = ./bspwmrc;
+      "bspwm/rc.d/polybar".source = ./polybar/run.sh;
+      "polybar" = {
+        source = ./polybar;
+        recursive = true;
+      };
+      "rofi/theme" = {
+        source = ./rofi;
+        recursive = true;
+      };
+      "zsh/prompt.zsh".source = ./zsh/prompt.zsh;
+
+      # GTK
+      "gtk-3.0/settings.ini".text = ''
+        [Settings]
+        gtk-theme-name=Sierra-compact-dark
+        gtk-icon-theme-name=Paper
+        gtk-fallback-icon-theme=gnome
+        gtk-application-prefer-dark-theme=true
+        gtk-cursor-theme-name=Paper
+        gtk-xft-hinting=1
+        gtk-xft-hintstyle=hintfull
+        gtk-xft-rgba=none
+      '';
+
+      # GTK2 global theme (widget and icon theme)
+      "gtk-2.0/gtkrc".text = ''
+        gtk-theme-name="Sierra-compact-dark"
+        gtk-icon-theme-name="Paper-Mono-Dark"
+        gtk-font-name="Sans 10"
+      '';
+
+      # QT4/5 global theme
+      "Trolltech.conf".text = ''
+        [Qt]
+        style=Sierra-compact-dark
+      '';
+    };
+  };
+}
