8d211b2b52a8b85980d1eaacdad644ff08c232df
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-03 15:13:25 -0600

    fix(envy): use pantheon instead of gnome

---
+++ modules/desktop/pantheon.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [ ./features/xserver.nix ./features/gtk.nix ];
+
+  services = {
+    printing.enable = true;
+    pantheon.contractor.enable = true;
+
+    xserver = {
+      desktopManager.pantheon.enable = true;
+
+      displayManager.lightdm = {
+        enable = true;
+        greeters.gtk.enable = true;
+      };
+    };
+  };
+}
