71b8da025a810d50a745e75b526e95ceac7364b7
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-04 01:06:25 -0600

    fix(transmission): give up on fancy home dir

--- modules/services/transmission.nix
+++ modules/services/transmission.nix
@@ -6,10 +6,8 @@
   services.transmission = {
     enable = true;
     settings = {
-      download-dir = "/home/emiller/torrents";
-      incomplete-dir = "/home/emiller/torrents/.incomplete";
       incomplete-dir-enabled = true;
-      rpc-whitelist = "127.0.0.1,192.168.*.*,10.0.0.*";
+      rpc-whitelist = "127.0.0.1,192.168.*.*";
       rpc-host-whitelist = "*";
       rpc-host-whitelist-enabled = true;
       ratio-limit = 0;
@@ -20,20 +18,5 @@
   networking.firewall = {
     allowedTCPPorts = [ 51413 ];
     allowedUDPPorts = [ 51413 ];
-  };
-
-  systemd.services.transmission = { ... }: {
-    options = {
-      serviceConfig = lib.mkOption {
-        apply = old:
-          old // {
-            ExecStartPre = pkgs.writeScript "transmission-pre-start-two" ''
-              #!${pkgs.runtimeShell}
-               ${old.ExecStartPre}
-               chmod 777 /home/emiller/torrents
-            '';
-          };
-      };
-    };
   };
 }
