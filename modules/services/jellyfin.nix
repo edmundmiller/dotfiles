433882b34f2281803464fd56ce76360dffbfb2a1
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-11 17:10:40 -0600

    fix(jellyfin): use docker image to run
    Figured out how to setup docker-containers which runs them as systemd
    services. I had to do it because the dotnet-sdk package didn't work on
    my rockpro for whatever reason because it's mislabeled as aarch64 and
    the package didn't like the label. Probably could've just fixed that but
    thought this might be a better solution for non-nixos stuff.

--- modules/services/jellyfin.nix
+++ modules/services/jellyfin.nix
@@ -1,7 +1,20 @@
 { config, lib, pkgs, ... }:
 
 {
-  services.jellyfin = { enable = true; };
+  # This doesn't work on RockPro64
+  # services.jellyfin = { enable = true; };
+
+  docker-containers = {
+    "jellyfin" = {
+      image = "jellyfin/jellyfin";
+      volumes = [
+        "/var/lib/jellyfin/config:/config"
+        "/var/cache/jellyfin:/cache"
+        "/data/media:/media"
+      ];
+      extraDockerOptions = [ "--net=host" ];
+    };
+  };
 
   networking.firewall = {
     allowedTCPPorts = [ 8096 ];
