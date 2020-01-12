5516a518a54d51ffe7b0984520144f108deefb86
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-11 23:38:18 -0600

    feat(rock): use CrystalGamma boot
    https://gist.github.com/CrystalGamma/e938cca081bb685c50dbcb02717f7ce8

--- configuration.rock.nix
+++ configuration.rock.nix
@@ -17,9 +17,69 @@
 
   networking.hostName = "rock";
   networking.networkmanager.enable = true;
-  boot.loader.grub.enable = false;
-
-  nixpkgs.config.allowUnsupportedSystem = true;
+  boot = {
+    tmpOnTmpfs = true;
+    loader.grub = {
+      enable = false;
+      version = 2;
+      device = "nodev";
+    };
+    loader.generic-extlinux-compatible = { enable = true; };
+    kernelPackages = pkgs.linuxPackagesFor (pkgs.buildLinux (pkgs // {
+      /* src=/home/user/linux-test.tar.xz;
+         version="5.4-rc8test";
+         modDirVersion="5.4.0-rc8-MANJARO-ARM";
+      */
+      src = /home/user/linux-5.5-rc5.tar.xz;
+      version = "5.5-rc5pbp";
+      modDirVersion = "5.5.0-rc5-MANJARO-ARM";
+      /* src=/home/user/linux-5.4.6.tar.gz;
+         version="5.4.6pbp";
+         modDirVersion="5.4.6-MANJARO-ARM";
+      */
+      autoModules = false;
+      defconfig = "pinebook_pro_defconfig";
+      kernelPatches = [
+        {
+          extraConfig = "CRYPTO_AEGIS128_SIMD n";
+          patch = null;
+          name = "no_aegis128_simd";
+        }
+        {
+          patch = null;
+          name = "reduced";
+          extraConfig = ''
+            ARM64_SVE n
+            PCI_XGENE n
+            PCI_HISI n
+            AHCI_XGENE n
+            DRM_RADEON n
+            DRM_AMDGPU n
+            DRM_NOUVEAU n
+            MFD_CROS_EC n
+            COMMON_CLK_XGENE n
+            PHY_QCOM_USB_HS n
+            PHY_QCOM_USB_HSIC n
+            HISI_PMU n
+            LIBNVDIMM n
+            BLK_DEV_PMEM n
+            ND_BLK n
+            MEDIA_SUPPORT n
+            STAGING n
+            MISC_FILESYSTEMS n
+            NETWORK_FILESYSTEMS n
+          '';
+        }
+        {
+          patch = null;
+          name = "psci-cpuidle";
+          extraConfig = "ARM_PSCI_CPUIDLE n";
+        }
+      ];
+    }));
+    supportedFilesystems = lib.mkForce [ ];
+    initrd.supportedFilesystems = lib.mkForce [ ];
+  };
 
   users.users.emiller.extraGroups = [ "networkmanager" ];
 }
