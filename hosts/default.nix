ce672bcaa9fe8ecea5079f3dd03cd9a3f92fe567
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-17 22:39:37 -0600

    feat: add hosts default, linode and personal
    - default is just the new default module
    - Personal is to eradicate all none xdg compliant things
    - linode installs on a linode. I guess they fixed my issue. I'll need to
    add a vultr module

---
+++ hosts/default.nix
@@ -0,0 +1,22 @@
+# hosts/default.nix --- settings common to all my systems
+
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    coreutils
+    git
+    killall
+    unzip
+    vim
+    wget
+    sshfs
+  ];
+
+  ### My user settings
+  my.username = "emiller";
+  my.user = {
+    isNormalUser = true;
+    uid = 1000;
+    extraGroups = [ "wheel" "video" "networkmanager" ];
+    shell = pkgs.zsh;
+  };
+}
---
+++ hosts/linode.nix
@@ -0,0 +1,63 @@
+# 1. Create three (3) disk images:
+#    - Installer: 1024mb
+#    - Swap: 512mb
+#    - NixOS: rest
+#
+# 2. Boot in rescue mode with:
+#    - /dev/sda -> Installer
+#    - /dev/sdb -> Swap
+#    - /dev/sdc -> NixOS
+#
+# 3. Once booted into Finnix (step 2) pipe this script to sh:
+#      iso=<nixos-64bit-iso-url>
+#      update-ca-certificates
+#      curl $url | dd of=/dev/sda
+#
+# 4. Install these dotfiles with
+#      git clone https://github.com/hlissner/dotfiles ~/.dotfiles
+#      ln -s ~/.dotfiles /etc/dotfiles
+#      cd ~/.dotfiles
+#      mk install-linode
+#
+# 4. Create a configuration profile:
+#    - Kernel: GRUB 2
+#    - /dev/sda -> NixOS
+#    - /dev/sdb -> Swap
+#    - Helpers: distro and auto network helpers = off
+#
+# 5. Reboot into profile.
+
+{ config, lib, pkgs, ... }: {
+  environment.systemPackages = with pkgs; [ inetutils mtr sysstat git ];
+
+  imports = [
+    ./.
+    <modules/editors/vim.nix>
+    <modules/shell/git.nix>
+    <modules/shell/zsh.nix>
+    <modules/services/ssh.nix>
+  ];
+
+  # GRUB
+  boot = {
+    kernelParams = [ "console=ttyS0" ];
+    loader = {
+      timeout = 10;
+      grub = {
+        enable = true;
+        version = 2;
+        device = "nodev";
+        copyKernels = true;
+        fsIdentifier = "provided";
+        extraConfig = "serial; terminal_input serial; terminal_output serial";
+      };
+      # Disable globals
+      systemd-boot.enable = false;
+      efi.canTouchEfiVariables = false;
+    };
+  };
+
+  networking.useDHCP = false;
+  networking.usePredictableInterfaceNames = false;
+  networking.interfaces.eth0.useDHCP = true;
+}
