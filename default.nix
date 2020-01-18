3512e0077a3bacb7041bf986414770920af9ebfd
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-17 22:24:47 -0600

    refactor(default): new default + options
    Highlights:
    - Secrets!
    - `cached-nix-shell`
    - Bringing back the shellAliases set from the env
    - modules is a new path
    - I'm not sure what's going on in options yet but it sets the my thing
    that gonna get rid of the home-manager.users.emiller calls.

--- default.nix
+++ default.nix
@@ -1,97 +1,37 @@
-{ config, pkgs, options, ... }:
+# default.nix --- my dotfile bootstrapper
 
-{
-  imports = [
-    <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
-    <home-manager/nixos>
+device:
+{ pkgs, options, lib, config, ... }: {
+  networking.hostName = lib.mkDefault device;
+
+  imports = [ ./options.nix "${./hosts}/${device}" ];
+
+  ### NixOS
+  nix.autoOptimiseStore = true;
+  nix.nixPath = options.nix.nixPath.default ++ [
+    # So we can use absolute import paths
+    "config=/etc/dotfiles/config"
+    "modules=/etc/dotfiles/modules"
   ];
+  # ...but we still need to set nixpkgs.overlays to make them visible to the
+  # rebuild process, however...
+  nixpkgs.overlays = import ./overlays.nix;
+  nixpkgs.config.allowUnfree = true;
 
-  boot = {
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-    };
-    cleanTmpDir = true;
-    plymouth.enable = true;
+  # For instant nix-shell scripts
+  environment.systemPackages = [ pkgs.my.cached-nix-shell ];
+  environment.shellAliases = {
+    nix-env = "NIXPKGS_ALLOW_UNFREE=1 nix-env";
+    nix-shell = ''
+      NIX_PATH="nixpkgs-overlays=/etc/dotfiles/overlays.nix:$NIX_PATH" nix-shell'';
+    nsh = "nix-shell";
+    nen = "nix-env";
+    dots = "make -C ~/.dotfiles";
   };
-
-  nix = {
-    nixPath = options.nix.nixPath.default
-      ++ [ "config=/etc/dotfiles/config" "packages=/etc/dotfiles/packages" ];
-    autoOptimiseStore = true;
-    trustedUsers = [ "root" "@wheel" ];
-  };
-  nixpkgs.config = {
-    allowUnfree = true;
-
-    packageOverrides = pkgs: {
-      unstable = import <nixpkgs-unstable> { config = config.nixpkgs.config; };
-      nur = import (builtins.fetchTarball
-        "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-          inherit pkgs;
-        };
-    };
-  };
-
-  environment = {
-    systemPackages = with pkgs; [
-      # Just the bear necessities~
-      libqalculate
-      coreutils
-      git
-      killall
-      unzip
-      vim
-      wget
-      # Support for extra filesystems
-      sshfs
-      exfat
-      ntfs3g
-      hfsprogs
-    ];
-    variables = {
-      XDG_CONFIG_HOME = "$HOME/.config";
-      XDG_CACHE_HOME = "$HOME/.cache";
-      XDG_DATA_HOME = "$HOME/.local/share";
-      XDG_BIN_HOME = "$HOME/.local/bin";
-    };
-  };
-
-  time.timeZone = "America/Chicago";
-  location = (if config.time.timeZone == "America/Chicago" then {
-    latitude = 32.98616;
-    longitude = -96.75043;
-  } else
-    { });
-
-  # Block well known bad hosts
-  networking.extraHosts = builtins.readFile (builtins.fetchurl {
-    name = "blocked_hosts.txt";
-    url =
-      "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext";
-  });
-
-  users.users.emiller = {
-    isNormalUser = true;
-    uid = 1000;
-    description = "Edmund Miller";
-    extraGroups = [ "wheel" "video" ];
-    shell = pkgs.zsh;
-  };
-
-  home-manager.users.emiller = {
-    xdg.enable = true;
-    home.file."bin" = {
-      source = ./bin;
-      recursive = true;
-    };
-  };
-
-  networking.firewall.enable = true;
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "19.03"; # Did you read the comment?
+  system.stateVersion = "19.09"; # Did you read the comment?
 }
