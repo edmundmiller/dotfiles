6a449d174eda38bdbaae0678d81ab305f913c329
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-19 17:03:46 -0600

    refactor(firefox): follow new conventions
    This is the commit where I understand `my`.
    It's set in options so that `my.home == home-manager...emiller`

--- modules/browser/firefox.nix
+++ modules/browser/firefox.nix
@@ -1,28 +1,27 @@
-{ config, lib, pkgs, ... }:
+# modules/browser/firefox.nix --- https://www.mozilla.org/en-US/firefox
 
-{
-  programs.browserpass.enable = true;
-  environment = {
-    sessionVariables = {
-      BROWSER = "firefox";
-      XDG_DESKTOP_DIR = "$HOME"; # prevent firefox creating ~/Desktop
-    };
+{ config, lib, pkgs, ... }: {
+  my.packages = with pkgs; [
+    firefox-bin
+    (pkgs.writeScriptBin "firefox-private" ''
+      #!${stdenv.shell}
+      ${firefox}/bin/firefox --private-window "$@"
+    '')
+    (makeDesktopItem {
+      name = "firefox-private";
+      desktopName = "Firefox (Private)";
+      genericName = "Open a private Firefox window";
+      icon = "firefox";
+      exec = "${firefox-bin}/bin/firefox --private-window";
+      categories = "Network";
+    })
+    tridactyl-native # FIXME still have to run in firefox
+    libu2f-host # Yubikey
+  ];
 
-    systemPackages = with pkgs; [
-      firefox-bin
-      (pkgs.writeScriptBin "firefox-private" ''
-        #! ${pkgs.bash}/bin/bash
-        firefox --private-window "$@"
-      '')
-      tridactyl-native
-      libu2f-host
-    ];
-  };
-
-  home-manager.users.emiller = {
-    xdg.configFile = {
-      # TODO install automagically
-      "tridactyl/tridactylrc".source = <config/tridactyl/tridactylrc>;
-    };
+  my.env.BROWSER = "firefox";
+  my.env.XDG_DESKTOP_DIR = "$HOME"; # prevent firefox creating ~/Desktop
+  my.home.xdg = {
+    configFile."tridactyl/tridactylrc".source = <config/tridactyl/tridactylrc>;
   };
 }
