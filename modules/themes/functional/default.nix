a3c1573c5d2f4437042c16484364af5d8a7ed905
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-19 22:13:32 -0600

    refactor(themes): bring functional up to new standard

--- modules/themes/functional/default.nix
+++ modules/themes/functional/default.nix
@@ -1,37 +1,23 @@
-{ config, pkgs, ... }:
+# modules/themes/functional/default.nix --- functional genomics/programming
 
-{
+{ config, lib, pkgs, ... }: {
   imports = [ ../. ]; # Load framework for themes
 
-  environment.systemPackages = with pkgs; [
-    sierra-gtk-theme
-    paper-icon-theme # for rofi
-  ];
-
-  fonts.fonts = [ pkgs.nerdfonts ];
+  theme.wallpaper = ./wallpaper.jpg;
 
   services.compton = {
     activeOpacity = "0.96";
     inactiveOpacity = "0.75";
-    opacityRules = [
-      "100:class_g = 'Firefox'"
-      "100:class_g = 'Gimp'"
-      "100:class_g = 'feh'"
-      "100:class_g = 'mpv'"
-    ];
     settings = {
       blur-background = true;
       blur-background-frame = true;
       blur-background-fixed = true;
       blur-kern = "7x7box";
       blur-strength = 340;
-      blur-background-exclude =
-        [ "window_type = 'dock'" "window_type = 'desktop'" ];
     };
   };
 
   services.xserver.displayManager.lightdm = {
-    background = "${./wallpaper.orange.png}";
     greeters.mini.extraConfig = ''
       text-color = "#bbc2cf"
       password-background-color = "#29323d"
@@ -40,51 +26,48 @@
     '';
   };
 
-  home-manager.users.emiller = {
-    home.file.".background-image".source = ./wallpaper.png;
-
-    xdg.configFile = {
-      "xtheme/90-theme".source = ./Xresources;
-      "dunst/dunstrc".source = ./dunstrc;
-      "bspwm/rc.d/theme".source = ./bspwmrc;
-      "bspwm/rc.d/polybar".source = ./polybar/run.sh;
-      "polybar" = {
-        source = ./polybar;
-        recursive = true;
-      };
-      "rofi/theme" = {
-        source = ./rofi;
-        recursive = true;
-      };
-      "zsh/prompt.zsh".source = ./zsh/prompt.zsh;
-
-      # GTK
-      "gtk-3.0/settings.ini".text = ''
-        [Settings]
-        gtk-theme-name=Sierra-compact-dark
-        gtk-icon-theme-name=Paper
-        gtk-fallback-icon-theme=gnome
-        gtk-application-prefer-dark-theme=true
-        gtk-cursor-theme-name=Paper
-        gtk-xft-hinting=1
-        gtk-xft-hintstyle=hintfull
-        gtk-xft-rgba=none
-      '';
-
-      "gtk-3.0/gtk.css".text = ".termite {padding: 15px;}";
-
-      # GTK2 global theme (widget and icon theme)
-      "gtk-2.0/gtkrc".text = ''
-        gtk-theme-name="Sierra-compact-dark"
-        gtk-icon-theme-name="Paper-Mono-Dark"
-        gtk-font-name="Sans 10"
-      '';
-
-      # QT4/5 global theme
-      "Trolltech.conf".text = ''
-        [Qt]
-        style=Sierra-compact-dark
-      '';
+  fonts.fonts = [ pkgs.nerdfonts ];
+  my.packages = with pkgs; [
+    sierra-gtk-theme
+    paper-icon-theme # for rofi
+  ];
+  my.zsh.rc = lib.readFile ./zsh/prompt.zsh;
+  my.home.xdg.configFile = {
+    "bspwm/rc.d/polybar".source = ./polybar/run.sh;
+    "bspwm/rc.d/theme".source = ./bspwmrc;
+    "dunst/dunstrc".source = ./dunstrc;
+    "polybar" = {
+      source = ./polybar;
+      recursive = true;
     };
+    "rofi/theme" = {
+      source = ./rofi;
+      recursive = true;
+    };
+    "tmux/theme".source = ./tmux.conf;
+    "xtheme/90-theme".source = ./Xresources;
+    # GTK
+    "gtk-3.0/settings.ini".text = ''
+      [Settings]
+      gtk-theme-name=Sierra-compact-dark
+      gtk-icon-theme-name=Paper
+      gtk-fallback-icon-theme=gnome
+      gtk-application-prefer-dark-theme=true
+      gtk-cursor-theme-name=Paper
+      gtk-xft-hinting=1
+      gtk-xft-hintstyle=hintfull
+      gtk-xft-rgba=none
+    '';
+    # GTK2 global theme (widget and icon theme)
+    "gtk-2.0/gtkrc".text = ''
+      gtk-theme-name="Sierra-compact-dark"
+      gtk-icon-theme-name="Paper-Mono-Dark"
+      gtk-font-name="Sans 10"
+    '';
+    # QT4/5 global theme
+    "Trolltech.conf".text = ''
+      [Qt]
+        style=Sierra-compact-dark
+    '';
   };
 }
