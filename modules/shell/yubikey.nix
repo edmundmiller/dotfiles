9f8cca12d6a96d161918cf164e17e04d587d0d85
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-20 12:52:41 -0600

    refactor(yubikey): use my for init

--- modules/shell/yubikey.nix
+++ modules/shell/yubikey.nix
@@ -1,21 +1,22 @@
 { config, lib, pkgs, ... }:
 
 {
-  my.packages = with pkgs; [
-    gnupg
-    yubikey-manager-qt
-    yubikey-manager
-    yubikey-personalization
-  ];
+  my = {
+    packages = with pkgs; [
+      gnupg
+      yubikey-manager-qt
+      yubikey-manager
+      yubikey-personalization
+    ];
+    init = ''
+      export GPG_TTY="$(tty)"
+      gpg-connect-agent /bye
+      export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+    '';
+  };
 
   services.udev.packages = [ pkgs.yubikey-personalization pkgs.libu2f-host ];
   services.pcscd.enable = true;
-
-  environment.shellInit = ''
-    export GPG_TTY="$(tty)"
-    gpg-connect-agent /bye
-    export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-  '';
 
   programs = {
     ssh.startAgent = false;
