2f67a49c794e76ed6d356eaa2cf74df60f9c577e
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-19 19:16:41 -0600

    refactor(syncthing): use hlissner's slick rewrite
    It does what I was doing but with more modularity

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -1,49 +1,52 @@
 { config, lib, pkgs, ... }:
 
 {
-  users.users.emiller.extraGroups = [ "syncthing" ];
+  my.packages = with pkgs; [ syncthing ];
 
-  environment = { systemPackages = with pkgs; [ syncthing ]; };
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
-    user = "emiller";
-    configDir = "/home/emiller/.config/syncthing";
-    dataDir = "/home/emiller/sync";
+    user = config.my.username;
+    configDir = "/home/${config.my.username}/.config/syncthing";
+    dataDir = "/home/${config.my.username}/.local/share/syncthing";
     declarative = {
       devices = {
-        omen = {
-          id =
-            "JZCOYRI-OVUFHZ5-TV3WQ7D-L7PV3JA-6JDYG6D-TOJ47KK-EJMX4TR-XQAL4AY";
-          name = "omen";
-        };
-        oneplus = {
-          id =
-            "6EYD6V4-4KPXAVK-PR3GGLT-AYIWQ72-BUWQAUJ-PA7KAH7-QVGXA7Q-SKSL7AD";
-          name = "oneplus";
-        };
-        meshify = {
-          id =
-            "BNE2NYW-PLPCOLI-Z2T6Y5X-YICNTZO-RLFCSMN-VJ4QFPD-XJILLSQ-34XERAQ";
-          name = "meshify";
-        };
-        node = {
-          id =
-            "OBXUZUC-NOTB65C-POT2MAD-MDCOEIB-HIPGRIZ-LVNRJO6-HF25SOF-URFN3QP";
-          name = "node";
-        };
-        # envy = {
-        #   id = "FIXME";
-        #   name = "envy";
-        # };
+        omen.id =
+          "JZCOYRI-OVUFHZ5-TV3WQ7D-L7PV3JA-6JDYG6D-TOJ47KK-EJMX4TR-XQAL4AY";
+        oneplus.id =
+          "6EYD6V4-4KPXAVK-PR3GGLT-AYIWQ72-BUWQAUJ-PA7KAH7-QVGXA7Q-SKSL7AD";
+        meshify.id =
+          "BNE2NYW-PLPCOLI-Z2T6Y5X-YICNTZO-RLFCSMN-VJ4QFPD-XJILLSQ-34XERAQ";
+        node.id =
+          "OBXUZUC-NOTB65C-POT2MAD-MDCOEIB-HIPGRIZ-LVNRJO6-HF25SOF-URFN3QP";
+        # envy.id =
+        # TODO
       };
-      folders = {
-        sync = {
+      folders = let
+        deviceEnabled = devices: lib.elem config.networking.hostName devices;
+        deviceType = devices:
+          if deviceEnabled devices then "sendreceive" else "receiveonly";
+      in {
+        projects = rec {
           devices = [ "omen" "oneplus" "meshify" "node" ];
-          id = "sync";
-          path = "/home/emiller/sync";
+          path = "/home/${config.my.username}/sync";
+          watch = false;
+          rescanInterval = 3600 * 2;
+          type = deviceType [ "omen" "meshify" ];
+          enable = deviceEnabled devices;
+        };
+        secrets = rec {
+          devices = [ "omen" "oneplus" "meshify" "node" ];
+          path = "/home/${config.my.username}/.secrets";
+          watch = true;
+          rescanInterval = 3600;
+          type = deviceType [ "omen" "meshify" ];
+          enable = deviceEnabled devices;
         };
       };
     };
   };
+
+  my.user.extraGroups = [ "syncthing" ];
+
 }
