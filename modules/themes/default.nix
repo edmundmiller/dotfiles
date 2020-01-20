7d2ebf3b638c1862070ffa4a066326a0c5cb27eb
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-19 21:43:31 -0600

    refactor(themes): add options and remove middle-earth
    It just wasn't working out. The wallpaper wasn't 4k.
    
    The options though is going to be great it autoblurs the background for me.

--- modules/themes/default.nix
+++ modules/themes/default.nix
@@ -1,16 +1,15 @@
-{ config, lib, pkgs, ... }:
+# modules/themes/default.nix
 
-{
-  environment.variables = {
-    GTK_DATA_PREFIX = [ "${config.system.path}" ];
-    QT_QPA_PLATFORMTHEME = "gtk2";
-  };
+{ config, ... }: {
+  imports = [ ./options.nix ];
 
+  # Try really hard to get QT to respect my GTK theme.
+  my.env.GTK_DATA_PREFIX = [ "${config.system.path}" ];
+  my.env.QT_QPA_PLATFORMTHEME = "gtk2";
   qt5 = {
     style = "gtk2";
     platformTheme = "gtk2";
   };
-
   services.xserver.displayManager.sessionCommands = ''
     export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
     source "$XDG_CONFIG_HOME"/xsession/*.sh
