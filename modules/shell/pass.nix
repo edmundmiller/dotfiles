8a774e4b52ac15f3b19c820d78d669a94c32d578
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-19 20:47:27 -0600

    refactor(pass): remove rofi
    This is included in <modules/desktop/apps/rofi.nix>

--- modules/shell/pass.nix
+++ modules/shell/pass.nix
@@ -2,11 +2,11 @@
 
 {
   environment = {
-    systemPackages = with pkgs; [
-      (pass.withExtensions
-        (exts: [ exts.pass-otp exts.pass-genphrase exts.pass-import ]))
-      (lib.mkIf (config.services.xserver.enable) rofi-pass)
-    ];
+    systemPackages = with pkgs;
+      [
+        (pass.withExtensions
+          (exts: [ exts.pass-otp exts.pass-genphrase exts.pass-import ]))
+      ];
 
     # HACK Have to symlink to ~/.password-store for mail
     variables.PASSWORD_STORE_DIR = "$HOME/.secrets/password-store";
