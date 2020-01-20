5b74fa48c1c06f101b22625e763a22bf269105a8
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-19 19:16:20 -0600

    feat(services): add a few services from hlissner

---
+++ modules/services/gitea.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # I prefer git@... ssh addresses over gitea@...
+  users.users.git = {
+    useDefaultShell = true;
+    home = "/var/lib/gitea";
+    group = "gitea";
+  };
+
+  services.gitea = {
+    enable = true;
+    log.level = "Info";
+
+    user = "git";
+    database = {
+      user = "git";
+      type = "postgres";
+    };
+    useWizard = false;
+    disableRegistration = true;
+
+    # We're assuming SSL-only connectivity
+    cookieSecure = true;
+  };
+}
