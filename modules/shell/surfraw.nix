41b64d42e7732635ddba1133046e1901a041c49a
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-19 21:09:48 -0600

    fix(surfraw): remove module

--- modules/shell/surfraw.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-{
-  environment.systemPackages = with pkgs; [ surfraw ];
-
-  home-manager.users.emiller.xdg.configFile = {
-    "surfraw".source = <config/surfraw>;
-  };
-}
