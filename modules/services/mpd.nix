094b5effe9ec7bab7d7ffb5432f6af090aff264c
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-19 20:50:20 -0600

    refactor(mpd): use my
    This lead to some regrouping

--- modules/services/mpd.nix
+++ modules/services/mpd.nix
@@ -1,36 +1,45 @@
 { config, lib, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ mpc_cli ];
+  my = {
+    packages = with pkgs; [
+      mpc_cli
+      (ncmpcpp.override { visualizerSupport = true; })
+    ];
 
-  services.mpd = {
-    enable = true;
-    musicDirectory = "/data/media/music";
-    startWhenNeeded = true;
-    extraConfig = ''
-      input {
-          plugin "curl"
-      }
-      audio_output {
-          type        "pulse"
-          name        "pulse audio"
-          server      "127.0.0.1"
-      }
-      audio_output {
-          type        "fifo"
-          name        "mpd_fifo"
-          path        "/tmp/mpd.fifo"
-          format      "44100:16:2"
-      }
-    '';
+    user.extraGroups = [ "mpd" ];
+
+    home.programs.beets.enable = true;
+  };
+
+  services = {
+    mpd = {
+      enable = true;
+      musicDirectory = "/data/media/music";
+      startWhenNeeded = true;
+      extraConfig = ''
+        input {
+                plugin "curl"
+        }
+
+        audio_output {
+            type        "pulse"
+            name        "pulse audio"
+            server      "127.0.0.1"
+        }
+
+        audio_output {
+            type        "fifo"
+            name        "mpd_fifo"
+            path        "/tmp/mpd.fifo"
+            format      "44100:16:2"
+        }
+      '';
+    };
   };
 
   hardware.pulseaudio.tcp = {
     enable = true;
     anonymousClients.allowedIpRanges = [ "127.0.0.1" ];
   };
-
-  my.user.extraGroups = [ "mpd" ];
-
-  my.programs.beets.enable = true;
 }
