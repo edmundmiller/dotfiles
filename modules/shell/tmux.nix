7e251df2b420729dc89c3765b319b5df0edf931b
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-21 00:35:23 -0600

    style: run nixfmt

--- modules/shell/tmux.nix
+++ modules/shell/tmux.nix
@@ -2,15 +2,16 @@
 
 {
   my = {
-    packages = with pkgs; [
-      # The developer of tmux chooses not to add XDG support for religious
-      # reasons (see tmux/tmux#142). Fortunately, nix offers a simple
-      # workaround:
-      (writeScriptBin "tmux" ''
-        #!${stdenv.shell}
-        exec ${tmux}/bin/tmux -f "$TMUX_HOME/config" "$@"
+    packages = with pkgs;
+      [
+        # The developer of tmux chooses not to add XDG support for religious
+        # reasons (see tmux/tmux#142). Fortunately, nix offers a simple
+        # workaround:
+        (writeScriptBin "tmux" ''
+          #!${stdenv.shell}
+          exec ${tmux}/bin/tmux -f "$TMUX_HOME/config" "$@"
         '')
-    ];
+      ];
 
     env.TMUX_HOME = "$XDG_CONFIG_HOME/tmux";
     env.TMUXIFIER = "$XDG_DATA_HOME/tmuxifier";
@@ -21,6 +22,9 @@
       _cache tmuxifier init -
       ${lib.readFile <config/tmux/aliases.zsh>}
     '';
-    home.xdg.configFile."tmux" = { source = <config/tmux>; recursive = true; };
+    home.xdg.configFile."tmux" = {
+      source = <config/tmux>;
+      recursive = true;
+    };
   };
 }
