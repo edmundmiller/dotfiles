4a1fa3d22310820830e3cfdc41606d773702a85c
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-24 00:40:01 -0600

    fix(node): update hardware

--- hosts/node/hardware-configuration.nix
+++ hosts/node/hardware-configuration.nix
@@ -1,5 +1,9 @@
 { ... }: {
   imports = [ <nixpkgs/nixos/modules/profiles/qemu-guest.nix> ];
+
+  boot.initrd.availableKernelModules =
+    [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
+
   boot.loader.grub.device = "/dev/vda";
 
   fileSystems."/" = {
