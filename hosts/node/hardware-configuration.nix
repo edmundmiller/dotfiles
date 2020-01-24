e7dd4e9a336fad1a980e3d2120a9f2afde0bd5ee
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-24 16:28:36 -0600

    fix(node): add grub stuff

--- hosts/node/hardware-configuration.nix
+++ hosts/node/hardware-configuration.nix
@@ -5,6 +5,8 @@
     [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
 
   boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
 
   fileSystems."/" = {
     device = "/dev/vda1";
