385d1e763d3ce634f9966cfcbd8691af9740eabb
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-27 12:00:27 -0600

    fix(git): move away from home-manager writing git config

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -7,36 +7,10 @@ in {
   my = {
     packages = with pkgs; [ gitAndTools.hub gitAndTools.diff-so-fancy ];
     zsh.rc = lib.readFile <config/git/aliases.zsh>;
-    # TODO Move away from home-manager to configure this
     # Do recursively, in case git stores files in this folder
-    # home.xdg.configFile = {
-    # "git/config".source = <config/git/config>;
-    # "git/ignore".source = <config/git/ignore>;
-    # };
-
-    home = {
-      programs = {
-        git = {
-          enable = true;
-          lfs.enable = true;
-          userName = "${name}";
-          userEmail = "${protonmail}";
-          signing.key = "BC10AA9D";
-          signing.signByDefault = true;
-          extraConfig = {
-            github = { user = "emiller88"; };
-            gitlab = { user = "emiller88"; };
-            color = { ui = "auto"; };
-            rebase = { autosquash = "true"; };
-            push = { default = "current"; };
-            merge = {
-              ff = "onlt";
-              log = "true";
-            };
-          };
-          ignores = [ ".direnv" ".envrc" ];
-        };
-      };
+    home.xdg.configFile = {
+      "git/config".source = <config/git/config>;
+      "git/ignore".source = <config/git/ignore>;
     };
   };
 }
