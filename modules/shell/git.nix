aa797e71f5e00ac690c975062d1d64f7dabdea29
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-27 22:05:20 -0600

    fix(git): add git-lfs

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -5,7 +5,7 @@ let
   protonmail = "edmund.a.miller@protonmail.com";
 in {
   my = {
-    packages = with pkgs; [ gitAndTools.hub gitAndTools.diff-so-fancy ];
+    packages = with pkgs; [ git-lfs gitAndTools.hub gitAndTools.diff-so-fancy ];
     zsh.rc = lib.readFile <config/git/aliases.zsh>;
     # Do recursively, in case git stores files in this folder
     home.xdg.configFile = {
