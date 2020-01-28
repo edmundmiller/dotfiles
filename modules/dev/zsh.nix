286b1678b5c779ae9390e749134352b9aaeba3b5
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-27 22:05:34 -0600

    fix(zsh): add shfmt

--- modules/dev/zsh.nix
+++ modules/dev/zsh.nix
@@ -1,5 +1,5 @@
 # modules/dev/zsh.nix --- http://zsh.sourceforge.net/
 
 { pkgs, ... }: {
-  my.packages = with pkgs; [ shellcheck my.zunit ];
+  my.packages = with pkgs; [ shellcheck shfmt my.zunit ];
 }
