54558761de6a0913f9db0863a0efb9cd4906f721
Edmund Miller <edmund.a.miller@protonmail.com>
2020-02-09 23:56:38 -0600

    fix(rust): add llvm bintools

--- modules/dev/rust.nix
+++ modules/dev/rust.nix
@@ -2,7 +2,7 @@
 
 { pkgs, ... }: {
   my = {
-    packages = with pkgs; [ rustup ];
+    packages = with pkgs; [ rustup llvmPackages.bintools ];
 
     env.RUSTUP_HOME = "$XDG_DATA_HOME/rustup";
     env.CARGO_HOME = "$XDG_DATA_HOME/cargo";
