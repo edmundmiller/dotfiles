b9c6c57ca1dcb89ab8325dd7a9756c03ed47adfe
Edmund Miller <edmund.a.miller@protonmail.com>
2020-02-22 22:29:20 -0600

    Revert "fix(sway): remove auto sway start"
    This reverts commit 4bc2a87c4a9fbf0d75a81f0e7652608da17b4c1a.

--- modules/desktop/sway.nix
+++ modules/desktop/sway.nix
@@ -14,7 +14,7 @@ in {
     ./apps/st.nix
   ];
 
-  # nixpkgs.overlays = [ waylandOverlay ];
+  nixpkgs.overlays = [ waylandOverlay ];
 
   fonts.fonts = [ pkgs.siji ];
 
@@ -35,6 +35,66 @@ in {
   };
 
   services.redshift.package = pkgs.unstable.redshift-wlr;
+
+  environment.systemPackages = with pkgs;
+    [
+      (pkgs.writeTextFile {
+        name = "startsway";
+        destination = "/bin/startsway";
+        executable = true;
+        text = ''
+          #! ${pkgs.bash}/bin/bash
+
+          # first import environment variables from the login manager
+          systemctl --user import-environment
+          # then start the service
+          exec systemctl --user start sway.service
+        '';
+      })
+    ];
+
+  systemd.user.targets.sway-session = {
+    description = "Sway compositor session";
+    documentation = [ "man:systemd.special(7)" ];
+    bindsTo = [ "graphical-session.target" ];
+    wants = [ "graphical-session-pre.target" ];
+    after = [ "graphical-session-pre.target" ];
+  };
+
+  systemd.user.services.sway = {
+    description = "Sway - Wayland window manager";
+    documentation = [ "man:sway(5)" ];
+    bindsTo = [ "graphical-session.target" ];
+    wants = [ "graphical-session-pre.target" ];
+    after = [ "graphical-session-pre.target" ];
+    # We explicitly unset PATH here, as we want it to be set by
+    # systemctl --user import-environment in startsway
+    environment.PATH = lib.mkForce null;
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = ''
+        ${pkgs.dbus}/bin/dbus-run-session ${pkgs.sway}/bin/sway --debug
+      '';
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
+
+  systemd.user.services.kanshi = {
+    description = "Kanshi output autoconfig ";
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    serviceConfig = {
+      # kanshi doesn't have an option to specifiy config file yet, so it looks
+      # at .config/kanshi/config
+      ExecStart = ''
+        ${pkgs.kanshi}/bin/kanshi
+      '';
+      RestartSec = 5;
+      Restart = "always";
+    };
+  };
 
   home-manager.users.emiller.xdg.configFile = {
     "sway" = {
