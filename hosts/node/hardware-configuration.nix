c4c7cfa49db3ed10b1fa9ae5467e73cf53bd035c
Edmund Miller <edmund.a.miller@protonmail.com>
2020-03-04 17:18:45 -0600

    fix(node): fix filesystems

--- hosts/node/hardware-configuration.nix
+++ hosts/node/hardware-configuration.nix
@@ -3,15 +3,20 @@
 
   boot.initrd.availableKernelModules =
     [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
 
   boot.loader.grub.device = "/dev/vda";
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
 
   fileSystems."/" = {
-    device = "/dev/vda1";
+    device = "/dev/disk/by-uuid/2ecca2ba-fc5d-4a48-815c-3882a68e12c0";
     fsType = "ext4";
   };
 
-  swapDevices = [{ device = "/dev/vda2"; }];
+  swapDevices =
+    [{ device = "/dev/disk/by-uuid/0b69e520-f338-4586-ae1d-1a89ce0126bb"; }];
+
 }
