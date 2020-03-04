c35186d841913b4bf6346a4e22fd4b825df49a5d
Edmund Miller <edmund.a.miller@protonmail.com>
2020-03-04 17:32:10 -0600

    feat(sway): add gestures

---
+++ modules/desktop/apps/libinput.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+
+{
+  my = {
+    packages = with pkgs; [ libinput libinput-gestures wmctrl ];
+
+    user.extraGroups = [ "input" ];
+
+    home.xdg.configFile = {
+      "libinput-gestures.conf" = {
+        source = <config/libinput-gestures.conf>;
+      };
+    };
+  };
+}
