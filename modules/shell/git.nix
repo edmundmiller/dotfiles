8f29d614e5f14381f3e54b441521059942c5fac0
Edmund Miller <edmund.a.miller@protonmail.com>
2020-04-24 10:55:13 -0500

    fix(git): add gitFull for git send-email

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -2,7 +2,12 @@
 
 {
   my = {
-    packages = with pkgs; [ git-lfs gitAndTools.hub gitAndTools.diff-so-fancy ];
+    packages = with pkgs; [
+      git-lfs
+      gitAndTools.hub
+      gitAndTools.diff-so-fancy
+      gitAndTools.gitFull
+    ];
     zsh.rc = lib.readFile <config/git/aliases.zsh>;
     # Do recursively, in case git stores files in this folder
     home.xdg.configFile = {
