88b2c43bf5306122df6523bd02037e869b97c9a7
Edmund Miller <edmund.a.miller@protonmail.com>
2020-05-08 12:56:54 -0500

    feat(firefox): add userChrome
    This is going to break and look off since it's from Henrik's theme
    
    fac2bb74f3028b2a67aa0bde0579218df2b2424f

--- modules/desktop/browsers/firefox.nix
+++ modules/desktop/browsers/firefox.nix
@@ -1,10 +1,15 @@
 # modules/browser/firefox.nix --- https://www.mozilla.org/en-US/firefox
+
 { config, options, lib, pkgs, ... }:
 with lib; {
   options.modules.desktop.browsers.firefox = {
     enable = mkOption {
       type = types.bool;
       default = false;
+    };
+    profileName = mkOption {
+      type = types.str;
+      default = config.my.username;
     };
   };
 
@@ -20,6 +25,23 @@ with lib; {
         categories = "Network";
       })
     ];
+
     my.env.XDG_DESKTOP_DIR = "$HOME"; # (try to) prevent ~/Desktop
+
+    # Use a stable profile name so we can target it in themes
+    my.home.home.file = let cfg = config.modules.desktop.browsers.firefox;
+    in {
+      ".mozilla/firefox/profiles.ini".text = ''
+        [Profile0]
+        Name=default
+        IsRelative=1
+        Path=${cfg.profileName}.default
+        Default=1
+
+        [General]
+        StartWithLastProfile=1
+        Version=2
+      '';
+    };
   };
 }
