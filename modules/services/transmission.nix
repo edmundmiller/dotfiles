0e9bfbacb76b53a07a5912c185f3963f8adf714f
Edmund Miller <edmund.a.miller@protonmail.com>
2020-05-08 13:47:16 -0500

    refactor(transmission): modularize

--- modules/services/transmission.nix
+++ modules/services/transmission.nix
@@ -1,24 +1,32 @@
-{ config, lib, pkgs, ... }:
-
-{
-  services.transmission = {
-    enable = true;
-    home = "/data/media/torrents";
-    user = config.my.username;
-    settings = {
-      incomplete-dir-enabled = true;
-      rpc-whitelist = "127.0.0.1,192.168.*.*";
-      rpc-host-whitelist = "*";
-      rpc-host-whitelist-enabled = true;
-      ratio-limit = 0;
-      ratio-limit-enabled = true;
+{ config, options, pkgs, lib, ... }:
+with lib; {
+  options.modules.services.transmission = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
     };
   };
 
-  networking.firewall = {
-    allowedTCPPorts = [ 51413 ];
-    allowedUDPPorts = [ 51413 ];
-  };
+  config = mkIf config.modules.services.transmission.enable {
+    services.transmission = {
+      enable = true;
+      home = "/data/media/torrents";
+      user = config.my.username;
+      settings = {
+        incomplete-dir-enabled = true;
+        rpc-whitelist = "127.0.0.1,192.168.*.*";
+        rpc-host-whitelist = "*";
+        rpc-host-whitelist-enabled = true;
+        ratio-limit = 0;
+        ratio-limit-enabled = true;
+      };
+    };
 
-  my.user.extraGroups = [ "transmission" ];
+    networking.firewall = {
+      allowedTCPPorts = [ 51413 ];
+      allowedUDPPorts = [ 51413 ];
+    };
+
+    my.user.extraGroups = [ "transmission" ];
+  };
 }
