937b9e0086921bdd39676629794ae7bf47d45f7f
Edmund Miller <edmund.a.miller@protonmail.com>
2020-05-08 13:27:01 -0500

    refactor(docker): modularize
    a41169c0737b7481a3e1d6790f1932f11f292d59

--- modules/services/docker.nix
+++ modules/services/docker.nix
@@ -1,23 +1,30 @@
-{ config, lib, pkgs, ... }:
-
-{
-  my = {
-    packages = with pkgs; [ docker docker-compose ];
-
-    env.DOCKER_CONFIG = "$XDG_CONFIG_HOME/docker";
-    env.MACHINE_STORAGE_PATH = "$XDG_DATA_HOME/docker/machine";
-
-    user.extraGroups = [ "docker" ];
-
-    zsh.rc = lib.readFile <config/docker/aliases.zsh>;
+{ config, options, pkgs, lib, ... }:
+with lib; {
+  options.modules.services.docker = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
   };
 
-  virtualisation = {
-    docker = {
-      enable = true;
-      autoPrune.enable = true;
-      enableOnBoot = false;
-      # listenOptions = [];
+  config = mkIf config.modules.services.docker.enable {
+    my = {
+      packages = with pkgs; [ docker docker-compose ];
+
+      env.DOCKER_CONFIG = "$XDG_CONFIG_HOME/docker";
+      env.MACHINE_STORAGE_PATH = "$XDG_DATA_HOME/docker/machine";
+
+      user.extraGroups = [ "docker" ];
+      zsh.rc = lib.readFile <config/docker/aliases.zsh>;
+    };
+
+    virtualisation = {
+      docker = {
+        enable = true;
+        autoPrune.enable = true;
+        enableOnBoot = false;
+        # listenOptions = [];
+      };
     };
   };
 }
