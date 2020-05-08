b9156b63b645a9ef0e85889611846b0ec51f71e9
Edmund Miller <edmund.a.miller@protonmail.com>
2020-05-08 13:10:57 -0500

    refactor(node): modularize
    d9a56ceb8787a9f53613e92fdd1fa1426ee61ce4

--- modules/dev/node.nix
+++ modules/dev/node.nix
@@ -1,31 +1,39 @@
 # modules/dev/node.nix --- https://nodejs.org/en/
 
-{ pkgs, ... }: {
-  imports = [ ./. ];
+{ config, options, lib, pkgs, ... }:
+with lib; {
+  options.modules.dev.node = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
 
-  my = {
-    packages = with pkgs; [
-      nodejs-12_x
-      python27 # For building node-gyp
-      solc
-      (yarn.override { nodejs = pkgs.nodejs-12_x; })
-    ];
+  config = mkIf config.modules.dev.node.enable {
+    my = {
+      packages = with pkgs; [
+        nodejs-12_x
+        python27 # For building node-gyp
+        solc
+        (yarn.override { nodejs = pkgs.nodejs-12_x; })
+      ];
 
-    env.NPM_CONFIG_USERCONFIG = "$XDG_CONFIG_HOME/npm/config";
-    env.NPM_CONFIG_CACHE = "$XDG_CACHE_HOME/npm";
-    env.NPM_CONFIG_TMP = "$XDG_RUNTIME_DIR/npm";
-    env.NPM_CONFIG_PREFIX = "$XDG_CACHE_HOME/npm";
-    env.NODE_REPL_HISTORY = "$XDG_CACHE_HOME/node/repl_history";
-    env.PATH = [ "$(yarn global bin)" ];
+      env.NPM_CONFIG_USERCONFIG = "$XDG_CONFIG_HOME/npm/config";
+      env.NPM_CONFIG_CACHE = "$XDG_CACHE_HOME/npm";
+      env.NPM_CONFIG_TMP = "$XDG_RUNTIME_DIR/npm";
+      env.NPM_CONFIG_PREFIX = "$XDG_CACHE_HOME/npm";
+      env.NODE_REPL_HISTORY = "$XDG_CACHE_HOME/node/repl_history";
+      env.PATH = [ "$(yarn global bin)" ];
 
-    # Run locally installed bin-script, e.g. n coffee file.coffee
-    alias.n = ''PATH="$(npm bin):$PATH"'';
-    alias.ya = "yarn";
-    alias.yart = "yarn dev || yarn start";
+      # Run locally installed bin-script, e.g. n coffee file.coffee
+      alias.n = ''PATH="$(npm bin):$PATH"'';
+      alias.ya = "yarn";
+      alias.yart = "yarn dev || yarn start";
 
-    home.xdg.configFile."npm/config".text = ''
-      cache=$XDG_CACHE_HOME/npm
-      prefix=$XDG_DATA_HOME/npm
-    '';
+      home.xdg.configFile."npm/config".text = ''
+        cache=$XDG_CACHE_HOME/npm
+        prefix=$XDG_DATA_HOME/npm
+      '';
+    };
   };
 }
