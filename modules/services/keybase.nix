fdc3e1ee319d51cc5d55f4938190be0c8096f65a
Edmund Miller <edmund.a.miller@protonmail.com>
2020-05-08 13:45:24 -0500

    refactor(keybase): modularize
    Until it dies from zoom

--- modules/services/keybase.nix
+++ modules/services/keybase.nix
@@ -1,14 +1,23 @@
 { config, lib, pkgs, ... }:
 
-{
-  services.kbfs = {
-    enable = true;
-    mountPoint = "%t/kbfs";
-    extraFlags = [ "-label %u" ];
+with lib; {
+  options.modules.services.keybase = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
   };
 
-  systemd.user.services.kbfs = {
-    environment = { KEYBASE_RUN_MODE = "prod"; };
-    serviceConfig.Slice = "keybase.slice";
+  config = mkIf config.modules.services.keybase.enable {
+    services.kbfs = {
+      enable = true;
+      mountPoint = "%t/kbfs";
+      extraFlags = [ "-label %u" ];
+    };
+
+    systemd.user.services.kbfs = {
+      environment = { KEYBASE_RUN_MODE = "prod"; };
+      serviceConfig.Slice = "keybase.slice";
+    };
   };
 }
