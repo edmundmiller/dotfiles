accd95243aeddf390df2ae80118ae3100b883196
Edmund Miller <edmund.a.miller@protonmail.com>
2020-05-08 19:04:57 -0500

    refactor(yubikey): modularize

--- modules/shell/yubikey.nix
+++ modules/shell/yubikey.nix
@@ -1,28 +1,38 @@
 { config, lib, pkgs, ... }:
 
-{
-  my = {
-    packages = with pkgs; [
-      gnupg
-      yubikey-manager-qt
-      yubikey-manager
-      yubikey-personalization
-    ];
-    init = ''
+with lib; {
+  options.modules.shell.yubikey = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  config = mkIf config.modules.shell.yubikey.enable {
+    my = {
+      packages = with pkgs; [
+        gnupg
+        yubikey-manager-qt
+        yubikey-manager
+        yubikey-personalization
+      ];
+    };
+
+    services.udev.packages = [ pkgs.yubikey-personalization pkgs.libu2f-host ];
+    services.pcscd.enable = true;
+
+    environment.shellInit = ''
       export GPG_TTY="$(tty)"
       gpg-connect-agent /bye
       export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
     '';
-  };
 
-  services.udev.packages = [ pkgs.yubikey-personalization pkgs.libu2f-host ];
-  services.pcscd.enable = true;
-
-  programs = {
-    ssh.startAgent = false;
-    gnupg.agent = {
-      enable = true;
-      enableSSHSupport = true;
+    programs = {
+      ssh.startAgent = false;
+      gnupg.agent = {
+        enable = true;
+        enableSSHSupport = true;
+      };
     };
   };
 }
