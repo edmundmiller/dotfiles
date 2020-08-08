b5dd152209edd4da33513a9784c0dfe1408a7786
Edmund Miller <edmund.a.miller@protonmail.com>
2020-08-08 18:49:44 -0500

    feat: Use cachix

--- default.nix
+++ default.nix
@@ -5,7 +5,11 @@ device: username:
   networking.hostName = lib.mkDefault device;
   my.username = username;
 
-  imports = [ ./modules "${./hosts}/${device}" ];
+  imports = [ ./modules "${./hosts}/${device}" ]
+    ++ (if builtins.pathExists (/etc/nixos/cachix.nix) then
+      [ /etc/nixos/cachix.nix ]
+    else
+      [ ]);
 
   ### NixOS
   nix.autoOptimiseStore = true;
@@ -32,11 +36,15 @@ device: username:
 
     gnumake # for our own makefile
     my.cached-nix-shell # for instant nix-shell scripts
+    cachix # less time buildin' mo time nixin'
+    (writeScriptBin "nix-shell" ''
+      #!${stdenv.shell}
+      NIX_PATH="nixpkgs-overlays=/etc/dotfiles/packages/default.nix:$NIX_PATH" ${nix}/bin/nix-shell "$@"
+    '')
+
   ];
   environment.shellAliases = {
     nix-env = "NIXPKGS_ALLOW_UNFREE=1 nix-env";
-    nix-shell = ''
-      NIX_PATH="nixpkgs-overlays=/etc/dotfiles/packages/default.nix:$NIX_PATH" nix-shell'';
     nsh = "nix-shell";
     nen = "nix-env";
     dots = "make -C ~/.dotfiles";
