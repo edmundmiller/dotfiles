8eaa9181223c4a3ab5fcd998f573bfd12b161336
Edmund Miller <edmund.a.miller@protonmail.com>
2020-08-08 18:54:56 -0500

    feat(stumpwm): yoink hlissner's config
    And bring up to speed with 20.03

--- modules/desktop/stumpwm.nix
+++ modules/desktop/stumpwm.nix
@@ -1,25 +1,33 @@
-{ config, lib, pkgs, ... }:
-
-{
+{ config, options, lib, pkgs, ... }:
+with lib; {
   imports = [ ./common.nix ];
 
-  environment.systemPackages = with pkgs; [ lightdm dunst libnotify ];
-
-  # unstable.services.picom.enable = true;
-  services = {
-    redshift.enable = true;
-    xserver = {
-      enable = true;
-      windowManager.default = "stumpwm";
-      windowManager.stumpwm.enable = true;
-      displayManager.lightdm.enable = true;
-      displayManager.lightdm.greeters.mini.enable = true;
+  options.modules.desktop.stumpwm = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
     };
   };
 
-  # link recursively so other modules can link files in their folders
-  my.home.xdg.configFile."stumpwm" = {
-    source = <config/stumpwm>;
-    recursive = true;
+  config = mkIf config.modules.desktop.stumpwm.enable {
+    environment.systemPackages = with pkgs; [ lightdm dunst libnotify ];
+
+    services = {
+      redshift.enable = true;
+      picom.enable = true;
+      xserver = {
+        enable = true;
+        windowManager.default = "none+stumpwm";
+        windowManager.stumpwm.enable = true;
+        displayManager.lightdm.enable = true;
+        displayManager.lightdm.greeters.mini.enable = true;
+      };
+    };
+
+    # link recursively so other modules can link files in their folders
+    my.home.xdg.configFile."stumpwm" = {
+      source = <config/stumpwm>;
+      recursive = true;
+    };
   };
 }
