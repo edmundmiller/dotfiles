b008a276b1014f64ba02b9caf49e76c989cef16d
Edmund Miller <edmund.a.miller@protonmail.com>
2020-09-16 14:18:33 -0500

    feat(nyxt): Yoink Taco's config

--- config/next/init.lisp
+++
@@ -1,7 +0,0 @@
-(in-package :next-user)
-
-(defclass my-buffer (buffer)
-  ((default-modes :initform
-     (cons 'vi-normal-mode (get-default 'buffer 'default-modes)))))
-
-(setf *buffer-class* 'my-buffer)
