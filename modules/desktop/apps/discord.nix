2a3e131f2235e840ce6c337c8f3613ce8b863380
Edmund Miller <edmund.a.miller@protonmail.com>
2020-10-16 09:12:04 -0500

    feat(discord): Add teams and slack

--- modules/desktop/apps/discord.nix
+++ modules/desktop/apps/discord.nix
@@ -4,15 +4,15 @@ with lib;
 with lib.my;
 let cfg = config.modules.desktop.apps.discord;
 in {
-  options.modules.desktop.apps.discord = {
-    enable = mkBoolOpt false;
-  };
+  options.modules.desktop.apps.discord = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
       # If not installed from unstable, Discord will sometimes soft-lock itself
       # on a "there's an update for discord" screen.
       unstable.discord
+      teams
+      slack
 
       # unstable.ripcord
     ];
