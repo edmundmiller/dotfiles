49dbfe5931017dda118261f322e6ac040bf7ded9
Edmund Miller <edmund.a.miller@protonmail.com>
2020-10-20 09:04:59 -0600

    style: Format xdg

--- modules/xdg.nix
+++ modules/xdg.nix
@@ -3,8 +3,7 @@
 # Set up and enforce XDG compliance. Other modules will take care of their own,
 # but this takes care of the general case.
 
-{ config, home-manager, ... }:
-{
+{ config, home-manager, ... }: {
   ### A tidy $HOME is a tidy mind
   home-manager.users.${config.user.name}.xdg.enable = true;
 
@@ -13,10 +12,10 @@
       # These are the defaults, and xdg.enable does set them, but due to load
       # order, they're not set before environment.variables are set, which could
       # cause race conditions.
-      XDG_CACHE_HOME  = "$HOME/.cache";
+      XDG_CACHE_HOME = "$HOME/.cache";
       XDG_CONFIG_HOME = "$HOME/.config";
-      XDG_DATA_HOME   = "$HOME/.local/share";
-      XDG_BIN_HOME    = "$HOME/.local/bin";
+      XDG_DATA_HOME = "$HOME/.local/share";
+      XDG_BIN_HOME = "$HOME/.local/bin";
     };
     variables = {
       # Conform more programs to XDG conventions. The rest are handled by their
