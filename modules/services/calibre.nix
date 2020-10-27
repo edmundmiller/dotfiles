8410077cc5311d5fd8ee39bc8417d4767d7a762a
Edmund Miller <edmund.a.miller@protonmail.com>
2020-10-27 11:44:56 -0600

    fix(calibre): Add libraryDir

--- modules/services/calibre.nix
+++ modules/services/calibre.nix
@@ -4,12 +4,11 @@ with lib;
 with lib.my;
 let cfg = config.modules.services.calibre;
 in {
-  options.modules.services.calibre = {
-    enable = mkBoolOpt false;
-  };
+  options.modules.services.calibre = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
     services.calibre-server.enable = true;
+    services.calibre-server.libraryDir = "/home/emiller/calibre";
 
     networking.firewall.allowedTCPPorts = [ 8080 ];
   };
