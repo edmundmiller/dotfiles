b2f193fb31fe8158453a103a588976876a20a62c
Edmund Miller <edmund.a.miller@protonmail.com>
2020-12-03 11:07:36 -0600

    fix(guix): Import the module correctly

--- modules/services/guix.nix
+++ modules/services/guix.nix
@@ -4,11 +4,12 @@ with lib;
 with lib.my;
 let cfg = config.modules.services.guix;
 in {
+  imports = [ inputs.guix.nixosModules.guix ];
   options.modules.services.guix = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
     nixpkgs.overlays = [ inputs.guix.overlay ];
 
-    environment.systemPackages = with pkgs; [ guix ];
+    services.guix.enable = true;
   };
 }
