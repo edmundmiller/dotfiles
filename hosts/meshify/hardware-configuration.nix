1e3c9717eaf8164612352a51cdb17a14aa0e5d3b
Edmund Miller <edmund.a.miller@protonmail.com>
2020-12-13 14:10:30 -0600

    fix(meshify): Follow nixos guide for hidpi

--- hosts/meshify/hardware-configuration.nix
+++ hosts/meshify/hardware-configuration.nix
@@ -16,9 +16,13 @@
   powerManagement.cpuFreqGovernor = "performance";
   hardware.cpu.amd.updateMicrocode = true;
 
-  ## Monitors
-  environment.variables.GDK_SCALE = "2";
-  environment.variables.GDK_DPI_SCALE = "0.5";
+  ## HiDPI Monitors
+  console.font = "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
+  environment.variables = {
+    GDK_SCALE = "2";
+    GDK_DPI_SCALE = "0.5";
+    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
+  };
 
   ## SSD
   fileSystems."/" = {
