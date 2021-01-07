25a03ce33c1a132734262a6e4dfdb4facaa0aa8a
Edmund Miller <edmund.a.miller@protonmail.com>
2021-01-07 13:21:28 -0600

    feat(zsh): Use overlay for nix-zsh-completions for flakes
    Until it's merged

---
+++ overlays/nix-zsh-completions.nix
@@ -0,0 +1,11 @@
+self: super:
+{
+    nix-zsh-completions = super.nix-zsh-completions.overrideAttrs (old: {
+    src = super.fetchFromGitHub {
+      owner = "Ma27";
+      repo = "nix-zsh-completions";
+      rev = "939c48c182e9d018eaea902b1ee9d00a415dba86";
+      sha256 = "sha256-3HVYez/wt7EP8+TlhTppm968Wl8x5dXuGU0P+8xNDpo=";
+    };
+  });
+}
