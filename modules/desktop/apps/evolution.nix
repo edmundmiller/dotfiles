241282c8370b7c90382412fb5836afa262c1e552
Edmund Miller <edmund.a.miller@protonmail.com>
2021-03-04 18:43:40 -0600

    fix(mail): Add gnome stuff

--- modules/desktop/apps/evolution.nix
+++ modules/desktop/apps/evolution.nix
@@ -7,7 +7,12 @@ in {
   options.modules.desktop.apps.evolution = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
+    # https://github.com/NixOS/nixpkgs/pull/103135
     programs.evolution.enable = true;
     programs.evolution.plugins = [ pkgs.evolution-ews ];
+    services.gnome3.evolution-data-server.enable = true;
+    services.gnome3.gnome-keyring.enable = true;
+    programs.seahorse.enable = true;
+    programs.dconf.enable = true;
   };
 }
