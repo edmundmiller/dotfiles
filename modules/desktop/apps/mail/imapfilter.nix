2be83b97dc872f7c5c88ff60b26d348881a06049
Edmund Miller <edmund.a.miller@protonmail.com>
2021-03-04 18:44:15 -0600

    fix(mail): Every module must be individually enabled

--- modules/desktop/apps/mail/imapfilter.nix
+++ modules/desktop/apps/mail/imapfilter.nix
@@ -1,21 +1,29 @@
 { config, lib, pkgs, ... }:
 
+with lib;
 with lib.my;
-let imapfilterconfig = "/home/emiller/.config/imapfilter/config.lua";
-in {
-  systemd.user.services.imapfilter = {
-    after = [ "mbsync.service" ];
-    wantedBy = [ "mbsync.service" ];
-    serviceConfig.ExecStart = [''
-      ${pkgs.imapfilter}/bin/imapfilter -c ${imapfilterconfig} -t /etc/ssl/certs/ca-bundle.crt -v
-    ''];
-    # serviceConfig.Restart = "always";
-  };
+let
+  imapfilterconfig = "/home/emiller/.config/imapfilter/config.lua";
 
-  home.configFile = {
-    "imapfilter" = {
-      source = "${configDir}/imapfilter";
-      recursive = true;
+  cfg = config.modules.desktop.apps.mail.imapfilter;
+in {
+  options.modules.desktop.apps.mail.imapfilter = { enable = mkBoolOpt false; };
+
+  config = mkIf cfg.enable {
+    systemd.user.services.imapfilter = {
+      after = [ "mbsync.service" ];
+      wantedBy = [ "mbsync.service" ];
+      serviceConfig.ExecStart = [''
+        ${pkgs.imapfilter}/bin/imapfilter -c ${imapfilterconfig} -t /etc/ssl/certs/ca-bundle.crt -v
+      ''];
+      # serviceConfig.Restart = "always";
+    };
+
+    home.configFile = {
+      "imapfilter" = {
+        source = "${configDir}/imapfilter";
+        recursive = true;
+      };
     };
   };
 }
