edcee19011976ead4e74e37180311a10ddca82b8
Edmund Miller <edmund.a.miller@protonmail.com>
2021-03-22 13:05:06 -0500

    feat(code): Add gitlens and nextflow

--- modules/editors/code.nix
+++ modules/editors/code.nix
@@ -6,15 +6,28 @@ let
   cfg = config.modules.editors.code;
   extensions = (with pkgs.vscode-extensions; [
     bbenoist.Nix
+    eamodio.gitlens
     vscodevim.vim
     ms-python.python
     ms-azuretools.vscode-docker
     ms-vsliveshare.vsliveshare
     ms-vscode-remote.remote-ssh
     ms-kubernetes-tools.vscode-kubernetes-tools
-  ]);
-  # ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-  # }];
+    redhat.vscode-yaml
+  ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    {
+      name = "nextflow";
+      publisher = "nextflow";
+      version = "0.3.0";
+      sha256 = "sha256-XLyGG8KoXaMhtbbY3V1r63B/4WFOV2horp184hV74dI=";
+    }
+    {
+      name = "vscode-groovy-lint";
+      publisher = "NicolasVuillamy";
+      version = "1.4.0";
+      sha256 = "sha256-Uz6U18Kr+tyarOv9b/GzD9g9LXRYyZ+jxagMVkHstZM=";
+    }
+  ];
   vscode-with-extensions =
     pkgs.vscode-with-extensions.override { vscodeExtensions = extensions; };
 in {
