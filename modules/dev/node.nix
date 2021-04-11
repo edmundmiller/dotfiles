042300db52d4db6bb6844fc38bb918d8feb6ba72
Edmund Miller <edmund.a.miller@protonmail.com>
2021-04-11 10:46:19 -0500

    Resolve paths to yarn/npm

--- modules/dev/node.nix
+++ modules/dev/node.nix
@@ -15,15 +15,15 @@ in {
     user.packages = with pkgs; [ nodejs yarn ];
 
     env.NPM_CONFIG_USERCONFIG = "$XDG_CONFIG_HOME/npm/config";
-    env.NPM_CONFIG_CACHE = "$XDG_CACHE_HOME/npm";
-    env.NPM_CONFIG_TMP = "$XDG_RUNTIME_DIR/npm";
-    env.NPM_CONFIG_PREFIX = "$XDG_CACHE_HOME/npm";
-    env.NODE_REPL_HISTORY = "$XDG_CACHE_HOME/node/repl_history";
-    env.PATH = [ "$(yarn global bin)" ];
+    env.NPM_CONFIG_CACHE      = "$XDG_CACHE_HOME/npm";
+    env.NPM_CONFIG_TMP        = "$XDG_RUNTIME_DIR/npm";
+    env.NPM_CONFIG_PREFIX     = "$XDG_CACHE_HOME/npm";
+    env.NODE_REPL_HISTORY     = "$XDG_CACHE_HOME/node/repl_history";
+    env.PATH = [ "$(${pkgs.yarn}/bin/yarn global bin)" ];
 
     # Run locally installed bin-script, e.g. n coffee file.coffee
     environment.shellAliases = {
-      n = ''PATH="$(npm bin):$PATH"'';
+      n  = "PATH=\"$(${pkgs.nodejs}/bin/npm bin):$PATH\"";
       ya = "yarn";
       yart = "yarn dev || yarn start";
     };
