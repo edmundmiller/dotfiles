4012b843d9d97b4d0022481b68db8eaa983ece45
Edmund Miller <edmund.a.miller@protonmail.com>
2021-04-28 20:48:47 -0500

    fix(meshify): Add 4k font settings

--- hosts/meshify/hardware-configuration.nix
+++ hosts/meshify/hardware-configuration.nix
@@ -21,7 +21,7 @@
   environment.variables = {
     GDK_SCALE = "2";
     GDK_DPI_SCALE = "0.5";
-    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
+    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2 -Dawt.useSystemAAFontSettings=lcd";
   };
 
   ## SSD
