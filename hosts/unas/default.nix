11f86b91ef596ef49bd6b05ccc32e312dbcdf5d0
Edmund Miller <edmund.a.miller@protonmail.com>
2021-05-16 22:56:00 -0500

    feat(unas): Add zfs backup to hdds

--- hosts/unas/default.nix
+++ hosts/unas/default.nix
@@ -1,6 +1,6 @@
 # No u nas
 
-{ ... }: {
+{ lib, ... }: {
   imports = [ ../home.nix ./hardware-configuration.nix ./nas.nix ];
 
   modules = {
@@ -22,7 +22,18 @@
 
   time.timeZone = "America/Chicago";
 
-  users.users.moni = {
-      isNormalUser = true;
+  users.users.moni = { isNormalUser = true; };
+
+  systemd.services.znapzend.serviceConfig.User = lib.mkForce "emiller";
+  services.znapzend = {
+    enable = true;
+    autoCreation = true;
+    zetup = {
+      "tank/user/home" = {
+        plan = "1d=>1h,1m=>1d,1y=>1m";
+        recursive = true;
+        destinations.local = { dataset = "datatank/backup"; };
+      };
+    };
   };
 }
