d2ff7ec23331306662e12ff2a785dc2e2fe5839b
Edmund Miller <edmund.a.miller@protonmail.com>
2021-05-25 15:28:16 -0500

    fix(unas): Remove user from backup pings

--- hosts/unas/backups.nix
+++ hosts/unas/backups.nix
@@ -25,12 +25,12 @@
     };
   };
 
-  systemd.user.services.restic-backups-local-sync-backup.postStop =
+  systemd.services.restic-backups-local-sync-backup.postStop =
     "${pkgs.curl}/bin/curl -m 10 --retry 5 https://hc-ping.com/09ae3517-b710-4a3e-ae68-16fe45f3697f";
 
-  systemd.user.services.restic-backups-local-archive-backup.postStop =
+  systemd.services.restic-backups-local-archive-backup.postStop =
     "${pkgs.curl}/bin/curl -m 10 --retry 5 https://hc-ping.com/773d889a-0ee4-42f7-98c8-7a106874a116";
 
-  systemd.user.services.restic-backups-gdrive-sync-backup.postStop =
+  systemd.services.restic-backups-gdrive-sync-backup.postStop =
     "${pkgs.curl}/bin/curl -m 10 --retry 5 https://hc-ping.com/bfae5213-4fd4-4700-86e5-3ad6f9a7f62e";
 }
