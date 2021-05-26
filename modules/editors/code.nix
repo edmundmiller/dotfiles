273c934af5f26dd4e3316f9c7e2298fc8be9fc2b
Edmund Miller <edmund.a.miller@protonmail.com>
2021-05-26 10:54:55 -0500

    feat: Add more plugins for vscode

--- modules/editors/code.nix
+++ modules/editors/code.nix
@@ -14,20 +14,21 @@ let
     ms-vscode-remote.remote-ssh
     ms-kubernetes-tools.vscode-kubernetes-tools
     redhat.vscode-yaml
-  ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-    {
-      name = "nextflow";
-      publisher = "nextflow";
-      version = "0.3.0";
-      sha256 = "sha256-XLyGG8KoXaMhtbbY3V1r63B/4WFOV2horp184hV74dI=";
-    }
-    {
-      name = "vscode-groovy-lint";
-      publisher = "NicolasVuillamy";
-      version = "1.4.0";
-      sha256 = "sha256-Uz6U18Kr+tyarOv9b/GzD9g9LXRYyZ+jxagMVkHstZM=";
-    }
-  ];
+    mikestead.dotenv
+    CoenraadS.bracket-pair-colorizer
+    timonwong.shellcheck
+    esbenp.prettier-vscode
+    file-icons.file-icons
+    mskelton.one-dark-theme
+    donjayamanne.githistory
+    editorconfig.editorconfig
+    yzhang.markdown-all-in-one
+  ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+    name = "nextflow";
+    publisher = "nextflow";
+    version = "0.3.0";
+    sha256 = "sha256-XLyGG8KoXaMhtbbY3V1r63B/4WFOV2horp184hV74dI=";
+  }];
   vscode-with-extensions =
     pkgs.vscode-with-extensions.override { vscodeExtensions = extensions; };
 in {
@@ -37,5 +38,8 @@ in {
 
     user.packages = with pkgs; [ editorconfig-core-c vscode-with-extensions ];
 
+    # For Liveshare
+    services.gnome3.gnome-keyring.enable = true;
+    programs.seahorse.enable = true;
   };
 }
