a9a9666a4330f537968e2c0664aeccecaf0fd41d
Edmund Miller <edmund.a.miller@protonmail.com>
2021-08-24 21:29:35 -0500

    feat(tailscale): Add initial module

---
+++ modules/services/tailscale.nix
@@ -0,0 +1,17 @@
+{ config, options, pkgs, lib, my, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.services.tailscale;
+in {
+  options.modules.services.tailscale = { enable = mkBoolOpt false; };
+
+  config = mkIf cfg.enable {
+    services.tailscale.enable = true;
+
+    networking.firewall = {
+      allowedTCPPorts = [ 41641 ];
+      allowedUDPPorts = [ 41641 ];
+    };
+  };
+}
