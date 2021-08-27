9d2ef6b5583d89c43dea820c4079faadfd307223
Edmund Miller <edmund.a.miller@protonmail.com>
2021-08-26 22:34:13 -0500

    feat(k3s): Update config
    Enable on unas as well.
    This hosts jellyfin now.

--- modules/services/k3s.nix
+++ modules/services/k3s.nix
@@ -4,16 +4,33 @@ with lib;
 with lib.my;
 let cfg = config.modules.services.k3s;
 in {
-  imports = [ inputs.k3s-flake.nixosModules.k3s-flake ];
   options.modules.services.k3s = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
-    services.k3s-flake= {
+    services.k3s = {
       enable = true;
+      package = pkgs.unstable.k3s;
       role = "server";
-      extraFlags = "--no-deploy servicelb --no-deploy traefik";
       docker = true;
+      extraFlags = "--no-deploy servicelb --no-deploy traefik";
+      # serverAddr = "https://192.168.1.121:6443";
     };
-    networking.firewall.allowedTCPPorts = [ 6443 ];
+
+    networking.firewall.allowedTCPPorts = [
+      80
+      443 # nginx
+      6443 # k8s
+    ];
+    networking.firewall.allowedUDPPorts = [ 8472 ];
+    networking.firewall.extraCommands = ''
+      iptables -I INPUT 3 -s 10.42.0.0/16 -j ACCEPT
+      iptables -I INPUT 3 -d 10.42.0.0/16 -j ACCEPT
+    '';
+
+    # Ceph
+    boot.kernelModules = [ "ceph" "rbd" ];
+    environment.systemPackages = [ pkgs.lvm2 ];
+
+    user.extraGroups = [ "568" ];
   };
 }
