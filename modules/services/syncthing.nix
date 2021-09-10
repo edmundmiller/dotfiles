4d5176dc791a31dd331709ae397c9575c714051a
Edmund Miller <edmund.a.miller@protonmail.com>
2021-09-10 18:28:29 -0500

    refactor(syncthing): Remove declarative scope

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -17,66 +17,66 @@ in {
       package = pkgs.unstable.syncthing;
       configDir = "/home/${config.user.name}/.config/syncthing";
       dataDir = "/home/${config.user.name}/.local/share/syncthing";
-      declarative = {
-        devices = {
-          omen.id =
-            "HJV4PMT-ROCGMFH-HXINT57-CKTYJSK-7QE7DH2-CEAYVJ6-NIMMDKA-MEOVNAB";
-          oneplus.id =
-            "UVAV55V-PAYJHDA-RDLKIGA-PTIKJAZ-7BWJHDW-TMKLGX7-RPYNTFR-LRNKKQC";
-          meshify.id =
-            "BNE2NYW-PLPCOLI-Z2T6Y5X-YICNTZO-RLFCSMN-VJ4QFPD-XJILLSQ-34XERAQ";
-          pbp.id =
-            "PQNQ54X-4WQ32AV-2TWQI4N-KF64EKW-FRFEVAL-WCONWOY-V4BK4PB-JYQGZQO"; # Set up externally
-          unas.id =
-            "QRDMQKL-RX4PO5I-2VM3SBA-42G3PVX-ZGGRDYU-P3C3AFN-OKLOVK4-BXRJAAN";
+      devices = {
+        omen.id =
+          "HJV4PMT-ROCGMFH-HXINT57-CKTYJSK-7QE7DH2-CEAYVJ6-NIMMDKA-MEOVNAB";
+        oneplus.id =
+          "UVAV55V-PAYJHDA-RDLKIGA-PTIKJAZ-7BWJHDW-TMKLGX7-RPYNTFR-LRNKKQC";
+        meshify.id =
+          "BNE2NYW-PLPCOLI-Z2T6Y5X-YICNTZO-RLFCSMN-VJ4QFPD-XJILLSQ-34XERAQ";
+        pbp.id =
+          "PQNQ54X-4WQ32AV-2TWQI4N-KF64EKW-FRFEVAL-WCONWOY-V4BK4PB-JYQGZQO"; # Set up externally
+        unas.id =
+          "QRDMQKL-RX4PO5I-2VM3SBA-42G3PVX-ZGGRDYU-P3C3AFN-OKLOVK4-BXRJAAN";
+        xps.id =
+          "VEXEG7A-7KC2HAK-B6WDY6C-MPHLFUR-MB2OHPT-RM4HTJF-IE6D5SH-UJQVCQH"; # Set up externally
+      };
+      folders = let
+        deviceEnabled = devices: lib.elem config.networking.hostName devices;
+        deviceType = devices:
+          if deviceEnabled devices then "sendreceive" else "receiveonly";
+      in {
+        archive = rec {
+          devices = [ "meshify" "omen" "unas" ];
+          path = "/home/${config.user.name}/archive";
+          watch = false;
+          rescanInterval = 3600 * 6;
+          type = deviceType [ "meshify" ];
+          enable = deviceEnabled devices;
+          versioning.type = "simple";
+          versioning.params.keep = "2";
         };
-        folders = let
-          deviceEnabled = devices: lib.elem config.networking.hostName devices;
-          deviceType = devices:
-            if deviceEnabled devices then "sendreceive" else "receiveonly";
-        in {
-          archive = rec {
-            devices = [ "meshify" "omen" "unas" ];
-            path = "/home/${config.user.name}/archive";
-            watch = false;
-            rescanInterval = 3600 * 6;
-            type = deviceType [ "meshify" ];
-            enable = deviceEnabled devices;
-            versioning.type = "simple";
-            versioning.params.keep = "2";
-          };
-          elfeed = rec {
-            devices = [ "meshify" "omen" "unas" "pbp" ];
-            path = "/home/${config.user.name}/.config/emacs/.local/elfeed";
-            watch = false;
-            rescanInterval = 3600 * 6;
-            type = deviceType [ "meshify" "omen" "pbp" ];
-            enable = deviceEnabled devices;
-          };
-          sync = rec {
-            devices = [ "omen" "oneplus" "meshify" "unas" "pbp" ];
-            path = "/home/${config.user.name}/sync";
-            watch = true;
-            rescanInterval = 3600 * 6;
-            type = deviceType [ "meshify" "omen" "pbp" ];
-            enable = deviceEnabled devices;
-          };
-          src = rec {
-            devices = [ "omen" "meshify" "unas" ];
-            path = "/home/${config.user.name}/src";
-            watch = false;
-            rescanInterval = 3600 * 2;
-            type = deviceType [ "meshify" "omen" ];
-            enable = deviceEnabled devices;
-          };
-          secrets = rec {
-            devices = [ "omen" "oneplus" "meshify" "unas" "pbp" ];
-            path = "/home/${config.user.name}/.secrets";
-            watch = true;
-            rescanInterval = 3600;
-            type = deviceType [ "meshify" "omen" "pbp" ];
-            enable = deviceEnabled devices;
-          };
+        elfeed = rec {
+          devices = [ "meshify" "omen" "unas" "pbp" ];
+          path = "/home/${config.user.name}/.config/emacs/.local/elfeed";
+          watch = false;
+          rescanInterval = 3600 * 6;
+          type = deviceType [ "meshify" "omen" "pbp" ];
+          enable = deviceEnabled devices;
+        };
+        sync = rec {
+          devices = [ "omen" "oneplus" "meshify" "unas" "pbp" "xps" ];
+          path = "/home/${config.user.name}/sync";
+          watch = true;
+          rescanInterval = 3600 * 6;
+          type = deviceType [ "meshify" "omen" "pbp" "xps" ];
+          enable = deviceEnabled devices;
+        };
+        src = rec {
+          devices = [ "omen" "meshify" "unas" ];
+          path = "/home/${config.user.name}/src";
+          watch = false;
+          rescanInterval = 3600 * 2;
+          type = deviceType [ "meshify" "omen" ];
+          enable = deviceEnabled devices;
+        };
+        secrets = rec {
+          devices = [ "omen" "oneplus" "meshify" "unas" "pbp" ];
+          path = "/home/${config.user.name}/.secrets";
+          watch = true;
+          rescanInterval = 3600;
+          type = deviceType [ "meshify" "omen" "pbp" ];
+          enable = deviceEnabled devices;
         };
       };
     };
