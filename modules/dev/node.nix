8d5f47a1d1a20060bbe4aa80e3685e5a272f06a5
Edmund Miller <edmund.a.miller@protonmail.com>
2021-09-24 19:50:39 -0500

    chore(node): Yoink hlissner's updates

--- modules/dev/node.nix
+++ modules/dev/node.nix
@@ -7,30 +7,39 @@
 
 with lib;
 with lib.my;
-let cfg = config.modules.dev.node;
+let
+  cfg = config.modules.dev.node;
+  node = pkgs.nodejs_latest;
 in {
-  options.modules.dev.node = { enable = mkBoolOpt false; };
-
-  config = mkIf cfg.enable {
-    user.packages = with pkgs; [ nodejs yarn ];
-
-    env.NPM_CONFIG_USERCONFIG = "$XDG_CONFIG_HOME/npm/config";
-    env.NPM_CONFIG_CACHE      = "$XDG_CACHE_HOME/npm";
-    env.NPM_CONFIG_TMP        = "$XDG_RUNTIME_DIR/npm";
-    env.NPM_CONFIG_PREFIX     = "$XDG_CACHE_HOME/npm";
-    env.NODE_REPL_HISTORY     = "$XDG_CACHE_HOME/node/repl_history";
-    env.PATH = [ "$(${pkgs.yarn}/bin/yarn global bin)" ];
-
-    # Run locally installed bin-script, e.g. n coffee file.coffee
-    environment.shellAliases = {
-      n  = "PATH=\"$(${pkgs.nodejs}/bin/npm bin):$PATH\"";
-      ya = "yarn";
-      yart = "yarn dev || yarn start";
-    };
-
-    home.configFile."npm/config".text = ''
-      cache=$XDG_CACHE_HOME/npm
-      prefix=$XDG_DATA_HOME/npm
-    '';
+  options.modules.dev.node = {
+    enable = mkBoolOpt false;
+    enableGlobally = mkBoolOpt false;
   };
+
+  config = mkIf cfg.enable (mkMerge [
+    (mkIf cfg.enableGlobally {
+      user.packages = with pkgs; [ node yarn ];
+
+      # Run locally installed bin-script, e.g. n coffee file.coffee
+      environment.shellAliases = {
+        n = ''PATH="$(${node}/bin/npm bin):$PATH"'';
+        ya = "yarn";
+      };
+
+      env.PATH = [ "$(${pkgs.yarn}/bin/yarn global bin)" ];
+    })
+
+    {
+      env.NPM_CONFIG_USERCONFIG = "$XDG_CONFIG_HOME/npm/config";
+      env.NPM_CONFIG_CACHE = "$XDG_CACHE_HOME/npm";
+      env.NPM_CONFIG_TMP = "$XDG_RUNTIME_DIR/npm";
+      env.NPM_CONFIG_PREFIX = "$XDG_CACHE_HOME/npm";
+      env.NODE_REPL_HISTORY = "$XDG_CACHE_HOME/node/repl_history";
+
+      home.configFile."npm/config".text = ''
+        cache=$XDG_CACHE_HOME/npm
+        prefix=$XDG_DATA_HOME/npm
+      '';
+    }
+  ]);
 }
