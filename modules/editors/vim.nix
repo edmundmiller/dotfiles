c58ca5b3d700cbb4d6d1c728fd3f3eefaef96dbe
Edmund Miller <edmund.a.miller@protonmail.com>
2021-12-12 16:17:57 -0600

    fix(vim): Add ctags

--- modules/editors/vim.nix
+++ modules/editors/vim.nix
@@ -9,7 +9,7 @@ in {
   config = mkIf cfg.enable {
     nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlay ];
 
-    user.packages = with pkgs; [ editorconfig-core-c neovim-nightly ];
+    user.packages = with pkgs; [ editorconfig-core-c neovim-nightly ctags ];
 
     # env.VIMINIT = "let \\$MYVIMRC='\\$XDG_CONFIG_HOME/nvim/init.vim' | source \\$MYVIMRC";
 
