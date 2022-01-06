6c01099b52811a8edcc0ac5e0bcf2f38c998dfdd
Edmund Miller <edmund.a.miller@protonmail.com>
2022-01-05 22:20:16 -0600

    style: Run nixfmt

--- modules/shell/bitwarden.nix
+++ modules/shell/bitwarden.nix
@@ -6,19 +6,19 @@ let cfg = config.modules.shell.bitwarden;
 in {
   options.modules.shell.bitwarden = with types; {
     enable = mkBoolOpt false;
-    config = mkOpt attrs {};
+    config = mkOpt attrs { };
   };
 
   config = mkIf cfg.enable {
-    user.packages = with pkgs; [
-      bitwarden-cli
-    ];
+    user.packages = with pkgs; [ bitwarden-cli ];
 
-    modules.shell.zsh.rcInit = "_cache bw completion --shell zsh; compdef _bw bw;";
+    modules.shell.zsh.rcInit =
+      "_cache bw completion --shell zsh; compdef _bw bw;";
 
-    system.userActivationScripts = mkIf (cfg.config != {}) {
+    system.userActivationScripts = mkIf (cfg.config != { }) {
       initBitwarden = ''
-        ${concatStringsSep "\n" (mapAttrsToList (n: v: "bw config ${n} ${v}") cfg.config)}
+        ${concatStringsSep "\n"
+        (mapAttrsToList (n: v: "bw config ${n} ${v}") cfg.config)}
       '';
     };
   };
