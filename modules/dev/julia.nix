4ef7402a16bb864fc034349852ad084d0726a6af
Edmund Miller <edmund.a.miller@protonmail.com>
2022-02-22 10:54:08 -0600

    feat(julia): Initialize module

---
+++ modules/dev/julia.nix
@@ -0,0 +1,15 @@
+# modules/dev/julia.nix --- Julia
+
+{ config, options, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.dev.julia;
+in
+{
+  options.modules.dev.julia = { enable = mkBoolOpt false; };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [ julia-bin ];
+  };
+}
