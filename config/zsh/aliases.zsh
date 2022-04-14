c53cd5bd81ee50ff6e0322732b2538c10bb896cd
Edmund Miller <edmund.a.miller@protonmail.com>
2022-04-13 20:11:43 -0500

    feat(zsh): Yoink all of Henrik's updates

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -16,7 +16,6 @@ alias path='echo -e ${PATH//:/\\n}'
 alias ports='netstat -tulanp'
 
 alias mk=make
-alias rmirror='rsync -rtvu --delete'
 alias gurl='curl --compressed'
 
 alias shutdown='sudo shutdown'
@@ -53,24 +52,35 @@ alias ssc='sudo systemctl'
 
 if command -v exa >/dev/null; then
   alias exa="exa --group-directories-first --git";
-  alias l="exa -1";
-  alias ll="exa -lg";
-  alias la="LC_COLLATE=C exa -la";
+  alias l="exa -blF";
+  alias ll="exa -abghilmu";
+  alias llm='ll --sort=modified'
+  alias la="LC_COLLATE=C exa -ablF";
+  alias tree='exa --tree'
+fi
+
+if (( $+commands[fasd] )); then
+  # fuzzy completion with 'z' when called without args
+  unalias z 2>/dev/null
+  function z {
+    [ $# -gt 0 ] && _z "$*" && return
+    cd "$(_z -l 2>&1 | fzf --height 40% --nth 2.. --reverse --inline-info +s --tac --query "${*##-* }" | sed 's/^[0-9,.]* *//')"
+  }
 fi
 
 autoload -U zmv
 
-take() {
+function take() {
 	mkdir "$1" && cd "$1"
 }
 compdef take=mkdir
 
-zman() {
+function zman() {
 	PAGER="less -g -I -s '+/^       "$1"'" man zshall
 }
 
 # Create a reminder with human-readable durations, e.g. 15m, 1h, 40s, etc
-r() {
+function r() {
 	local time=$1
 	shift
 	sched "$time" "notify-send --urgency=critical 'Reminder' '$@'; ding"
--- config/zsh/completion.zsh
+++ config/zsh/completion.zsh
@@ -1,114 +1,92 @@
 fpath+=( $ZDOTDIR/completions )
 
-## Options
+# Don't offer history completion; we have fzf, C-r, and
+# zsh-history-substring-search for that.
+ZSH_AUTOSUGGEST_STRATEGY=(completion)
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=30
+
+# Expand partial paths, e.g. cd f/b/z == cd foo/bar/baz (assuming no ambiguity)
+zstyle ':completion:*:paths' path-completion yes
+
+# Fix slow one-by-one character pasting when bracketed-paste-magic is on. See
+# zsh-users/zsh-syntax-highlighting#295
+zstyle ':bracketed-paste-magic' active-widgets '.self-*'
+
+# Options
 setopt COMPLETE_IN_WORD    # Complete from both ends of a word.
 setopt PATH_DIRS           # Perform path search even on command names with slashes.
 setopt AUTO_MENU           # Show completion menu on a successive tab press.
 setopt AUTO_LIST           # Automatically list choices on ambiguous completion.
-setopt AUTO_PARAM_SLASH    # If completed parameter is a directory, add a trailing slash.
-setopt AUTO_PARAM_KEYS
-setopt FLOW_CONTROL        # Disable start/stop characters in shell editor.
+# setopt AUTO_PARAM_SLASH    # If completed parameter is a directory, add a trailing slash.
+# setopt AUTO_PARAM_KEYS
+# setopt FLOW_CONTROL        # Disable start/stop characters in shell editor.
 unsetopt MENU_COMPLETE     # Do not autoselect the first completion entry.
 unsetopt COMPLETE_ALIASES  # Completion for aliases
-unsetopt ALWAYS_TO_END     # Move cursor to the end of a completed word.
+# unsetopt ALWAYS_TO_END     # Move cursor to the end of a completed word.
 unsetopt CASE_GLOB
-
-# Use caching to make completion for commands such as dpkg and apt usable.
-zstyle ':completion::complete:*' use-cache on
-zstyle ':completion::complete:*' cache-path "$ZSH_CACHE/zcompcache"
-
-# Case-insensitive (all), partial-word, and then substring completion.
-zstyle ':completion:*' matcher-list '' \
-       'm:{a-z\-}={A-Z\_}' \
-       'r:[^[:alpha:]]||[[:alpha:]]=** r:|=* m:{a-z\-}={A-Z\_}' \
-       'r:[[:ascii:]]||[[:ascii:]]=** r:|=* m:{a-z\-}={A-Z\_}'
-
-# Group matches and describe.
-zstyle ':completion:*:*:*:*:*' menu select
-zstyle ':completion:*:matches' group 'yes'
-zstyle ':completion:*:options' description 'yes'
-zstyle ':completion:*:options' auto-description '%d'
-zstyle ':completion:*:corrections' format ' %F{green}-- %d (errors: %e) --%f'
-zstyle ':completion:*:descriptions' format ' %F{yellow}-- %d --%f'
-zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
-zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
-zstyle ':completion:*:default' list-prompt '%S%M matches%s'
-zstyle ':completion:*' format ' %F{yellow}-- %d --%f'
-zstyle ':completion:*' group-name ''
-zstyle ':completion:*' verbose yes
 
 # Fuzzy match mistyped completions.
 zstyle ':completion:*' completer _complete _list _match _approximate
 zstyle ':completion:*:match:*' original only
 zstyle ':completion:*:approximate:*' max-errors 1 numeric
-
 # Increase the number of errors based on the length of the typed word.
 zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)'
-
 # Don't complete unavailable commands.
-zstyle ':completion:*:functions' ignored-patterns '(_*|pre(cmd|exec))'
-
-# Array completion element sorting.
-zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
-
-# Directories
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*:functions' ignored-patterns '(_*|.*|pre(cmd|exec))'
+# Group matches and describe.
+zstyle ':completion:*:corrections' format '%B%F{green}%d (errors: %e)%f%b'
+zstyle ':completion:*:messages' format '%B%F{yellow}%d%f%b'
+zstyle ':completion:*:warnings' format '%B%F{red}No such %d%f%b'
+zstyle ':completion:*:errors' format '%B%F{red}No such %d%f%b'
+zstyle ':completion:*:descriptions' format $'%{\e[35;1m%}%d%{\e[0m%}'
+zstyle ':completion:*:default' list-prompt '%S%M matches%s'
+# Omit parent and current directories from completion results when they are
+# already named in the input.
 zstyle ':completion:*:*:cd:*' ignore-parents parent pwd
-zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
-zstyle ':completion:*:*:cd:*:directory-stack' menu yes select
-zstyle ':completion:*:-tilde-:*' group-order 'named-directories' 'path-directories' 'users' 'expand'
+# Merge multiple, consecutive slashes in paths
 zstyle ':completion:*' squeeze-slashes true
-zstyle ':completion:*' special-dirs true
-
-# History
+# Don't wrap around when navigating to either end of history
 zstyle ':completion:*:history-words' stop yes
 zstyle ':completion:*:history-words' remove-all-dups yes
 zstyle ':completion:*:history-words' list false
 zstyle ':completion:*:history-words' menu yes
-
-# Environmental Variables
+# Exclude internal/fake envvars
 zstyle ':completion::*:(-command-|export):*' fake-parameters ${${${_comps[(I)-value-*]#*,}%%,*}:#-*-}
-
-# Populate hostname completion.
+# Sory array completion candidates
+zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
+# Complete hostnames from ssh files too
 zstyle -e ':completion:*:hosts' hosts 'reply=(
   ${=${=${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) 2>/dev/null)"}%%[#| ]*}//\]:[0-9]*/ }//,/ }//\[/ }
   ${=${${${${(@M)${(f)"$(cat ~/.ssh/config 2>/dev/null)"}:#Host *}#Host }:#*\**}:#*\?*}}
 )'
-
-# Don't complete uninteresting users...
-zstyle ':completion:*:*:*:users' ignored-patterns \
+# Don't complete uninteresting users
+zstyle ':completion:*:users' ignored-patterns \
   adm amanda apache avahi beaglidx bin cacti canna clamav daemon \
   dbus distcache dovecot fax ftp games gdm gkrellmd gopher \
   hacluster haldaemon halt hsqldb ident junkbust ldap lp mail \
   mailman mailnull mldonkey mysql nagios \
-  named netdump news nfsnobody nobody nscd ntp nut nx openvpn \
+  named netdump news nfsnobody nobody 'nixbld*' nscd ntp nut nx openvpn \
   operator pcap postfix postgres privoxy pulse pvm quagga radvd \
-  rpc rpcuser rpm shutdown squid sshd sync uucp vcsa xfs '_*'
-
+  rpc rpcuser rpm shutdown squid sshd sync 'systemd-*' uucp vcsa xfs '_*'
 # ... unless we really want to.
 zstyle '*' single-ignored show
-
 # Ignore multiple entries.
 zstyle ':completion:*:(rm|kill|diff):*' ignore-line other
 zstyle ':completion:*:rm:*' file-patterns '*:all-files'
-
-# Kill
+# PID completion for kill
 zstyle ':completion:*:*:*:*:processes' command 'ps -u $LOGNAME -o pid,user,command -w'
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;36=0=01'
 zstyle ':completion:*:*:kill:*' menu yes select
 zstyle ':completion:*:*:kill:*' force-list always
 zstyle ':completion:*:*:kill:*' insert-ids single
-
 # Man
 zstyle ':completion:*:manuals' separate-sections true
 zstyle ':completion:*:manuals.(^1*)' insert-sections true
-
 # Media Players
 zstyle ':completion:*:*:mpg123:*' file-patterns '*.(mp3|MP3):mp3\ files *(-/):directories'
 zstyle ':completion:*:*:mpg321:*' file-patterns '*.(mp3|MP3):mp3\ files *(-/):directories'
 zstyle ':completion:*:*:ogg123:*' file-patterns '*.(ogg|OGG|flac):ogg\ files *(-/):directories'
 zstyle ':completion:*:*:mocp:*' file-patterns '*.(wav|WAV|mp3|MP3|ogg|OGG|flac):ogg\ files *(-/):directories'
-
 # SSH/SCP/RSYNC
 zstyle ':completion:*:(scp|rsync):*' tag-order 'hosts:-host:host hosts:-domain:domain hosts:-ipaddr:ip\ address *'
 zstyle ':completion:*:(scp|rsync):*' group-order users files all-files hosts-domain hosts-host hosts-ipaddr
@@ -117,58 +95,3 @@ zstyle ':completion:*:ssh:*' group-order users hosts-domain hosts-host users hos
 zstyle ':completion:*:(ssh|scp|rsync):*:hosts-host' ignored-patterns '*(.|:)*' loopback ip6-loopback localhost ip6-localhost broadcasthost
 zstyle ':completion:*:(ssh|scp|rsync):*:hosts-domain' ignored-patterns '<->.<->.<->.<->' '^[-[:alnum:]]##(.[-[:alnum:]]##)##' '*@*'
 zstyle ':completion:*:(ssh|scp|rsync):*:hosts-ipaddr' ignored-patterns '^(<->.<->.<->.<->|(|::)([[:xdigit:].]##:(#c,2))##(|%*))' '127.0.0.<->' '255.255.255.255' '::1' 'fe80::*'
-
-# fzf
-if command -v fzf >/dev/null; then
-  # fuzzy completion with 'z' when called without args
-  unalias z 2> /dev/null
-  z() {
-    [ $# -gt 0 ] && _z "$*" && return
-    cd "$(_z -l 2>&1 | fzf --height 40% --nth 2.. --reverse --inline-info +s --tac --query "${*##-* }" | sed 's/^[0-9,.]* *//')"
-  }
-
-  __git_log () {
-    # format str implies:
-    #  --abbrev-commit
-    #  --decorate
-    git log \
-      --color=always \
-      --graph \
-      --all \
-      --date=short \
-      --format="%C(bold blue)%h%C(reset) %C(green)%ad%C(reset) | %C(white)%s %C(red)[%an] %C(bold yellow)%d"
-  }
-
-  _fzf_complete_git() {
-    ARGS="$@"
-
-    # these are commands I commonly call on commit hashes.
-    # cp->cherry-pick, co->checkout
-
-    if [[ $ARGS == 'git cp'* || \
-          $ARGS == 'git cherry-pick'* || \
-          $ARGS == 'git co'* || \
-          $ARGS == 'git checkout'* || \
-          $ARGS == 'git reset'* || \
-          $ARGS == 'git show'* || \
-          $ARGS == 'git log'* ]]; then
-      _fzf_complete "--reverse --multi" "$@" < <(__git_log)
-    else
-      eval "zle ${fzf_default_completion:-expand-or-complete}"
-    fi
-  }
-
-  _fzf_complete_git_post() {
-    sed -e 's/^[^a-z0-9]*//' | awk '{print $1}'
-  }
-
-  # pass completion suggested by @d4ndo (#362) (slightly modified)
-  _fzf_complete_pass() {
-    _fzf_complete '+m' "$@" < <(
-      local pwdir=${PASSWORD_STORE_DIR-~/.password-store/}
-      find "$pwdir" -name "*.gpg" -print |
-          sed -e "s#${pwdir}/\{0,1\}##" |
-          sed -e 's/\(.*\)\.gpg/\1/'
-    )
-  }
-fi
