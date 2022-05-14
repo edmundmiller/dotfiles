e3d94bf4e43bfc007add9b17921d40d835227fab
Edmund Miller <edmund.a.miller@protonmail.com>
2022-05-13 22:25:50 -0500

    chore: Update themes

--- modules/themes/default.nix
+++ modules/themes/default.nix
@@ -7,8 +7,7 @@
 with lib;
 with lib.my;
 let cfg = config.modules.theme;
-in
-{
+in {
   options.modules.theme = with types; {
     active = mkOption {
       type = nullOr str;
@@ -36,23 +35,118 @@ in
     };
 
     onReload = mkOpt (attrsOf lines) { };
+
+    fonts = {
+      # TODO Use submodules
+      mono = {
+        name = mkOpt str "Monospace";
+        size = mkOpt int 12;
+      };
+      sans = {
+        name = mkOpt str "Sans";
+        size = mkOpt int 10;
+      };
+    };
+
+    colors = {
+      black = mkOpt str "#000000"; # 0
+      red = mkOpt str "#FF0000"; # 1
+      green = mkOpt str "#00FF00"; # 2
+      yellow = mkOpt str "#FFFF00"; # 3
+      blue = mkOpt str "#0000FF"; # 4
+      magenta = mkOpt str "#FF00FF"; # 5
+      cyan = mkOpt str "#00FFFF"; # 6
+      silver = mkOpt str "#BBBBBB"; # 7
+      grey = mkOpt str "#888888"; # 8
+      brightred = mkOpt str "#FF8800"; # 9
+      brightgreen = mkOpt str "#00FF80"; # 10
+      brightyellow = mkOpt str "#FF8800"; # 11
+      brightblue = mkOpt str "#0088FF"; # 12
+      brightmagenta = mkOpt str "#FF88FF"; # 13
+      brightcyan = mkOpt str "#88FFFF"; # 14
+      white = mkOpt str "#FFFFFF"; # 15
+
+      # Color classes
+      types = {
+        bg = mkOpt str cfg.colors.black;
+        fg = mkOpt str cfg.colors.white;
+        panelbg = mkOpt str cfg.colors.types.bg;
+        panelfg = mkOpt str cfg.colors.types.fg;
+        border = mkOpt str cfg.colors.types.bg;
+        error = mkOpt str cfg.colors.red;
+        warning = mkOpt str cfg.colors.yellow;
+        highlight = mkOpt str cfg.colors.white;
+      };
+    };
   };
 
   config = mkIf (cfg.active != null) (mkMerge [
-    # Read xresources files in ~/.config/xtheme/* to allow modular
-    # configuration of Xresources.
-    (
-      let
-        xrdb = ''${pkgs.xorg.xrdb}/bin/xrdb -merge "$XDG_CONFIG_HOME"/xtheme/*'';
-      in
-      {
-        services.xserver.displayManager.sessionCommands = xrdb;
-        modules.theme.onReload.xtheme = xrdb;
-      }
-    )
+    # Read xresources files in ~/.config/xtheme/* to allow modular configuration
+    # of Xresources.
+    (let
+      xrdb =
+        ''cat "$XDG_CONFIG_HOME"/xtheme/* | ${pkgs.xorg.xrdb}/bin/xrdb -load'';
+    in {
+      home.configFile."xtheme.init" = {
+        text = xrdb;
+        executable = true;
+      };
+      modules.theme.onReload.xtheme = xrdb;
+    })
+
+    (mkIf config.modules.desktop.bspwm.enable {
+      home.configFile."bspwm/rc.d/05-init" = {
+        text = "$XDG_CONFIG_HOME/xtheme.init";
+        executable = true;
+      };
+    })
 
     {
       home.configFile = {
+        "xtheme/00-init".text = with cfg.colors; ''
+          #define bg   ${types.bg}
+          #define fg   ${types.fg}
+          #define blk  ${black}
+          #define red  ${red}
+          #define grn  ${green}
+          #define ylw  ${yellow}
+          #define blu  ${blue}
+          #define mag  ${magenta}
+          #define cyn  ${cyan}
+          #define wht  ${white}
+          #define bblk ${grey}
+          #define bred ${brightred}
+          #define bgrn ${brightgreen}
+          #define bylw ${brightyellow}
+          #define bblu ${brightblue}
+          #define bmag ${brightmagenta}
+          #define bcyn ${brightcyan}
+          #define bwht ${silver}
+        '';
+        "xtheme/05-colors".text = ''
+          *.foreground: fg
+          *.background: bg
+          *.color0:  blk
+          *.color1:  red
+          *.color2:  grn
+          *.color3:  ylw
+          *.color4:  blu
+          *.color5:  mag
+          *.color6:  cyn
+          *.color7:  wht
+          *.color8:  bblk
+          *.color9:  bred
+          *.color10: bgrn
+          *.color11: bylw
+          *.color12: bblu
+          *.color13: bmag
+          *.color14: bcyn
+          *.color15: bwht
+        '';
+        "xtheme/05-fonts".text = with cfg.fonts.mono; ''
+          *.font: xft:${name}:pixelsize=${toString (size)}
+          Emacs.font: ${name}:pixelsize=${toString (size)}
+        '';
         # GTK
         "gtk-3.0/settings.ini".text = ''
           [Settings]
@@ -74,7 +168,7 @@ in
           ''gtk-theme-name="${cfg.gtk.theme}"''}
           ${optionalString (cfg.gtk.iconTheme != "")
           ''gtk-icon-theme-name="${cfg.gtk.iconTheme}"''}
-          gtk-font-name="Sans 10"
+          gtk-font-name="Sans ${toString (cfg.fonts.sans.size)}"
         '';
         # QT4/5 global theme
         "Trolltech.conf".text = ''
@@ -82,10 +176,17 @@ in
           ${optionalString (cfg.gtk.theme != "") "style=${cfg.gtk.theme}"}
         '';
       };
+
+      fonts.fontconfig.defaultFonts = {
+        sansSerif = [ cfg.fonts.sans.name ];
+        monospace = [ cfg.fonts.mono.name ];
+      };
     }
 
-    (mkIf (cfg.wallpaper != null) (
-      let
+    (mkIf (cfg.wallpaper != null)
+    # Set the wallpaper ourselves so we don't need .background-image and/or
+    # .fehbg polluting $HOME
+      (let
         wCfg = config.services.xserver.desktopManager.wallpaper;
         command = ''
           if [ -e "$XDG_DATA_HOME/wallpaper" ]; then
@@ -95,40 +196,33 @@ in
               $XDG_DATA_HOME/wallpaper
           fi
         '';
-      in
-      {
-        # Set the wallpaper ourselves so we don't need .background-image and/or
-        # .fehbg polluting $HOME
+      in {
         services.xserver.displayManager.sessionCommands = command;
         modules.theme.onReload.wallpaper = command;
 
         home.dataFile =
           mkIf (cfg.wallpaper != null) { "wallpaper".source = cfg.wallpaper; };
-      }
-    ))
+      }))
 
     (mkIf (cfg.loginWallpaper != null) {
       services.xserver.displayManager.lightdm.background = cfg.loginWallpaper;
     })
 
-    (mkIf (cfg.onReload != { }) (
-      let
-        reloadTheme = with pkgs;
-          (writeScriptBin "reloadTheme" ''
-            #!${stdenv.shell}
-            echo "Reloading current theme: ${cfg.active}"
-            ${concatStringsSep "\n" (mapAttrsToList (name: script: ''
-              echo "[${name}]"
-              ${script}
-            '') cfg.onReload)}
-          '');
-      in
-      {
-        user.packages = [ reloadTheme ];
-        system.userActivationScripts.reloadTheme = ''
-          [ -z "$NORELOAD" ] && ${reloadTheme}/bin/reloadTheme
-        '';
-      }
-    ))
+    (mkIf (cfg.onReload != { }) (let
+      reloadTheme = with pkgs;
+        (writeScriptBin "reloadTheme" ''
+          #!${stdenv.shell}
+          echo "Reloading current theme: ${cfg.active}"
+          ${concatStringsSep "\n" (mapAttrsToList (name: script: ''
+            echo "[${name}]"
+            ${script}
+          '') cfg.onReload)}
+        '');
+    in {
+      user.packages = [ reloadTheme ];
+      system.userActivationScripts.reloadTheme = ''
+        [ -z "$NORELOAD" ] && ${reloadTheme}/bin/reloadTheme
+      '';
+    }))
   ]);
 }
