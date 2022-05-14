69262a491587de8fb9ed8b8993b42b874249bbff
Edmund Miller <edmund.a.miller@protonmail.com>
2022-05-13 22:25:50 -0500

    chore: Update themes

--- modules/themes/functional/default.nix
+++ modules/themes/functional/default.nix
@@ -1,12 +1,11 @@
-# modules/themes/functional/default.nix --- a functional theme
+# modules/themes/functional/default.nix --- For functional genomics/programming
 
 { options, config, lib, pkgs, ... }:
 
 with lib;
 with lib.my;
 let cfg = config.modules.theme;
-in
-{
+in {
   config = mkIf (cfg.active == "functional") (mkMerge [
     # Desktop-agnostic configuration
     {
@@ -18,6 +17,32 @@ in
             iconTheme = "Paper";
             cursorTheme = "Paper";
           };
+          fonts = {
+            sans.name = "Fira Sans";
+            mono.name = "Fira Code";
+          };
+          colors = {
+            black = "#1E2029";
+            red = "#ffb86c";
+            green = "#50fa7b";
+            yellow = "#f0c674";
+            blue = "#61bfff";
+            magenta = "#bd93f9";
+            cyan = "#8be9fd";
+            silver = "#e2e2dc";
+            grey = "#5B6268";
+            brightred = "#de935f";
+            brightgreen = "#0189cc";
+            brightyellow = "#f9a03f";
+            brightblue = "#8be9fd";
+            brightmagenta = "#ff79c6";
+            brightcyan = "#0189cc";
+            white = "#f8f8f2";
+
+            types.fg = "#bbc2cf";
+            types.panelbg = "#21242b";
+            types.border = "#1a1c25";
+          };
         };
 
         shell.zsh.rcFiles = [ ./config/zsh/prompt.zsh ];
@@ -25,12 +50,12 @@ in
         desktop.browsers = {
           firefox.userChrome = concatMapStringsSep "\n" readFile
             [ ./config/firefox/userChrome.css ];
-          # qutebrowser.userStyles = concatMapStringsSep "\n" readFile
-          #   (map toCSSFile [
-          #     ./config/qutebrowser/userstyles/monospace-textareas.scss
-          #     ./config/qutebrowser/userstyles/stackoverflow.scss
-          #     ./config/qutebrowser/userstyles/xkcd.scss
-          #   ]);
+          qutebrowser.userStyles = concatMapStringsSep "\n" readFile
+            (map toCSSFile [
+              ./config/qutebrowser/userstyles/monospace-textareas.scss
+              ./config/qutebrowser/userstyles/stackoverflow.scss
+              ./config/qutebrowser/userstyles/xkcd.scss
+            ]);
         };
       };
     }
@@ -38,21 +63,18 @@ in
     # Desktop (X11) theming
     (mkIf config.services.xserver.enable {
       user.packages = with pkgs; [
-        dracula-theme
+        unstable.dracula-theme
         paper-icon-theme # for rofi
       ];
       fonts = {
         fonts = with pkgs; [
           fira-code
           fira-code-symbols
+          open-sans
           jetbrains-mono
           siji
           font-awesome
         ];
-        fontconfig.defaultFonts = {
-          sansSerif = [ "Fira Sans" ];
-          monospace = [ "Fira Code" ];
-        };
       };
 
       # Compositor
@@ -77,10 +99,10 @@ in
 
       # Login screen theme
       services.xserver.displayManager.lightdm.greeters.mini.extraConfig = ''
-        text-color = "#ff79c6"
-        password-background-color = "#1E2029"
-        window-color = "#181a23"
-        border-color = "#181a23"
+        text-color = "${cfg.colors.magenta}"
+        password-background-color = "${cfg.colors.black}"
+        window-color = "${cfg.colors.types.border}"
+        border-color = "${cfg.colors.types.border}"
       '';
 
       # Other dotfiles
@@ -91,8 +113,8 @@ in
             "xtheme/90-theme".source = ./config/Xresources;
           }
           (mkIf desktop.bspwm.enable {
-            "bspwm/rc.d/polybar".source = ./config/polybar/run.sh;
-            "bspwm/rc.d/theme".source = ./config/bspwmrc;
+            "bspwm/rc.d/00-theme".source = ./config/bspwmrc;
+            "bspwm/rc.d/95-polybar".source = ./config/polybar/run.sh;
           })
           (mkIf desktop.apps.rofi.enable {
             "rofi/theme" = {
@@ -105,7 +127,17 @@ in
               source = ./config/polybar;
               recursive = true;
             };
-            "dunst/dunstrc".source = ./config/dunstrc;
+            "dunst/dunstrc".text = import ./config/dunstrc cfg;
+            "Dracula-purple-solid-kvantum" = {
+              recursive = true;
+              source =
+                "${pkgs.unstable.dracula-theme}/share/themes/Dracula/kde/kvantum/Dracula-purple-solid";
+              target = "Kvantum/Dracula-purple-solid";
+            };
+            "kvantum.kvconfig" = {
+              text = "theme=Dracula-purple-solid";
+              target = "Kvantum/kvantum.kvconfig";
+            };
           })
           (mkIf desktop.media.graphics.vector.enable {
             "inkscape/templates/default.svg".source =
