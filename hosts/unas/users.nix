23d3b5fd9c1ffe8cd459e6d6c1a06eefe6a60faf
Edmund Miller <edmund.a.miller@protonmail.com>
2022-06-19 11:34:06 -0500

    fix(unas): Set users shell to bash

--- hosts/unas/users.nix
+++ hosts/unas/users.nix
@@ -1,15 +1,18 @@
-{
+{ pkgs, ... }: {
   users.users = {
     monimiller = {
       isNormalUser = true;
+      createHome = true;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIjADG7vmI/moup3J2UyFjj51CNpV4VVo+f9oOr/n4r4 monicadd4@gmail.com"
       ];
+      shell = pkgs.bashInteractive;
     };
 
     tdmiller = {
       isNormalUser = true;
       createHome = true;
+      shell = pkgs.bashInteractive;
     };
   };
 }
