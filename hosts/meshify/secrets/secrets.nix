bdc2b0963b17513ce9dc1c201e7a5dc5108fc896
Edmund Miller <edmund.a.miller@protonmail.com>
2022-11-27 19:53:53 -0600

    feat(services): Add scrobbling to mpd

---
+++ hosts/meshify/secrets/secrets.nix
@@ -0,0 +1,4 @@
+let
+  key =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK3EVc3A55QHe83NXfqrClVohWz2DscDgx0pr4PSlcGO edmund.a.miller@protonmail.com";
+in { "lastfm-password.age".publicKeys = [ key ]; }
