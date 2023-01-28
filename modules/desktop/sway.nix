c885dfd2f7d389e1370635c8ef06df9d5f41a2d1
Edmund Miller <edmund.a.miller@protonmail.com>
2023-01-28 08:21:01 -0600

    feat(sway): Use greetd

--- modules/desktop/sway.nix
+++ modules/desktop/sway.nix
@@ -76,6 +76,15 @@ in {
       ];
     };
 
-    services.xserver.displayManager.gdm.enable = true;
+    services.greetd = {
+      enable = true;
+      settings = {
+
+        default_session = {
+          command = "${pkgs.greetd.greetd}/bin/agreety --cmd sway";
+        };
+
+      };
+    };
   };
 }
