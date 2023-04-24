8f6131fc0229973e2717137cc5b179bdb8b3fa52
Edmund Miller <edmund.a.miller@protonmail.com>
2023-04-24 12:11:09 -0500

    feat(term): Try out wezterm

---
+++ modules/desktop/term/wezterm.nix
@@ -0,0 +1,19 @@
+# modules/desktop/term/wezterm.nix
+
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.term.wezterm;
+in {
+  options.modules.desktop.term.wezterm = { enable = mkBoolOpt false; };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [ wezterm ];
+
+    # TODO Waiting for config to stablize
+    # home.configFile = {
+    #   "wezterm/wezterm.lua".source = "${configDir}/wezterm/wezterm.lua";
+    # };
+  };
+}
