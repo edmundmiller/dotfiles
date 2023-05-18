f0da86d2c555f3c10c8337f0596dc5e91818d241
Edmund Miller <edmund.a.miller@protonmail.com>
2023-05-18 15:52:16 -0500

    feat(hyprland): enable dunst

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -21,5 +21,24 @@ in {
         font-awesome
       ];
     };
+
+    environment.systemPackages = with pkgs; [
+      dunst
+      libnotify
+      (waybar.override {
+        wireplumberSupport= true;
+        nlSupport = true;
+      })
+    ];
+
+    systemd.user.services."dunst" = {
+      enable = true;
+      description = "";
+      wantedBy = [ "default.target" ];
+      serviceConfig.Restart = "always";
+      serviceConfig.RestartSec = 2;
+      serviceConfig.ExecStart = "${pkgs.dunst}/bin/dunst";
+    };
+
   };
 }
