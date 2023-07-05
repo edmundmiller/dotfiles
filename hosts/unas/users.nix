de5ec025f2cf95a202dc1770db94a77213946fd4
Edmund Miller <edmund.a.miller@protonmail.com>
2023-07-05 12:11:41 -0500

    fix: Change Moni's shell to zsh for warp

--- hosts/unas/users.nix
+++ hosts/unas/users.nix
@@ -7,7 +7,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIjADG7vmI/moup3J2UyFjj51CNpV4VVo+f9oOr/n4r4 monicadd4@gmail.com"
       ];
-      shell = pkgs.bashInteractive;
+      shell = pkgs.zsh;
     };
 
     tdmiller = {
