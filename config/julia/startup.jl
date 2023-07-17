c981cbc678ae69a01cbf03b4f7b713816a6898f1
Edmund Miller <edmund.a.miller@protonmail.com>
2023-07-17 09:00:01 -0500

    Add lines to auto load the local project

--- config/julia/startup.jl
+++ config/julia/startup.jl
@@ -11,3 +11,10 @@ try
 catch e
     @warn "Error initializing Revise" exception=(e, catch_backtrace())
 end
+
+# Auto start the local project
+# https://bkamins.github.io/julialang/2020/05/10/julia-project-environments.html
+using Pkg
+if isfile("Project.toml") && isfile("Manifest.toml")
+    Pkg.activate(".")
+end
