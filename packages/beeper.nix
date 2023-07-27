fa0199e1fe3df2d9925aba2ad745eadc7d81b494
Edmund Miller <edmund.a.miller@protonmail.com>
2023-07-26 21:08:48 -0400

    Add beeper desktop item

--- packages/beeper.nix
+++ packages/beeper.nix
@@ -1,20 +1,35 @@
 {
   appimageTools,
   fetchurl,
+  makeWrapper,
   ...
 }: let
-  name = "beeper";
+  pname = "beeper";
   version = "3.65.19";
+  name = "${pname}-${version}";
+  src = fetchurl {
+    url = "https://download.beeper.com/linux/appImage/x64";
+    hash = "sha256-jcLNAbfXScHwUMDTuDwICNxETKXl3sROPKhLt1MswZk=";
+    name = "${name}.AppImage";
+  };
 in
   appimageTools.wrapType2 {
-    inherit name version;
+    inherit name src version;
 
-    src = fetchurl {
-      url = "https://download.beeper.com/linux/appImage/x64";
-      hash = "sha256-OPtp0lXo4Xw0LQOR9CuOaDdd2+YhaBMjvgWlvbPU2cM=";
-    };
-
-    # extraPkgs = pkgs: with pkgs; [];
+    extraInstallCommands = let
+      appimageContents = appimageTools.extractType2 {inherit name src;};
+    in ''
+      mv $out/bin/${name} $out/bin/${pname}
+      source "${makeWrapper}/nix-support/setup-hook"
+      wrapProgram $out/bin/${pname} \
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}"
+      install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
+      install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/512x512/apps/${pname}.png \
+         $out/share/icons/hicolor/512x512/apps/${pname}.png
+      substituteInPlace $out/share/applications/${pname}.desktop \
+        --replace 'Icon=lens' 'Icon=${pname}' \
+        --replace 'Exec=AppRun' 'Exec=${pname}'
+    '';
 
     meta = {
       homepage = "https://www.beeper.com/";
@@ -24,3 +39,13 @@ in
       maintainers = ["emiller88"];
     };
   }
+# [Desktop Entry]
+# Name=Beeper
+# Exec=AppRun --no-sandbox %U
+# Terminal=false
+# Type=Application
+# Icon=beeper
+# StartupWMClass=Beeper
+# X-AppImage-Version=2307192q06wwc12
+# Comment=Beeper: Unified Messenger
+# Categories=Utility;
