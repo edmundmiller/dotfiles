d3a4ea3a27daa2a04eab59430cdf5e96534ad051
Edmund Miller <edmund.a.miller@protonmail.com>
2023-09-13 10:09:46 -0500

    feat(beeper): Update package to match nixpkgs
    Still need to PR a version override.

--- packages/beeper.nix
+++ packages/beeper.nix
@@ -1,41 +1,65 @@
 {
-  appimageTools,
+  lib,
   fetchurl,
+  stdenv,
+  appimageTools,
+  libsecret,
   makeWrapper,
-  ...
 }: let
   pname = "beeper";
-  version = "v3.75.16";
+  version = "3.75.16";
   name = "${pname}-${version}";
   src = fetchurl {
-    url = "https://download.beeper.com/linux/appImage/x64";
-    hash = "sha256-0upeew7iCZUqT93r6ZCBiGjzQun9DjyI6w+TDTLrIMo=";
-    name = "${name}.AppImage";
+    url = "https://download.todesktop.com/2003241lzgn20jd/beeper-${version}.AppImage";
+    hash = "sha512-1AgqD5rz9Vx0S7vbeEZ0TEhqB1q4noo19ijii5YYU/2qZsjeEhEqfbrLXJyBLAGxSfTDHoy07+Nc+UDYTDt0QQ==";
+  };
+  appimage = appimageTools.wrapType2 {
+    inherit version pname src;
+    extraPkgs = pkgs: with pkgs; [libsecret];
+  };
+  appimageContents = appimageTools.extractType2 {
+    inherit version pname src;
   };
 in
-  appimageTools.wrapType2 {
-    inherit name src version;
+  stdenv.mkDerivation rec {
+    inherit name pname;
 
-    extraInstallCommands = let
-      appimageContents = appimageTools.extractType2 {inherit name src;};
-    in ''
-      mv $out/bin/${name} $out/bin/${pname}
-      source "${makeWrapper}/nix-support/setup-hook"
+    src = appimage;
+
+    nativeBuildInputs = [makeWrapper];
+
+    installPhase = ''
+      runHook preInstall
+
+      mv bin/${name} bin/${pname}
+
+      mkdir -p $out/
+      cp -r bin $out/bin
+
+      mkdir -p $out/share/${pname}
+      cp -a ${appimageContents}/locales $out/share/${pname}
+      cp -a ${appimageContents}/resources $out/share/${pname}
+      cp -a ${appimageContents}/usr/share/icons $out/share/
+      install -Dm 644 ${appimageContents}/${pname}.desktop -t $out/share/applications/
+
+      substituteInPlace $out/share/applications/${pname}.desktop --replace "AppRun" "${pname}"
+
       wrapProgram $out/bin/${pname} \
-        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}"
-      install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
-      install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/512x512/apps/${pname}.png \
-         $out/share/icons/hicolor/512x512/apps/${pname}.png
-      substituteInPlace $out/share/applications/${pname}.desktop \
-        --replace 'Icon=lens' 'Icon=${pname}' \
-        --replace 'Exec=AppRun' 'Exec=${pname}'
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}} --no-update"
+
+      runHook postInstall
     '';
 
-    meta = {
-      homepage = "https://www.beeper.com/";
-      description = "All your chats in one app. Yes, really.";
-      # license = lib.licenses.mit;
+    meta = with lib; {
+      description = "Universal chat app.";
+      longDescription = ''
+        Beeper is a universal chat app. With Beeper, you can send
+        and receive messages to friends, family and colleagues on
+        many different chat networks.
+      '';
+      homepage = "https://beeper.com";
+      license = licenses.unfree;
+      maintainers = with maintainers; [jshcmpbll];
       platforms = ["x86_64-linux"];
-      maintainers = ["emiller88"];
     };
   }
