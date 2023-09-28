3ddaf70c01e1756676258c69db6c84e29d2317a9
Edmund Miller <edmund.a.miller@protonmail.com>
2023-09-28 14:49:04 -0500

    fix(gnome): Add packages to exclude

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -15,6 +15,26 @@ in {
     services.xserver.enable = true;
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
+    environment.gnome.excludePackages =
+      (with pkgs; [
+        gnome-photos
+        gnome-tour
+      ])
+      ++ (with pkgs.gnome; [
+        cheese # webcam tool
+        gnome-music
+        gedit # text editor
+        epiphany # web browser
+        geary # email reader
+        gnome-characters
+        tali # poker game
+        iagno # go game
+        hitori # sudoku game
+        atomix # puzzle game
+        yelp # Help view
+        gnome-contacts
+        gnome-initial-setup
+      ]);
     programs.dconf.enable = true;
 
     environment.sessionVariables.NIXOS_OZONE_WL = "1";
