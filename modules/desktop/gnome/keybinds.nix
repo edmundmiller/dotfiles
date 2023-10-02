1c865c794afa2b63c5ea29180ba66878dad4b2e0
Edmund Miller <edmund.a.miller@protonmail.com>
2023-10-02 09:23:37 -0500

    Add gnome keybinds

---
+++ modules/desktop/gnome/keybinds.nix
@@ -0,0 +1,6 @@
+# /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/binding
+#   '<Super>c'
+# /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/command
+#   'org-capture'
+# /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/name
+#   'org-capture'
