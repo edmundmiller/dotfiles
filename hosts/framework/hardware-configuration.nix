08d5ec4b5e0893dd6b14e4bdbf199aa3a899e854
Edmund Miller <edmund.a.miller@protonmail.com>
2023-10-02 14:19:25 -0500

    feat(framework): Try to fix Fingerprint on gnome

--- hosts/framework/hardware-configuration.nix
+++ hosts/framework/hardware-configuration.nix
@@ -37,6 +37,30 @@
   services.xserver.libinput.touchpad.disableWhileTyping = true;
   services.xserver.xkbOptions = "caps:escape";
 
+  # Fingerprint gnome fix
+  # https://github.com/NixOS/nixpkgs/issues/171136#issuecomment-1627779037
+  security.pam.services.login.fprintAuth = false;
+  # similarly to how other distributions handle the fingerprinting login
+  security.pam.services.gdm-fingerprint = lib.mkIf (config.services.fprintd.enable) {
+    text = ''
+      auth       required                    pam_shells.so
+      auth       requisite                   pam_nologin.so
+      auth       requisite                   pam_faillock.so      preauth
+      auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
+      auth       optional                    pam_permit.so
+      auth       required                    pam_env.so
+      auth       [success=ok default=1]      ${pkgs.gnome.gdm}/lib/security/pam_gdm.so
+      auth       optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
+
+      account    include                     login
+
+      password   required                    pam_deny.so
+
+      session    include                     login
+      session    optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
+    '';
+  };
+
   # high-resolution display
   fonts.fontconfig.hinting.enable = false;
 
