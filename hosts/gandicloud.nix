dd4b49047a8d2e5836b3f34382faa4194d31cdc4
Edmund Miller <edmund.a.miller@protonmail.com>
2023-10-16 11:25:54 +0200

    feat: Add GandiCloud host

---
+++ hosts/gandicloud.nix
@@ -0,0 +1,69 @@
+# This is the configuration required to run NixOS on GandiCloud.
+{
+  lib,
+  modulesPath,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [inetutils mtr sysstat git];
+
+  modules = {
+    editors = {
+      default = "nvim";
+      vim.enable = true;
+    };
+    shell = {
+      git.enable = true;
+      zsh.enable = true;
+    };
+    services.ssh.enable = true;
+  };
+
+  # BOILERPLATE
+  # This is the configuration required to run NixOS on GandiCloud.
+  imports = [
+    (modulesPath + "/virtualisation/openstack-config.nix")
+  ];
+  config = {
+    boot.initrd.kernelModules = [
+      "xen-blkfront"
+      "xen-tpmfront"
+      "xen-kbdfront"
+      "xen-fbfront"
+      "xen-netfront"
+      "xen-pcifront"
+      "xen-scsifront"
+    ];
+
+    # Show debug kernel message on boot then reduce loglevel once booted
+    boot.consoleLogLevel = 7;
+    boot.kernel.sysctl."kernel.printk" = "4 4 1 7";
+
+    # For "openstack console log show"
+    boot.kernelParams = ["console=ttyS0"];
+    systemd.services."serial-getty@ttyS0" = {
+      enable = true;
+      wantedBy = ["multi-user.target"];
+      serviceConfig.Restart = "always";
+    };
+
+    # The device exposed by Xen
+    boot.loader.grub.device = lib.mkForce "/dev/xvda";
+
+    # This is to get a prompt via the "openstack console url show" command
+    systemd.services."getty@tty1" = {
+      enable = lib.mkForce true;
+      wantedBy = ["multi-user.target"];
+      serviceConfig.Restart = "always";
+    };
+
+    # This is required to get an IPv6 address on our infrastructure
+    networking.tempAddresses = "disabled";
+
+    nix.extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+
+    system.stateVersion = "23.05";
+  };
+}
