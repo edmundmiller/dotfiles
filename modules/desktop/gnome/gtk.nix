f2551d95d510639d5ca0bb32018b349bb9167c69
Edmund Miller <edmund.a.miller@protonmail.com>
2023-10-21 23:54:11 +0200

    refactor: Split into a few files to make it easier to manage

---
+++ modules/desktop/gnome/gtk.nix
@@ -0,0 +1,36 @@
+{pkgs}: {
+  env.GTK_THEME = "palenight";
+  home-manager.users.emiller = {
+    ## GTK
+    gtk = {
+      enable = true;
+
+      iconTheme = {
+        name = "Papirus-Dark";
+        package = pkgs.papirus-icon-theme;
+      };
+
+      theme = {
+        name = "palenight";
+        package = pkgs.palenight-theme;
+      };
+
+      cursorTheme = {
+        name = "Numix-Cursor";
+        package = pkgs.numix-cursor-theme;
+      };
+
+      gtk3.extraConfig = {
+        Settings = ''
+          gtk-application-prefer-dark-theme=1
+        '';
+      };
+
+      gtk4.extraConfig = {
+        Settings = ''
+          gtk-application-prefer-dark-theme=1
+        '';
+      };
+    };
+  };
+}
