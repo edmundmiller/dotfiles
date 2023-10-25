d835dcad83baa76c9d05d90e143b1b47d5f832f7
Edmund Miller <edmund.a.miller@protonmail.com>
2023-10-24 22:03:31 -0500

    feat(nuc): Add initial config

---
+++ hosts/nuc/modules/paperless.nix
@@ -0,0 +1,13 @@
+{
+  config,
+  ...
+}: {
+  modules.services.paperless.enable = true;
+
+  services.paperless = {
+    address = "0.0.0.0";
+    mediaDir = "/data/media/docs/paperless";
+    consumptionDir = "/data/media/docs/consume";
+    passwordFile = config.age.secrets.paperless-adminCredentials.path;
+  };
+}
