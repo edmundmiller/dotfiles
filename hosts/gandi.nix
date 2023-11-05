1302953521dad66677857922bb58352d0f22ef9b
Edmund Miller <edmund.a.miller@protonmail.com>
2023-11-05 12:16:44 -0600

    feat(gandi): Add services

--- hosts/gandi.nix
+++ hosts/gandi.nix
@@ -15,7 +15,11 @@ with lib; {
       git.enable = true;
       zsh.enable = true;
     };
-    services.ssh.enable = true;
+    services = {
+      ssh.enable = true;
+      syncthing.enable = true;
+      tailscale.enable = true;
+    };
   };
 
   environment.systemPackages = with pkgs; [inetutils mtr sysstat git];
