84cbd472680ffa7bc0af387b8e46222c8db708a3
Edmund Miller <edmund.a.miller@protonmail.com>
2023-11-13 20:09:22 -0600

    fix(shell): Update prompt

--- modules/shell/nushell.nix
+++ modules/shell/nushell.nix
@@ -58,10 +58,10 @@ in {
       starship = {
         enable = true;
         settings = {
-          add_newline = true;
+          add_newline = false;
           character = {
-            success_symbol = "[➜](bold green)";
-            error_symbol = "[➜](bold red)";
+            success_symbol = "[λ](bold green)";
+            error_symbol = "[λ](bold red)";
           };
         };
       };
