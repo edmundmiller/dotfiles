64924d519a787360face9644fa747746f63b1d32
Edmund Miller <edmund.a.miller@protonmail.com>
2023-11-19 19:21:49 -0600

    feat: Add serif fonts

--- modules/desktop/gnome/palenight.nix
+++ modules/desktop/gnome/palenight.nix
@@ -10,7 +10,15 @@
   stylix.image = ../../themes/functional/config/wallpaper.png;
   stylix.polarity = "dark";
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/material-palenight.yaml";
-  stylix.fonts.monospace.package = pkgs.jetbrains-mono;
-  stylix.fonts.monospace.name = "JetBrains Mono";
+  stylix.fonts = {
+    serif.package = pkgs.dejavu_fonts;
+    serif.name = "DejaVu Serif";
+
+    sansSerif.package = pkgs.fira;
+    sansSerif.name = "Fira Sans";
+
+    monospace.package = pkgs.jetbrains-mono;
+    monospace.name = "JetBrains Mono";
+  };
   stylix.targets.gnome.enable = true;
 }
