4ff2a0a552eaf45aa19ec4c9957f58fe60afd8f6
Edmund Miller <edmund.a.miller@protonmail.com>
2023-11-25 16:18:28 -0600

    fix: Bitwarden got fixed

--- modules/shell/bitwarden.nix
+++ modules/shell/bitwarden.nix
@@ -16,7 +16,7 @@ in {
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
-      # bitwarden FIXME https://github.com/NixOS/nixpkgs/issues/262730
+      bitwarden
       bitwarden-cli
     ];
 
