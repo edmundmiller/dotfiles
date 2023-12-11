1e7a77fea6534859b57f26a24e52fbdc77c027c5
Edmund Miller <edmund.a.miller@protonmail.com>
2023-12-11 15:30:38 -0600

    style: Run alejandro

--- packages/git-sim.nix
+++ packages/git-sim.nix
@@ -1,8 +1,8 @@
-{ lib
-, python3
-, fetchFromGitHub
+{
+  lib,
+  python3,
+  fetchFromGitHub,
 }:
-
 python3.pkgs.buildPythonApplication rec {
   pname = "git-sim";
   version = "0.3.3";
@@ -20,13 +20,13 @@ python3.pkgs.buildPythonApplication rec {
     python3.pkgs.wheel
   ];
 
-  pythonImportsCheck = [ "git_sim" ];
+  pythonImportsCheck = ["git_sim"];
 
   meta = with lib; {
     description = "Visually simulate Git operations in your own repos with a single terminal command";
     homepage = "https://github.com/initialcommit-com/git-sim";
     license = licenses.gpl2Only;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [];
     mainProgram = "git-sim";
   };
 }
