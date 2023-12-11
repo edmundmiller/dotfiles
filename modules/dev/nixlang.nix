6854d900cf1d1a464f5c6d675a743fa62afa7317
Edmund Miller <edmund.a.miller@protonmail.com>
2023-12-11 14:17:10 -0600

    feat(nix): Add alias for devenv-parts

--- modules/dev/nixlang.nix
+++ modules/dev/nixlang.nix
@@ -23,6 +23,7 @@ in {
 
     environment.shellAliases = {
       devenv-init = "nix flake init --template github:cachix/devenv && ${pkgs.direnv}/bin/direnv allow";
+      devenv-parts = "nix flake init --template github:cachix/devenv#flake-parts && ${pkgs.direnv}/bin/direnv allow";
       manfzf = "manix \"\" | grep '^# ' | sed 's/^# \(.*\) (.*/\1/;s/ (.*//;s/^# //' | fzf --preview=\"manix '{}'\" | xargs manix";
     };
   };
