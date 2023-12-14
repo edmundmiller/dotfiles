f4ad488bbb46901847ee4613c3f785df7ed6e0fb
Edmund Miller <edmund.a.miller@protonmail.com>
2023-12-14 17:21:57 -0600

    refactor(hass): Back to the container
    No. Can't go. I can't do storage again. I just can't! I won't go back in
    the dark!

--- modules/services/hass.nix
+++ modules/services/hass.nix
@@ -11,35 +11,18 @@ in {
   options.modules.services.hass = {enable = mkBoolOpt false;};
 
   config = mkIf cfg.enable {
-    services.home-assistant = {
-      enable = true;
-      config = {
-        # TODO
-        # http = {
-        #   use_x_forwarded_for = true;
-        #   trusted_proxies = ["127.0.0.1" "::1"];
-        # };
-
-        icloud = {};
-        frontend = {};
-        sun = {};
-        mobile_app = {};
-        default_config = {};
-        homeassistant = {
-          unit_system = "imperial";
-          time_zone = "America/Chicago";
-          temperature_unit = "C";
-          name = "home";
-          latitude = 32.983;
-          longitude = -96.752;
-        };
-        http.server_port = 8123;
-      };
-
-      extraComponents = ["backup" "zha"];
-
-      # TODO
-      openFirewall = true;
+    virtualisation.oci-containers.containers."homeassistant" = {
+      autoStart = true;
+      image = "ghcr.io/home-assistant/home-assistant:stable";
+      volumes = [
+        "/home/emiller/HomeAssistant:/config"
+        "/etc/localtime:/etc/localtime:ro"
+      ];
+      extraOptions = [
+        "--device=/dev/ttyUSB0"
+        "--network=host"
+        "--privileged"
+      ];
     };
   };
 }
