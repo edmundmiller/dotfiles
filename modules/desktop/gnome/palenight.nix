07e38b1a142bf4c0d3027d8a538ec22c3a59e8c9
Edmund Miller <edmund.a.miller@protonmail.com>
2023-12-15 21:14:12 -0600

    feat: Add font sizes

--- modules/desktop/gnome/palenight.nix
+++ modules/desktop/gnome/palenight.nix
@@ -19,6 +19,13 @@
 
     monospace.package = pkgs.jetbrains-mono;
     monospace.name = "JetBrains Mono";
+
+    sizes = {
+      applications = 14;
+      desktop = 13;
+      popups = 12;
+      terminal = 14;
+    };
   };
   stylix.targets.gnome.enable = true;
 
