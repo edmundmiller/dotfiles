c3100138e3c8afa4f6d65c3a8ced35824c1c09ce
Edmund Miller <edmund.a.miller@protonmail.com>
2023-12-16 17:06:01 -0600

    fix(unas): Just let Tailscale manage ssh keys

--- hosts/unas/users.nix
+++ hosts/unas/users.nix
@@ -6,9 +6,6 @@
     monimiller = {
       isNormalUser = true;
       createHome = true;
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIjADG7vmI/moup3J2UyFjj51CNpV4VVo+f9oOr/n4r4 monicadd4@gmail.com"
-      ];
       shell = pkgs.zsh;
     };
 
@@ -16,7 +13,6 @@
       isNormalUser = true;
       createHome = true;
       shell = pkgs.bashInteractive;
-      openssh.authorizedKeys.keyFiles = ["/home/tdmiller/.ssh/id_tailscale.pub"];
     };
   };
 }
