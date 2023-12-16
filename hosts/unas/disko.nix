feaba8971460fe0a10bc3b6c5e02f614adf1418f
Edmund Miller <edmund.a.miller@protonmail.com>
2023-12-16 13:43:42 -0600

    fix(unas): Bump gpt size

--- hosts/unas/disko.nix
+++ hosts/unas/disko.nix
@@ -11,7 +11,7 @@
           type = "gpt";
           partitions = {
             ESP = {
-              size = "64M";
+              size = "512M";
               type = "EF00";
               content = {
                 type = "filesystem";
