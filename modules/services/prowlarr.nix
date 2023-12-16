8d7d44076e42101fb7e9c2ad88aa5173c7273812
Edmund Miller <edmund.a.miller@protonmail.com>
2023-12-16 16:31:16 -0600

    feat(services): Add prowlarr service

---
+++ modules/services/prowlarr.nix
@@ -0,0 +1,17 @@
+{
+  options,
+  config,
+  lib,
+  ...
+}:
+with lib;
+with lib.my; let
+  cfg = config.modules.services.prowlarr;
+in {
+  options.modules.services.prowlarr = {enable = mkBoolOpt false;};
+
+  config = mkIf cfg.enable {
+    services.prowlarr.enable = true;
+    services.prowlarr.openFirewall = true;
+  };
+}
