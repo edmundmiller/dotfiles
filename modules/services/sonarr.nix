e8345844768c2c883de650b51981876b46356b32
Edmund Miller <edmund.a.miller@protonmail.com>
2023-12-16 16:36:14 -0600

    feat(services): Add sonarr

---
+++ modules/services/sonarr.nix
@@ -0,0 +1,17 @@
+{
+  config,
+  options,
+  lib,
+  ...
+}:
+with lib;
+with lib.my; let
+  cfg = config.modules.services.sonarr;
+in {
+  options.modules.services.sonarr = {enable = mkBoolOpt false;};
+
+  config = mkIf cfg.enable {
+    services.sonarr.enable = true;
+    services.sonarr.openFirewall = true;
+  };
+}
