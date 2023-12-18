1acaded713a1215c900209a22710a7f9b255da57
Edmund Miller <edmund.a.miller@protonmail.com>
2023-12-17 19:38:10 -0600

    fix(unas): Remove secrets

--- hosts/unas/secrets/secrets.nix
+++
@@ -1,7 +0,0 @@
-let
-  key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINrCDUEOwi4dVfqQweD4a6roNi9hWtZQ2lB2trKet5dS edmund.a.miller@gmail.com";
-in {
-  "minio-rootCredentials.age".publicKeys = [key];
-  "qb.age".publicKeys = [key];
-  "paperless-adminCredentials.age".publicKeys = [key];
-}
