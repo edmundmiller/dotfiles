68495d169579eead6a33971585c7eb0f1de11d45
Edmund Miller <edmund.a.miller@protonmail.com>
2023-12-18 12:52:52 -0600

    refactor: Make a Nextflow module

---
+++ modules/dev/nextflow.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  options,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.my; let
+  cfg = config.modules.dev.nextflow;
+in {
+  options.modules.dev.nextflow = {enable = mkBoolOpt false;};
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [
+      jdk17
+      my.nf-core
+    ];
+
+    environment.shellAliases = {
+      nf = "nextflow";
+    };
+  };
+}
