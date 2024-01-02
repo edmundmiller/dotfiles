a6e2f8366c93964b194a6430f80b5f3b6d84c8ed
Edmund Miller <edmund.a.miller@protonmail.com>
2024-01-02 14:05:40 -0600

    fix(packages): Downgrade required go version

--- packages/catgpt.nix
+++ packages/catgpt.nix
@@ -14,9 +14,14 @@ buildGoModule rec {
     hash = "sha256-2smt7C8YK2qPuUChmfDhWYYG2poxz/W5qXBgUtJLEIk=";
   };
 
-  vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  vendorHash = "sha256-0K2aq0jmbaVfK0TosAYpkQOQDpyn8LNeJVqY62Zzt+A=";
 
   ldflags = ["-s" "-w"];
+
+  # HACK
+  prePatch = ''
+    sed -i 's,go 1.21.5,go 1.21.4,' go.mod
+  '';
 
   meta = with lib; {
     description = "Catgpt` is a command-line tool that uses the OpenAI model to generate text based on user input";
