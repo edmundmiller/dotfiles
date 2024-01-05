978af7e3c47e6ab89092b66f9018fd3171c1f060
Edmund Miller <git@edmundmiller.dev>
2024-01-05 12:39:44 -0600

    refactor(packages): Use let for pname and version

--- packages/nf-core.nix
+++ packages/nf-core.nix
@@ -2,51 +2,53 @@
   lib,
   python3,
   fetchFromGitHub,
-}:
-python3.pkgs.buildPythonApplication rec {
+}: let
   pname = "nf-core";
-  version = "2.11.1";
-  pyproject = true;
+  version = "2.10";
+in
+  python3.pkgs.buildPythonApplication rec {
+    inherit pname version;
+    pyproject = true;
 
-  src = fetchFromGitHub {
-    owner = "nf-core";
-    repo = "tools";
-    rev = version;
-    hash = "sha256-1yk8PWOUoz1f2sGsPTbgKHn40tjoSgrTx/GgoQzlveo=";
-  };
+    src = fetchFromGitHub {
+      owner = "nf-core";
+      repo = "tools";
+      rev = version;
+      hash = "sha256-1yk8PWOUoz1f2sGsPTbgKHn40tjoSgrTx/GgoQzlveo=";
+    };
 
-  nativeBuildInputs = [
-    python3.pkgs.setuptools
-    python3.pkgs.wheel
-  ];
+    nativeBuildInputs = [
+      python3.pkgs.setuptools
+      python3.pkgs.wheel
+    ];
 
-  propagatedBuildInputs = with python3.pkgs; [
-    click
-    filetype
-    gitpython
-    jinja2
-    jsonschema
-    markdown
-    packaging
-    prompt-toolkit
-    pytest
-    pyyaml
-    questionary
-    requests
-    requests-cache
-    rich
-    rich-click
-    tabulate
-  ];
+    propagatedBuildInputs = with python3.pkgs; [
+      click
+      filetype
+      gitpython
+      jinja2
+      jsonschema
+      markdown
+      packaging
+      prompt-toolkit
+      pytest
+      pyyaml
+      questionary
+      requests
+      requests-cache
+      rich
+      rich-click
+      tabulate
+    ];
 
-  pythonImportsCheck = ["nf_core"];
+    pythonImportsCheck = ["nf_core"];
 
-  meta = with lib; {
-    description = "Python package with helper tools for the nf-core community";
-    homepage = "https://github.com/nf-core/tools";
-    changelog = "https://github.com/nf-core/tools/blob/${src.rev}/CHANGELOG.md";
-    license = licenses.mit;
-    maintainers = with maintainers; [emiller88];
-    mainProgram = "tools";
-  };
-}
+    meta = with lib; {
+      description = "Python package with helper tools for the nf-core community";
+      homepage = "https://github.com/nf-core/tools";
+      changelog = "https://github.com/nf-core/tools/blob/${src.rev}/CHANGELOG.md";
+      license = licenses.mit;
+      maintainers = with maintainers; [emiller88];
+      mainProgram = "tools";
+    };
+  }
