407043fca7789c3b9b719f97b63fc902e290c05a
Edmund Miller <git@edmundmiller.dev>
2024-01-05 16:27:30 -0600

    refactor(packages): Use fetchpatch for atuin on zfs

--- packages/atuin.nix
+++ packages/atuin.nix
@@ -1,5 +1,14 @@
-{atuin}:
+{
+  atuin,
+  fetchpatch,
+}:
 atuin.overrideAttrs (_old: {
   # as cursed as doing mitigations=off in the kernel command line
-  patches = [./0001-make-atuin-on-zfs-fast-again.patch];
+  patches = [
+    (fetchpatch
+      {
+        url = "https://github.com/Mic92/dotfiles/raw/main/home-manager/pkgs/atuin/0001-make-atuin-on-zfs-fast-again.patch";
+        hash = "sha256-2hN3n9d3ClgIcccb3mxPElOjJ9OrvRAhKitTl2lVgMM=";
+      })
+  ];
 })
