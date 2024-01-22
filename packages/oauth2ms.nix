d83a8b97b5bbd0afdd50c9250df3830e97b7b86d
Edmund Miller <git@edmundmiller.dev>
2024-01-22 17:43:35 -0600

    feat(packages): Add a stab at oauth2ms

---
+++ packages/oauth2ms.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  python3,
+  fetchFromGitHub,
+}:
+python3.pkgs.buildPythonApplication rec {
+  pname = "oauth2ms";
+  version = "unstable-2021-07-10";
+
+  src = fetchFromGitHub {
+    owner = "harishkrupo";
+    repo = "oauth2ms";
+    rev = "a1ef0cabfdea57e9309095954b90134604e21c08";
+    hash = "sha256-xPSWlHJAXhhj5I6UMjUtH1EZqCZWHJMFWTu3a4k1ETc=";
+  };
+
+  nativeBuildInputs = [
+    python3.pkgs.importlib-metadata
+    python3.pkgs.setuptools
+    python3.pkgs.wheel
+  ];
+
+  propagatedBuildInputs = with python3.pkgs; [
+    pyxdg
+    msal
+    python-gnupg
+  ];
+
+  meta = with lib; {
+    description = "";
+    homepage = "https://github.com/harishkrupo/oauth2ms";
+    license = licenses.asl20;
+    maintainers = with maintainers; [];
+    mainProgram = "oauth2ms";
+    platforms = platforms.all;
+  };
+}
