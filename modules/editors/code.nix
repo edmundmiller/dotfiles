5e0a0f320382f22ee871fe4db03775e5ce17cd11
Edmund Miller <git@edmundmiller.dev>
2024-01-29 08:59:35 -0600

    feat(editors): Try to get code working in a nix way

--- modules/editors/code.nix
+++ modules/editors/code.nix
@@ -12,14 +12,24 @@ in {
   options.modules.editors.code = {enable = mkBoolOpt false;};
 
   config = mkIf cfg.enable {
-    home-manager.users.emiller.programs.vscode.enable = true;
-    home-manager.users.emiller.programs.vscode.package =
-      pkgs.unstable.vscode-fhsWithPackages
-      (ps: with ps; [editorconfig-core-c hub neovim]);
-
+    home-manager.users.emiller.programs.vscode = {
+      enable = true;
+      extensions = with pkgs.vscode-extensions; [
+        ms-toolsai.jupyter
+        # nf-core.nf-core-extensionpack
+        github.copilot
+        github.copilot-chat
+        # gitpod.gitpod-desktop
+        eamodio.gitlens
+        bbenoist.nix
+        # reditorsupport.r
+      ];
+    };
     # For Liveshare
     services.gnome.gnome-keyring.enable = true;
     programs.seahorse.enable = true;
+    # FIXME if kde
+    programs.ssh.askPassword = lib.mkForce "${pkgs.libsForQt5.ksshaskpass}/libexec/ksshaskpass";
     programs.dconf.enable = true;
   };
 }
