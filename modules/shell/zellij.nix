a8f8a47f8dcd23e8ccedea9df360d74b713cc750
Edmund Miller <git@edmundmiller.dev>
2024-02-02 16:31:58 -0600

    feat(shell): Try out zellij

---
+++ modules/shell/zellij.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  options,
+  pkgs,
+  lib,
+  ...
+}:
+with lib;
+with lib.my; let
+  cfg = config.modules.shell.zellij;
+in {
+  options.modules.shell.zellij = with types; {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    home-manager.users.${config.user.name}.programs.zellij = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+  };
+}
