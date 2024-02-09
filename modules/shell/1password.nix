f24951ae45c1213b8fd67eabfea2e0104895856e
Edmund Miller <git@edmundmiller.dev>
2024-02-09 09:36:15 +0100

    feat: Add 1password

---
+++ modules/shell/1password.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  options,
+  pkgs,
+  lib,
+  ...
+}:
+with lib;
+with lib.my; let
+  cfg = config.modules.shell.bitwarden;
+in {
+  options.modules.shell."1password" = with types; {
+    enable = mkBoolOpt false;
+    config = mkOpt attrs {};
+  };
+
+  config = mkIf cfg.enable {
+    # user.packages = with pkgs; [
+    # ];
+
+    programs._1password.enable = true;
+    programs._1password-gui.enable = true;
+    programs._1password-gui.polkitPolicyOwners = [user.user];
+  };
+}
