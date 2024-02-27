d3b7b26595875df27017845138d273273fd61a08
Edmund Miller <git@edmundmiller.dev>
2024-02-26 19:51:05 -0600

    chore(packages): Bump gxf2bed

--- packages/gxf2bed.nix
+++ packages/gxf2bed.nix
@@ -5,16 +5,16 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "gxf2bed";
-  version = "0.1.0";
+  version = "0.2.0";
 
   src = fetchFromGitHub {
     owner = "alejandrogzi";
     repo = "gxf2bed";
     rev = "v${version}";
-    hash = "sha256-C88zPTUF9rHQ+susq6CPK16bGs6GAFH4QI/whkjuIKQ=";
+    hash = "sha256-7EFmaX1axQ574N3vg+hma4F832oHxlFf2bshEsNoDUM=";
   };
 
-  cargoHash = "sha256-zsbm1wZI199G++wzy0sagDh7jNZIYxa8HmzaFNej9Q4=";
+  cargoHash = "sha256-KPD5UTM9QFeC2rZ+oF2Mv5Ar7c5VlEZ/mwKiSoV1s+o=";
 
   meta = with lib; {
     description = "Fastest GTF/GFF-to-BED converter chilling around";
