7884c928db90e629d71094851c583d5b7543aadd
Edmund Miller <git@edmundmiller.dev>
2024-03-23 10:21:49 -0500

    chore: xkbOptions => xkb.options

--- modules/hardware/ergodox.nix
+++ modules/hardware/ergodox.nix
@@ -16,6 +16,6 @@ in {
     # 'teensyload FILE' to load a new config into the ergodox
     environment.shellAliases.teensyload = "sudo teensy-loader-cli -w -v --mcu=atmega32u4";
     # Make right-alt the compose key, so ralt+a+a = å or ralt+o+/ = ø
-    services.xserver.xkbOptions = "compose:ralt";
+    services.xserver.xkb.options = "compose:ralt";
   };
 }
