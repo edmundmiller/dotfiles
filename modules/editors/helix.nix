8388e428e836602f5d9a1006d26ca315b6692a68
Edmund Miller <git@edmundmiller.dev>
2024-04-01 15:54:38 -0500

    feat(editor): Add Helix file explorer with lf
    https://dpc.pw/posts/helix-file-picker-with-lf/

--- modules/editors/helix.nix
+++ modules/editors/helix.nix
@@ -40,6 +40,7 @@ in {
           space.w = ":w";
           space.q = ":q";
           esc = ["collapse_selection" "keep_primary_selection"];
+          C-f = [":new" ":insert-output lf -selection-path=/dev/stdout" "split_selection_on_newline" "goto_file" "goto_last_modification" "goto_last_modified_file" ":buffer-close!" ":redraw"];
         };
       };
     };
