a1ba4ac27bf48da4704fd86ca6011c26335ec933
Edmund Miller <git@edmundmiller.dev>
2024-04-09 15:35:58 -0500

    chore: emacs-unstable-pgtk => emacs-pgtk

--- modules/editors/emacs.nix
+++ modules/editors/emacs.nix
@@ -28,7 +28,7 @@ in {
     user.packages = with pkgs; [
       ## Emacs itself
       binutils # native-comp needs 'as', provided by this
-      ((emacsPackagesFor emacs-unstable-pgtk).emacsWithPackages
+      ((emacsPackagesFor emacs-pgtk).emacsWithPackages
         (epkgs:
           with epkgs; [
             vterm
