83228441104c83add23f3eb8a109c742cd08b2ee
Edmund Miller <git@edmundmiller.dev>
2024-06-28 09:02:37 -0500

    feat: Use nixvim

--- modules/editors/vim.nix
+++ modules/editors/vim.nix
@@ -11,16 +11,24 @@ with lib.my; let
   cfg = config.modules.editors.vim;
 in {
   options.modules.editors.vim = {enable = mkBoolOpt false;};
+  imports = [inputs.nixvim.nixosModules.nixvim];
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
       editorconfig-core-c
-      inputs.neovim-nightly-overlay.packages.${pkgs.system}.default
+      # inputs.neovim-nightly-overlay.packages.${pkgs.system}.default
       ctags
       unstable.lua-language-server
       lazygit
     ];
 
+    programs.nixvim = {
+      enable = true;
+      # vimdiffAlias = true;
+      clipboard.register = "unnamedplus";
+      vimAlias = true;
+      editorconfig.enable = true;
+    };
     # env.VIMINIT = "let \\$MYVIMRC='\\$XDG_CONFIG_HOME/nvim/init.vim' | source \\$MYVIMRC";
 
     environment.shellAliases = {
