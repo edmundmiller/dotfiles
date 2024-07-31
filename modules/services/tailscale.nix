e6833ff4a794f9964892f22b20e14a6e69a14d4e
Edmund Miller <git@edmundmiller.dev>
2024-07-31 15:38:49 -0500

    fix: Update tailscale MagicDNS

--- modules/services/tailscale.nix
+++ modules/services/tailscale.nix
@@ -17,7 +17,7 @@ in {
 
     # MagicDNS
     networking.nameservers = ["100.100.100.100" "8.8.8.8" "1.1.1.1"];
-    networking.search = ["tailff8ca.ts.net"];
+    networking.search = ["cinnamon-rooster.ts.net"];
 
     environment.shellAliases = {
       ts = "tailscale";
