f5994c89f5e701eabcb5a10f3b45d3ccd68914f9
Edmund Miller <git@edmundmiller.dev>
2024-08-09 09:39:31 -0500

    fix: Add desktop.gnome.dconf setting

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -16,8 +16,6 @@ in
   };
 
   config = mkIf cfg.enable {
-    imports = [ ./gnome/dconf ];
-
     services.xserver.enable = true;
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
