75839a56869e809a5eaf1bbb4a1caaddae4890a9
Edmund Miller <git@edmundmiller.dev>
2024-08-16 09:46:33 -0500

    feat: Add autoUpgrade flake to server config

--- hosts/server.nix
+++ hosts/server.nix
@@ -2,7 +2,12 @@
 #
 # Only to be used for headless servers, at home or abroad, with more
 # security/automation-minded configuration.
-{ lib, pkgs, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 {
   boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_6_1_hardened;
 
@@ -29,4 +34,11 @@
 
   # HACK https://github.com/danth/stylix/issues/200
   stylix.image = ../modules/themes/functional/config/wallpaper.png;
+
+  # https://discourse.nixos.org/t/deployment-tools-evaluating-nixops-deploy-rs-and-vanilla-nix-rebuild/36388/12?u=emiller88
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.dates = "*-*-* *:20:00";
+  system.autoUpgrade.flake = "github:edmundmiller/dotfiles#${config.networking.hostName}";
+  system.autoUpgrade.flags = [ "--refresh" ];
+  system.autoUpgrade.randomizedDelaySec = "5m";
 }
