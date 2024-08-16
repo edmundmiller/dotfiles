09c1c9073c4932774672525cfd074bc917590b17
Edmund Miller <git@edmundmiller.dev>
2024-08-16 10:57:59 -0500

    fix: Remove old secrets

--- hosts/meshify/secrets/secrets.nix
+++
@@ -1,6 +0,0 @@
-let
-  key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK3EVc3A55QHe83NXfqrClVohWz2DscDgx0pr4PSlcGO edmund.a.miller@protonmail.com";
-in
-{
-  "lastfm-password.age".publicKeys = [ key ];
-}
