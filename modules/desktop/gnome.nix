cbe21836d712047179737dc492d216bae675598d
Edmund Miller <git@edmundmiller.dev>
2024-08-16 20:19:32 -0500

    fix: It's a cursor issue

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -18,7 +18,7 @@ in
   config = mkIf cfg.enable {
     services.xserver.enable = true;
     services.xserver.displayManager.gdm.enable = true;
-    services.xserver.displayManager.gdm.wayland = false;
+    services.xserver.displayManager.gdm.wayland = true;
     services.xserver.desktopManager.gnome.enable = true;
     services.xserver.desktopManager.gnome.debug = true;
     environment.gnome.excludePackages =
