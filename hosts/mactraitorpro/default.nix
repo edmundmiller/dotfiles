5eb5adadfab3444c995149b8cb1372264f7a6d7a
Edmund Miller <git@edmundmiller.dev>
2025-01-09 17:37:09 -0600

    feat: Copy over seqeratop config

---
+++ hosts/mactraitorpro/default.nix
@@ -0,0 +1,73 @@
+{ config, ... }:
+{
+  config = {
+    modules = {
+      editors = {
+        default = "nvim";
+        emacs.enable = true;
+        vim.enable = true;
+      };
+      dev = {
+        python.enable = true;
+        python.conda.enable = true;
+        R.enable = true;
+      };
+
+      shell = {
+        "1password".enable = true;
+        ai.enable = true;
+        direnv.enable = true;
+        git.enable = true;
+        tmux.enable = true;
+        zsh.enable = true;
+      };
+
+      services = {
+        docker.enable = true;
+        ssh.enable = true;
+      };
+    };
+    # Use homebrew to install casks and Mac App Store apps
+    homebrew = {
+      enable = true;
+
+      casks = [
+        "1password"
+        "bartender"
+        "raycast"
+        "soundsource"
+        "ghostty"
+      ];
+
+    #   masApps = {
+    #     "Drafts" = 1435957248;
+    #     "Reeder" = 1529448980;
+    #     "Things" = 904280696;
+    #     "Timery" = 1425368544;
+    #   };
+    };
+
+    # set some OSX preferences that I always end up hunting down and changing.
+    system.defaults = {
+      # minimal dock
+      dock = {
+        autohide = true;
+        orientation = "left";
+        show-process-indicators = false;
+        show-recents = false;
+        static-only = true;
+      };
+      # a finder that tells me what I want to know and lets me work
+      finder = {
+        AppleShowAllExtensions = true;
+        ShowPathbar = true;
+        FXEnableExtensionChangeWarning = false;
+      };
+      # Tab between form controls and F-row that behaves as F1-F12
+      NSGlobalDomain = {
+        # AppleKeyboardUIMode = 3;
+        "com.apple.keyboard.fnState" = false;
+      };
+    };
+  };
+}
