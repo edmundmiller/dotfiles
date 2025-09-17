0d4d5a645ecc56f3418849c00b2e7046dae80240
Edmund Miller <git@edmundmiller.dev>
2025-09-16 22:17:48 -0500

    refactor: Move todotxtfmt from root to bin/ directory
    Move the todotxtfmt Go project from the repository root into the bin/ directory for better organization.
    
    refactor: Move todotxtfmt from root to bin/ directory
    
    Move the todotxtfmt Go project from the repository root into the bin/ directory for better organization.

--- config/todotxt/worktrees/issue-gh-macos/test_simple.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-function test_simple() {
-  assert_equals "hello" "hello"
-  assert_contains "hello world" "world"  
-  assert_not_equals "foo" "bar"
-}
--- config/todotxt/worktrees/review/tests/example_test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-function test_bashunit_is_installed() {
-  assert_same "bashunit is installed" "bashunit is installed"
-}
