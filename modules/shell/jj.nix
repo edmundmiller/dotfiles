12e087c89bf0f75efdcc42b96b0161b49fa611c3
Edmund Miller <git@edmundmiller.dev>
2025-11-06 16:40:57 -0600

    fix(jj): Add neovim jj resolve setup

---
+++ modules/shell/jj.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  options,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.my;
+let
+  cfg = config.modules.shell.jj;
+in
+{
+  options.modules.shell.jj = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [
+      jujutsu
+    ];
+
+    # Use home-manager's xdg.configFile directly for proper activation
+    home-manager.users.${config.user.name} = {
+      xdg.configFile = {
+        "jj/config.toml".source = "${configDir}/jj/config.toml";
+      };
+    };
+
+    modules.shell.zsh.rcFiles = [ "${configDir}/jj/aliases.zsh" ];
+  };
+}
