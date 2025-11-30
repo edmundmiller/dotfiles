16f7876139c9b2f7efa3396c77b079e5ded690ec
Edmund Miller <git@edmundmiller.dev>
2025-11-30 16:19:08 -0600

    feat(nvim): Add CodeCompanion plugin configuration

---
+++ config/nvim/lua/plugins/CodeCompanion.lua
@@ -0,0 +1,10 @@
+-- NOTE $ bun install -g @zed-industries/claude-code-acp
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {},
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+}
