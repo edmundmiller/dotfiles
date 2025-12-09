5304b5f4bb3e40e86cae20a207db1f8004d60cd8
Edmund Miller <git@edmundmiller.dev>
2025-12-09 13:12:04 -0600

    fix(taskwarrior): use tasksh package for shell alias
    task shell was removed in Taskwarrior 3.x and moved to separate tasksh package.
    
    Ref: https://news.ycombinator.com/item?id=3482583

--- config/taskwarrior/aliases.zsh
+++ config/taskwarrior/aliases.zsh
@@ -1,7 +1,9 @@
 # Taskwarrior aliases for zsh
 
 # Task shell with readline support
-alias ts='rlwrap -i -r -C task task shell'
+# From: https://news.ycombinator.com/item?id=3482583
+# Note: `task shell` was removed in Taskwarrior 3.x and moved to separate `tasksh` package
+alias tsh='rlwrap -i -r -C tasksh tasksh'
 
 # Taskwarrior TUI
 alias tt="taskwarrior-tui"
