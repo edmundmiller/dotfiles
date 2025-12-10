0a47f356c440e1d9d6524d3c46186ae25a192381
Edmund Miller <git@edmundmiller.dev>
2025-12-09 19:57:46 -0600

    feat(theme): switch to Catppuccin Mocha across all tools
    - Ghostty: Change from Flexoki to Catppuccin Latte/Mocha
    - Taskwarrior: Replace broken RGB colors with correct 256-color values
      - Fix intense blue background bleeding issue
      - Add comprehensive color definitions for all UI elements
    - Neovim: Enable astroui.lua and set colorscheme to catppuccin-mocha
    - OpenCode: Set theme to catppuccin
    
    Catppuccin Mocha palette reference:
      - Red (#f38ba8) -> color211
      - Peach (#fab387) -> color216
      - Yellow (#f9e2af) -> color223
      - Green (#a6e3a1) -> color151
      - Blue (#89b4fa) -> color111

--- config/nvim/lua/plugins/astroui.lua
+++ config/nvim/lua/plugins/astroui.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroUI provides the basis for configuring the AstroNvim User Interface
 -- Configuration documentation can be found with `:h astroui`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -10,8 +8,8 @@ return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
-    -- change colorscheme
-    colorscheme = "astrodark",
+    -- change colorscheme - Catppuccin Mocha for consistency with Ghostty/Taskwarrior
+    colorscheme = "catppuccin-mocha",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
