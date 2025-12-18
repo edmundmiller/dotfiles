acc64b83ab8c1a082148c79cd396ab6df353c10c
Edmund Miller <git@edmundmiller.dev>
2025-12-18 17:38:22 -0600

    feat(ghostty): add prise shell integration with aliases and config

---
+++ modules/shell/prise.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  options,
+  pkgs,
+  lib,
+  ...
+}:
+with lib;
+with lib.my;
+let
+  cfg = config.modules.shell.prise;
+in
+{
+  options.modules.shell.prise = with types; {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    # Prise is installed via homebrew on macOS
+    # If you want to add it to nix packages, uncomment:
+    # user.packages = [ pkgs.prise ];
+
+    modules.shell.zsh = {
+      rcFiles = [ "${configDir}/prise/aliases.zsh" ];
+    };
+
+    home.configFile = {
+      "prise" = {
+        source = "${configDir}/prise";
+        recursive = true;
+      };
+    };
+
+    env = {
+      PRISE_CONFIG_DIR = "$XDG_CONFIG_HOME/prise";
+    };
+  };
+}
