191b7c26b14befc51c71190fe2cd6413641a5071
Edmund Miller <git@edmundmiller.dev>
2026-01-09 21:24:22 -0600

    fix(aerc): update to new module option structure

--- modules/desktop/apps/mail/aerc.nix
+++ modules/desktop/apps/mail/aerc.nix
@@ -15,40 +15,42 @@ in
   };
 
   config = mkIf cfg.enable {
-    home-manager.users.${config.user.name}.accounts.email = {
-      accounts = {
-        Gmail.aerc = {
-          enable = true;
-        };
+    home-manager.users.${config.user.name} = {
+      accounts.email = {
+        accounts = {
+          Gmail.aerc = {
+            enable = true;
+          };
 
-        fastmail.aerc = {
-          enable = true;
-        };
-        UTD.aerc = {
-          enable = false;
-          imapAuth = "oauthbearer";
-          # NOTE https://atlas.utdallas.edu/TDClient/30/Portal/KB/ArticleDet?ID=301
-          # imapOauth2Params = {
-          #   client_id = "8d281d1d-9c4d-4bf7-b16e-032d15de9f6c"; # Tenant
-          #   client_secret = "a9170526-040b-453c-aac5-36155cba7a26"; # Application ID
-          #   scope = "";
-          #   token_endpoint = "https://outlook.office365.com/EWS/Exchange.asmx";
-          # };
+          fastmail.aerc = {
+            enable = true;
+          };
+          UTD.aerc = {
+            enable = false;
+            imapAuth = "oauthbearer";
+            # NOTE https://atlas.utdallas.edu/TDClient/30/Portal/KB/ArticleDet?ID=301
+            # imapOauth2Params = {
+            #   client_id = "8d281d1d-9c4d-4bf7-b16e-032d15de9f6c"; # Tenant
+            #   client_secret = "a9170526-040b-453c-aac5-36155cba7a26"; # Application ID
+            #   scope = "";
+            #   token_endpoint = "https://outlook.office365.com/EWS/Exchange.asmx";
+            # };
+          };
         };
       };
-    };
 
-    home-manager.users.${config.user.name}.programs.aerc = {
-      enable = true;
-      extraConfig.general.unsafe-accounts-conf = true;
-      #   # FIXME Still doesn't have the right permissions
-      #   # "aerc/accounts.conf".source = <config/aerc/accounts.conf>;
-      #   extraConfig = "${configDir}/aerc/aerc.conf";
-      #   "aerc/binds.conf".source = "${configDir}/aerc/binds.conf";
-      #   "aerc/templates" = {
-      #     source = "${configDir}/aerc/templates";
-      #     recursive = true;
-      #   };
+      programs.aerc = {
+        enable = true;
+        extraConfig.general.unsafe-accounts-conf = true;
+        #   # FIXME Still doesn't have the right permissions
+        #   # "aerc/accounts.conf".source = <config/aerc/accounts.conf>;
+        #   extraConfig = "${configDir}/aerc/aerc.conf";
+        #   "aerc/binds.conf".source = "${configDir}/aerc/binds.conf";
+        #   "aerc/templates" = {
+        #     source = "${configDir}/aerc/templates";
+        #     recursive = true;
+        #   };
+      };
     };
   };
 }
