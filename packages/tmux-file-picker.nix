ba7887487d87d2835514d6ee46c26b60f1066a52
Edmund Miller <git@edmundmiller.dev>
2026-01-13 23:12:41 -0600

    feat(tmux): add tmux-file-picker popups

---
+++ packages/tmux-file-picker.nix
@@ -0,0 +1,48 @@
+{ lib
+, stdenvNoCC
+, fetchFromGitHub
+, makeWrapper
+, fzf
+, fd
+, bat
+, tree
+, zoxide
+, coreutils
+}:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "tmux-file-picker";
+  version = "unstable-2025-12-21";
+
+  src = fetchFromGitHub {
+    owner = "raine";
+    repo = "tmux-file-picker";
+    rev = "0473f7abe87b95bc008e1cbfd16578e9cee93565";
+    hash = "sha256-Usn0uzqmKijodq7Pll4DCs2HSvINoLVjARZoebQh/Wk=";
+  };
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp tmux-file-picker $out/bin/
+    chmod +x $out/bin/tmux-file-picker
+
+    wrapProgram $out/bin/tmux-file-picker \
+      --prefix PATH : ${lib.makeBinPath [
+        fzf
+        fd
+        bat
+        tree
+        zoxide
+        coreutils
+      ]}
+  '';
+
+  meta = with lib; {
+    description = "Tmux file picker for AI agents";
+    homepage = "https://github.com/raine/tmux-file-picker";
+    license = licenses.mit;
+    platforms = platforms.unix;
+  };
+}
