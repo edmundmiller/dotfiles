277d57e8c5032a0acea63d8b01e6ccc739225298
Edmund Miller <git@edmundmiller.dev>
2026-01-20 22:09:34 -0600

    Add ErgoDox firmware source (original from ZSA Oryx)

---
+++ zsa_ergodox_ez_m32u4_base_wagdn_qmvNyL_ergo-drifter-fork-fork_source/zsa_ergodox_ez_m32u4_base_ergo-drifter-fork-fork_source/config.h
@@ -0,0 +1,11 @@
+#undef DEBOUNCE
+#define DEBOUNCE 40
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 140
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define HOLD_ON_OTHER_KEY_PRESS
+#define SERIAL_NUMBER "wagdn/qmvNyL"
+#define LAYER_STATE_8BIT
+
