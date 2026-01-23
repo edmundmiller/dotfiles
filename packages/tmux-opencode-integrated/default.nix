a79dd8564b32111eab56c7bb672d9ec449eb7edc
Edmund Miller <git@edmundmiller.dev>
2026-01-23 16:59:14 -0600

    tmux: add nix check phase

--- packages/tmux-opencode-integrated/default.nix
+++ packages/tmux-opencode-integrated/default.nix
@@ -1,20 +1,32 @@
-{ lib, stdenv, makeWrapper, python3 }:
+{
+  lib,
+  stdenv,
+  python3,
+  makeWrapper,
+}:
 
 let
-  pythonEnv = python3.withPackages (ps: with ps; [ libtmux ]);
+  pythonEnv = python3.withPackages (ps: [ ps.libtmux ]);
+  pythonTestEnv = python3.withPackages (ps: [ ps.libtmux ps.pytest ]);
 in
 stdenv.mkDerivation {
   pname = "tmux-opencode-integrated";
   version = "0.1.0";
 
-  src = ./scripts;
+  src = ./.;
 
   nativeBuildInputs = [ makeWrapper ];
   buildInputs = [ pythonEnv ];
+  nativeCheckInputs = [ pythonTestEnv ];
+
+  doCheck = true;
+  checkPhase = ''
+    pytest -q tests
+  '';
 
   installPhase = ''
     mkdir -p $out/share/tmux-plugins/tmux-opencode-integrated/scripts
-    cp -r * $out/share/tmux-plugins/tmux-opencode-integrated/scripts/
+    cp -r scripts/* $out/share/tmux-plugins/tmux-opencode-integrated/scripts/
     
     chmod +x $out/share/tmux-plugins/tmux-opencode-integrated/scripts/smart-name.sh
     chmod +x $out/share/tmux-plugins/tmux-opencode-integrated/scripts/smart_name.py
