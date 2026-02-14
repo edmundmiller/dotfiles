23d77911113a5ecc62f4c1c63c2e5bc247d1f7de
Edmund Miller <git@edmundmiller.dev>
2026-02-14 15:42:09 -0600

    nvim: bind SPC SPC and SPC ff to fff.nvim

--- config/nvim/lua/plugins/fff.lua
+++ config/nvim/lua/plugins/fff.lua
@@ -1,6 +1,7 @@
 -- FFF.nvim - Fast fuzzy file finder
 -- https://github.com/dmtrKovalenko/fff.nvim
 -- Rust-backed fuzzy file picker with git status, frecency, typo-resistant search
+-- Keybindings: <Leader><Leader>, <Leader>ff (astrocore.lua)
 
 ---@type LazySpec
 return {
@@ -10,8 +11,4 @@ return {
   end,
   lazy = false,
   opts = {},
-  keys = {
-    { "<Leader>ff", function() require("fff").find_files() end, desc = "Find files (fff)" },
-    { "<Leader>fF", function() require("fff").find_in_git_root() end, desc = "Find files in git root" },
-  },
 }
