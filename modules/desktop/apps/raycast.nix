29b26a5df1def1d17b888f0a7c639c1237c28d06
Edmund Miller <git@edmundmiller.dev>
2026-02-14 16:37:25 -0600

    raycast: use optionalAttrs isDarwin to hide homebrew from NixOS

--- modules/desktop/apps/raycast.nix
+++ modules/desktop/apps/raycast.nix
@@ -35,89 +35,91 @@ in
     enable = mkBoolOpt false;
   };
 
-  config = mkIf (cfg.enable && isDarwin) {
-    # Install via homebrew cask (auto_updates, always current)
-    homebrew.casks = [ "raycast" ];
+  config = optionalAttrs isDarwin (
+    mkIf cfg.enable {
+      # Install via homebrew cask (auto_updates, always current)
+      homebrew.casks = [ "raycast" ];
 
-    # Disable Spotlight hotkey so Raycast can claim ⌘Space
-    system.defaults.CustomUserPreferences = {
-      "com.apple.symbolichotkeys" = {
-        AppleSymbolicHotKeys = {
-          "64" = {
-            enabled = false;
-            value = {
-              type = "standard";
-              parameters = [
-                32
-                49
-                1048576
-              ];
+      # Disable Spotlight hotkey so Raycast can claim ⌘Space
+      system.defaults.CustomUserPreferences = {
+        "com.apple.symbolichotkeys" = {
+          AppleSymbolicHotKeys = {
+            "64" = {
+              enabled = false;
+              value = {
+                type = "standard";
+                parameters = [
+                  32
+                  49
+                  1048576
+                ];
+              };
             };
           };
         };
+
+        # Raycast preferences
+        "com.raycast.macos" = {
+          raycastGlobalHotkey = "Command-49"; # ⌘Space
+          raycastShouldFollowSystemAppearance = true;
+          navigationCommandStyleIdentifierKey = "vim";
+          onboardingCompleted = true;
+          useHyperKeyIcon = true;
+          raycastPreferredWindowMode = "compact";
+          "raycastUI_preferredTextSize" = "large";
+        };
       };
 
-      # Raycast preferences
-      "com.raycast.macos" = {
-        raycastGlobalHotkey = "Command-49"; # ⌘Space
-        raycastShouldFollowSystemAppearance = true;
-        navigationCommandStyleIdentifierKey = "vim";
-        onboardingCompleted = true;
-        useHyperKeyIcon = true;
-        raycastPreferredWindowMode = "compact";
-        "raycastUI_preferredTextSize" = "large";
+      # Auto-launch Raycast at login
+      launchd.user.agents.raycast = {
+        command = ''"/Applications/Raycast.app/Contents/MacOS/Raycast"'';
+        serviceConfig.RunAtLoad = true;
       };
-    };
 
-    # Auto-launch Raycast at login
-    launchd.user.agents.raycast = {
-      command = ''"/Applications/Raycast.app/Contents/MacOS/Raycast"'';
-      serviceConfig.RunAtLoad = true;
-    };
+      # Symlink script commands
+      home-manager.users.${config.user.name} = {
+        home.file = {
+          # Raycast script commands
+          "Scripts/raycast/neovide-quake-pro.swift" = {
+            source = "${configDir}/raycast-scripts/neovide-quake-pro.swift";
+            executable = true;
+          };
+          "Scripts/raycast/open-daily-note.sh" = {
+            source = "${configDir}/raycast-scripts/open-daily-note.sh";
+            executable = true;
+          };
+          "Scripts/raycast/neovide-simple-toggle.swift" = {
+            source = "${config.dotfiles.binDir}/raycast-scripts/neovide-simple-toggle.swift";
+            executable = true;
+          };
 
-    # Symlink script commands
-    home-manager.users.${config.user.name} = {
-      home.file = {
-        # Raycast script commands
-        "Scripts/raycast/neovide-quake-pro.swift" = {
-          source = "${configDir}/raycast-scripts/neovide-quake-pro.swift";
-          executable = true;
-        };
-        "Scripts/raycast/open-daily-note.sh" = {
-          source = "${configDir}/raycast-scripts/open-daily-note.sh";
-          executable = true;
-        };
-        "Scripts/raycast/neovide-simple-toggle.swift" = {
-          source = "${config.dotfiles.binDir}/raycast-scripts/neovide-simple-toggle.swift";
-          executable = true;
-        };
-
-        # Todo.txt raycast scripts
-        "Scripts/raycast/todo/add-todotxt-task.sh" = {
-          source = "${configDir}/todo/raycast/add-todotxt-task.sh";
-          executable = true;
-        };
-        "Scripts/raycast/todo/do-todotxt-task.sh" = {
-          source = "${configDir}/todo/raycast/do-todotxt-task.sh";
-          executable = true;
-        };
-        "Scripts/raycast/todo/list-todotxt-projects.sh" = {
-          source = "${configDir}/todo/raycast/list-todotxt-projects.sh";
-          executable = true;
-        };
-        "Scripts/raycast/todo/list-todotxt-tasks.sh" = {
-          source = "${configDir}/todo/raycast/list-todotxt-tasks.sh";
-          executable = true;
-        };
-        "Scripts/raycast/todo/list-todotxt-tasks-context.sh" = {
-          source = "${configDir}/todo/raycast/list-todotxt-tasks-context.sh";
-          executable = true;
-        };
-        "Scripts/raycast/todo/_lib_todo_raycast.sh" = {
-          source = "${configDir}/todo/raycast/_lib_todo_raycast.sh";
-          executable = true;
+          # Todo.txt raycast scripts
+          "Scripts/raycast/todo/add-todotxt-task.sh" = {
+            source = "${configDir}/todo/raycast/add-todotxt-task.sh";
+            executable = true;
+          };
+          "Scripts/raycast/todo/do-todotxt-task.sh" = {
+            source = "${configDir}/todo/raycast/do-todotxt-task.sh";
+            executable = true;
+          };
+          "Scripts/raycast/todo/list-todotxt-projects.sh" = {
+            source = "${configDir}/todo/raycast/list-todotxt-projects.sh";
+            executable = true;
+          };
+          "Scripts/raycast/todo/list-todotxt-tasks.sh" = {
+            source = "${configDir}/todo/raycast/list-todotxt-tasks.sh";
+            executable = true;
+          };
+          "Scripts/raycast/todo/list-todotxt-tasks-context.sh" = {
+            source = "${configDir}/todo/raycast/list-todotxt-tasks-context.sh";
+            executable = true;
+          };
+          "Scripts/raycast/todo/_lib_todo_raycast.sh" = {
+            source = "${configDir}/todo/raycast/_lib_todo_raycast.sh";
+            executable = true;
+          };
         };
       };
-    };
-  };
+    }
+  );
 }
