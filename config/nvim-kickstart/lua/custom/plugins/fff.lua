15a1fbe92cbad12578e9734877301e75f40f7bef
Edmund Miller <git@edmundmiller.dev>
2026-02-14 17:36:37 -0600

    nvim: bind SPC SPC to fff find in git root

--- config/nvim-kickstart/lua/custom/plugins/fff.lua
+++ config/nvim-kickstart/lua/custom/plugins/fff.lua
@@ -13,6 +13,7 @@ return {
     { '<Leader>ff', function() require('fff').find_files() end, desc = 'Find files (fff)' },
     { '<Leader>fF', function() require('fff').find_in_git_root() end, desc = 'Find files in git root' },
     { '<Leader>fd', function() require('fff').find_files_in_dir(vim.fn.input('Directory: ', '', 'dir')) end, desc = 'Find in directory' },
+    { '<Leader><Leader>', function() require('fff').find_in_git_root() end, desc = 'Find in git root' },
     { '<Leader>gf', function() require('fff').find_in_git_root() end, desc = 'Git files' },
   },
 }
