1daee3b5abca67472d40908dfb60301793fb3358
Edmund Miller <git@edmundmiller.dev>
2026-02-14 22:24:49 -0600

    nvim: add SPC qq to quit

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -8,6 +8,9 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Quit
+vim.keymap.set('n', '<leader>qq', '<cmd>qa<CR>', { desc = 'Quit all' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
