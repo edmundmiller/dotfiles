d640bed23ef82b5f465c01e4da03fa993ced8701
Edmund Miller <git@edmundmiller.dev>
2026-02-19 21:25:07 -0600

    feat(homepage): wire up dashboard with all NUC services
    Groups: Media (Jellyfin, Audiobookshelf), Downloads (*arr stack),
    Home (HA, Homebridge), Monitoring (Gatus).
    Widgets: CPU/memory/disk, datetime, search.
    Access: http://nuc.cinnamon-rooster.ts.net:8082

--- modules/services/homepage.nix
+++ modules/services/homepage.nix
@@ -8,6 +8,8 @@ with lib.my;
 let
   cfg = config.modules.services.homepage;
   homepagePort = 8082;
+  tailnet = "cinnamon-rooster.ts.net";
+  nucBase = "http://nuc.${tailnet}";
 in
 {
   options.modules.services.homepage = {
@@ -19,28 +21,118 @@ in
       enable = true;
       listenPort = homepagePort;
       openFirewall = true;
+
+      # Allow access from tailscale hostname
+      allowedHosts = "localhost:${toString homepagePort},nuc.${tailnet}:${toString homepagePort}";
+
+      settings = {
+        title = "NUC";
+        favicon = "https://nixos.org/favicon.png";
+        theme = "dark";
+        color = "slate";
+        headerStyle = "clean";
+        hideVersion = true;
+      };
+
       widgets = [
         {
-          audiobookshelf = {
-            url = "http://audiobookshelf.host.or.ip:port";
-            key = "audiobookshelflapikey";
+          resources = {
+            cpu = true;
+            memory = true;
+            disk = "/";
           };
         }
-        # TODO Bazaar
-        # TODO Calendar
-        # TODO Health checks
-        # TODO Home assistant
-        # TODO Jellyfin
-        # TODO Jellyseer
-        # TODO NextDNS
-        # TODO Paperless
-        # TODO Radarr
-        # TODO Scrutiny
-        # TODO Speedtest tracker
-        # TODO Tailscale
-        # TODO romm
-        # TODO sonarr
-        # TODO transmission
+        {
+          datetime = {
+            text_size = "xl";
+            format = {
+              timeStyle = "short";
+              dateStyle = "short";
+              hourCycle = "h23";
+            };
+          };
+        }
+        {
+          search = {
+            provider = "duckduckgo";
+            target = "_blank";
+          };
+        }
+      ];
+
+      services = [
+        {
+          "Media" = [
+            {
+              "Jellyfin" = {
+                href = "${nucBase}:8096";
+                description = "Media server";
+                icon = "jellyfin.svg";
+              };
+            }
+            {
+              "Audiobookshelf" = {
+                href = "${nucBase}:13378";
+                description = "Audiobooks & podcasts";
+                icon = "audiobookshelf.svg";
+              };
+            }
+          ];
+        }
+        {
+          "Downloads" = [
+            {
+              "Radarr" = {
+                href = "${nucBase}:7878";
+                description = "Movies";
+                icon = "radarr.svg";
+              };
+            }
+            {
+              "Sonarr" = {
+                href = "${nucBase}:8989";
+                description = "TV shows";
+                icon = "sonarr.svg";
+              };
+            }
+            {
+              "Prowlarr" = {
+                href = "${nucBase}:9696";
+                description = "Indexer manager";
+                icon = "prowlarr.svg";
+              };
+            }
+          ];
+        }
+        {
+          "Home" = [
+            {
+              "Home Assistant" = {
+                href = "https://homeassistant.${tailnet}";
+                description = "Home automation";
+                icon = "home-assistant.svg";
+              };
+            }
+            {
+              "Homebridge" = {
+                href = "https://homebridge.${tailnet}";
+                description = "HomeKit bridge";
+                icon = "homebridge.svg";
+              };
+            }
+          ];
+        }
+        {
+          "Monitoring" = [
+            {
+              "Gatus" = {
+                href = "https://gatus.${tailnet}";
+                description = "Status page";
+                icon = "gatus.svg";
+              };
+            }
+          ];
+        }
       ];
     };
 
