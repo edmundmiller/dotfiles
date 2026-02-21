ba48410c7a41c08bb8e7960a154a3618c63bcfb2
Edmund Miller <git@edmundmiller.dev>
2026-02-21 16:06:18 -0600

    feat(tmux/sesh): consistent symbols across all filter views

--- config/tmux/sesh-picker.sh
+++ config/tmux/sesh-picker.sh
@@ -12,9 +12,9 @@ SESSION=$($SCRIPT_DIR/sesh-all.sh | fzf-tmux -p 80%,70% \
   --header "^a all  ^t tmux  ^c configs  ^x zoxide  ^d kill" \
   --bind "tab:down,btab:up" \
   --bind "ctrl-a:change-prompt(> )+reload($SCRIPT_DIR/sesh-all.sh)" \
-  --bind "ctrl-t:change-prompt(tmux> )+reload($SESH list -t)" \
-  --bind "ctrl-c:change-prompt(configs> )+reload($SESH list -c)" \
-  --bind "ctrl-x:change-prompt(zoxide> )+reload($SCRIPT_DIR/zoxide-list.sh)" \
+  --bind "ctrl-t:change-prompt(tmux> )+reload($SESH list -t | awk '{printf \"\\033[35m▪\\033[0m %s\\n\", \$0}')" \
+  --bind "ctrl-c:change-prompt(configs> )+reload($SESH list -c | awk '{printf \"\\033[33m▫\\033[0m %s\\n\", \$0}')" \
+  --bind "ctrl-x:change-prompt(zoxide> )+reload($SCRIPT_DIR/zoxide-list.sh | awk '{printf \"\\033[34m›\\033[0m %s\\n\", \$0}')" \
   --bind "ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(> )+reload($SCRIPT_DIR/sesh-all.sh)")
 
 # Strip colored dot prefix (• ) added by sesh-all.sh before connecting
