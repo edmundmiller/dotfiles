f2be4a137dc83363f395ced2846e565180cf48e0
Edmund Miller <git@edmundmiller.dev>
2026-02-21 21:44:41 -0600

    feat(tmux/sesh): bigger symbols ■ □ ▸

--- config/tmux/sesh-picker.sh
+++ config/tmux/sesh-picker.sh
@@ -15,12 +15,12 @@ SESSION=$($SCRIPT_DIR/sesh-all.sh | fzf-tmux -p 80%,70% \
   --header "^a all  ^t tmux  ^c configs  ^x zoxide  ^d kill" \
   --bind "tab:down,btab:up" \
   --bind "ctrl-a:change-prompt(> )+reload($SCRIPT_DIR/sesh-all.sh)" \
-  --bind "ctrl-t:change-prompt(tmux> )+reload($SESH list -t | grep -vxF '$CUR_SESS' | awk '{printf \"\\033[35m▪\\033[0m %s\\n\", \$0}')" \
-  --bind "ctrl-c:change-prompt(configs> )+reload($SESH list -c | grep -vxF '$CUR_SESS' | awk '{printf \"\\033[33m▫\\033[0m %s\\n\", \$0}')" \
-  --bind "ctrl-x:change-prompt(zoxide> )+reload($SCRIPT_DIR/zoxide-list.sh | grep -vxF '$CUR_DIR_SHORT' | awk '{printf \"\\033[34m›\\033[0m %s\\n\", \$0}')" \
+  --bind "ctrl-t:change-prompt(tmux> )+reload($SESH list -t | grep -vxF '$CUR_SESS' | awk '{printf \"\\033[35m■\\033[0m %s\\n\", \$0}')" \
+  --bind "ctrl-c:change-prompt(configs> )+reload($SESH list -c | grep -vxF '$CUR_SESS' | awk '{printf \"\\033[33m□\\033[0m %s\\n\", \$0}')" \
+  --bind "ctrl-x:change-prompt(zoxide> )+reload($SCRIPT_DIR/zoxide-list.sh | grep -vxF '$CUR_DIR_SHORT' | awk '{printf \"\\033[34m▸\\033[0m %s\\n\", \$0}')" \
   --bind "ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(> )+reload($SCRIPT_DIR/sesh-all.sh)")
 
 # Strip ANSI escape codes, then the symbol prefix
-SESSION=$(printf '%s' "$SESSION" | sed 's/\x1b\[[0-9;]*m//g; s/^[▪▫›] //')
+SESSION=$(printf '%s' "$SESSION" | sed 's/\x1b\[[0-9;]*m//g; s/^[■□▸] //')
 [ -n "$SESSION" ] && $SESH connect "$SESSION"
 exit 0
