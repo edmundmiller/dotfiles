e1934b474b6b3e01a4cda8bf4ce60f32a9c9638c
Edmund Miller <git@edmundmiller.dev>
2026-02-21 21:39:02 -0600

    feat(tmux/sesh): rename notes=>obsidian, hide current session/dir

--- config/tmux/sesh-picker.sh
+++ config/tmux/sesh-picker.sh
@@ -3,6 +3,9 @@
 
 SESH=/opt/homebrew/bin/sesh
 SCRIPT_DIR=${TMUX_HOME:-/Users/emiller/.config/tmux}
+CUR_SESS=$(tmux display-message -p '#S' 2>/dev/null)
+CUR_DIR=$(tmux display-message -p '#{pane_current_path}' 2>/dev/null)
+CUR_DIR_SHORT=${CUR_DIR/#$HOME/\~}
 
 SESSION=$($SCRIPT_DIR/sesh-all.sh | fzf-tmux -p 80%,70% \
   --ansi \
@@ -12,9 +15,9 @@ SESSION=$($SCRIPT_DIR/sesh-all.sh | fzf-tmux -p 80%,70% \
   --header "^a all  ^t tmux  ^c configs  ^x zoxide  ^d kill" \
   --bind "tab:down,btab:up" \
   --bind "ctrl-a:change-prompt(> )+reload($SCRIPT_DIR/sesh-all.sh)" \
-  --bind "ctrl-t:change-prompt(tmux> )+reload($SESH list -t | awk '{printf \"\\033[35m▪\\033[0m %s\\n\", \$0}')" \
-  --bind "ctrl-c:change-prompt(configs> )+reload($SESH list -c | awk '{printf \"\\033[33m▫\\033[0m %s\\n\", \$0}')" \
-  --bind "ctrl-x:change-prompt(zoxide> )+reload($SCRIPT_DIR/zoxide-list.sh | awk '{printf \"\\033[34m›\\033[0m %s\\n\", \$0}')" \
+  --bind "ctrl-t:change-prompt(tmux> )+reload($SESH list -t | grep -vxF '$CUR_SESS' | awk '{printf \"\\033[35m▪\\033[0m %s\\n\", \$0}')" \
+  --bind "ctrl-c:change-prompt(configs> )+reload($SESH list -c | grep -vxF '$CUR_SESS' | awk '{printf \"\\033[33m▫\\033[0m %s\\n\", \$0}')" \
+  --bind "ctrl-x:change-prompt(zoxide> )+reload($SCRIPT_DIR/zoxide-list.sh | grep -vxF '$CUR_DIR_SHORT' | awk '{printf \"\\033[34m›\\033[0m %s\\n\", \$0}')" \
   --bind "ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(> )+reload($SCRIPT_DIR/sesh-all.sh)")
 
 # Strip ANSI escape codes, then the symbol prefix
