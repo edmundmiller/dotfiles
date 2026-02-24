0ca2364c31a96dc335566a38e93f18d056e009ac
Edmund Miller <git@edmundmiller.dev>
2026-02-24 11:50:31 -0600

    feat(nvim): restore tmux nav

---
+++ config/nvim/lua/custom/plugins/tmux-navigator.lua
@@ -0,0 +1,5 @@
+return {
+  'christoomey/vim-tmux-navigator',
+  lazy = false,
+}
+-- vim: ts=2 sts=2 sw=2 et
--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -25,14 +25,8 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- Split/pane navigation is handled by vim-tmux-navigator in
+-- `lua/custom/plugins/tmux-navigator.lua`.
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
