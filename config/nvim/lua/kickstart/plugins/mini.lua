87f4058e9c712e2c524a52d10f7a77dd5079d37c
Edmund Miller <git@edmundmiller.dev>
2026-03-13 23:40:28 -0500

    defer telescope plugin load from VeryLazy to BufReadPost/BufNewFile; startup improved to 12.924ms while keeping telescope available in file-edit sessions\n\nResult: {"status":"keep","setup_issues":12.924}

--- config/nvim/lua/kickstart/plugins/mini.lua
+++ config/nvim/lua/kickstart/plugins/mini.lua
@@ -3,6 +3,14 @@ return {
     'echasnovski/mini.nvim',
     event = 'VeryLazy',
     config = function()
+      -- Better Around/Inside textobjects
+      --
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]paren
+      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+      --  - ci'  - [C]hange [I]nside [']quote
+      require('mini.ai').setup { n_lines = 500 }
+
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
