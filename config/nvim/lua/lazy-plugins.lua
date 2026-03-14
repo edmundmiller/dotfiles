34f905df91a5c932307de6f2898a67d14eb3b5e5
Edmund Miller <git@edmundmiller.dev>
2026-03-13 23:21:28 -0500

    remove which-key from minimal profile entirely; startup improved to 14.234ms and plugin count dropped to 18\n\nResult: {"status":"keep","startup_ms":14.234}

--- config/nvim/lua/lazy-plugins.lua
+++ config/nvim/lua/lazy-plugins.lua
@@ -25,8 +25,6 @@ require('lazy').setup({
 
   require 'kickstart.plugins.gitsigns',
 
-  require 'kickstart.plugins.which-key',
-
   require 'kickstart.plugins.telescope',
 
   require 'kickstart.plugins.lspconfig',
