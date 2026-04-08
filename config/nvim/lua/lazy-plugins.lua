acdc20719d5e63947a41e7589669aebfea18a6ae
Edmund Miller <git@edmundmiller.dev>
2026-04-07 20:49:34 -0500

    nvim: add pi.nvim for in-editor pi coding agent workflow

--- config/nvim/lua/lazy-plugins.lua
+++ config/nvim/lua/lazy-plugins.lua
@@ -43,6 +43,9 @@ require('lazy').setup({
 
   require 'kickstart.plugins.treesitter',
 
+  -- pi.nvim - Neovim interface for the pi coding agent
+  { "alex35mil/pi.nvim", dependencies = { "HakonHarnes/img-clip.nvim" } },
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
