c948fcd49c6ab7241f063faa93f08e3ce673a631
Edmund Miller <git@edmundmiller.dev>
2026-04-24 22:52:11 -0500

    fix(node): stop exporting deprecated npm tmp config

--- modules/dev/node.nix
+++ modules/dev/node.nix
@@ -81,7 +81,8 @@ in
     {
       env.NPM_CONFIG_USERCONFIG = "$XDG_CONFIG_HOME/npm/config";
       env.NPM_CONFIG_CACHE = "$XDG_CACHE_HOME/npm";
-      env.NPM_CONFIG_TMP = "$XDG_RUNTIME_DIR/npm";
+      # npm 11 warns that `tmp` is no longer a supported config key. Let npm use
+      # the platform temp dir (TMPDIR/TMP/TEMP) instead of exporting NPM_CONFIG_TMP.
       env.NPM_CONFIG_PREFIX = "$XDG_CACHE_HOME/npm";
       env.NODE_REPL_HISTORY = "$XDG_CACHE_HOME/node/repl_history";
 
