4b365c03cca26770af3e2968a6867775dd43fd6e
Edmund Miller <git@edmundmiller.dev>
2026-05-03 23:37:31 -0500

    fix: use herdr release binary

--- packages/herdr/default.nix
+++ packages/herdr/default.nix
@@ -1,31 +1,52 @@
 {
   lib,
-  rustPlatform,
-  inputs,
-  pkg-config,
-  zig,
-  stdenv,
-  apple-sdk_15,
+  stdenvNoCC,
+  fetchurl,
 }:
 
-rustPlatform.buildRustPackage rec {
-  pname = "herdr";
+let
   version = "0.5.4";
 
-  src = inputs.herdr-repo;
+  assets = {
+    aarch64-darwin = {
+      name = "herdr-macos-aarch64";
+      hash = "sha256-2bVw0yEFEQU3t/iDH6gJD4l/WIMDPQTYuGLxHT6BPoc=";
+    };
+    x86_64-darwin = {
+      name = "herdr-macos-x86_64";
+      hash = "sha256-ofF4BqVz2uH2ci7lIEybLwOUn76XpibbyqU/pIEhEXo=";
+    };
+    aarch64-linux = {
+      name = "herdr-linux-aarch64";
+      hash = "sha256-PL2I6MQEQ3TqwY5UYw3RGHUTGLQlKTHU322uUqrzZPA=";
+    };
+    x86_64-linux = {
+      name = "herdr-linux-x86_64";
+      hash = "sha256-TAmxUY92ko7aMz/uaG6H8+d2O9La1nh6EtnpVuNoT4c=";
+    };
+  };
 
-  cargoHash = "sha256-Y9lXxeE1ochoCdQZDUZNlitwjCHtce2sdTvMZCcipkk=";
+  asset =
+    assets.${stdenvNoCC.hostPlatform.system}
+      or (throw "herdr: unsupported system ${stdenvNoCC.hostPlatform.system}");
+in
+stdenvNoCC.mkDerivation {
+  pname = "herdr";
+  inherit version;
 
-  nativeBuildInputs = [ pkg-config ];
+  src = fetchurl {
+    url = "https://github.com/ogulcancelik/herdr/releases/download/v${version}/${asset.name}";
+    inherit (asset) hash;
+  };
 
-  buildInputs = lib.optionals stdenv.isDarwin [ apple-sdk_15 ];
+  dontUnpack = true;
 
-  preBuild = ''
-    export PATH=${zig}/bin:${apple-sdk_15}/usr/bin:$PATH
-    export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
-    export DEVELOPER_DIR=${apple-sdk_15}/Platforms/MacOSX.platform/Developer
-    export SDKROOT=${apple-sdk_15}/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.5.sdk
-    export NIX_LDFLAGS="-L$SDKROOT/usr/lib ''${NIX_LDFLAGS:-}"
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm755 "$src" "$out/bin/herdr"
+
+    runHook postInstall
   '';
 
   meta = with lib; {
@@ -33,6 +54,7 @@ rustPlatform.buildRustPackage rec {
     homepage = "https://github.com/ogulcancelik/herdr";
     license = licenses.agpl3Plus;
     mainProgram = "herdr";
-    platforms = platforms.unix;
+    platforms = attrNames assets;
+    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
   };
 }
