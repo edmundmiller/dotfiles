d0846518a224fc9de13b5d36e3e8226ecc239856
Edmund Miller <git@edmundmiller.dev>
2026-06-22 22:14:58 -0500

    fix(hass): restore adaptive wall lamp color

--- modules/services/hass/_domains/ambient.nix
+++ modules/services/hass/_domains/ambient.nix
@@ -367,6 +367,24 @@ in
             action = "scene.turn_on";
             target.entity_id = "scene.arrival_wall_lamp_previous";
           }
+          {
+            action = "adaptive_lighting.set_manual_control";
+            data = {
+              entity_id = "switch.adaptive_lighting_living_space";
+              lights = [ "light.essentials_a19_a60_5" ];
+              manual_control = false;
+            };
+          }
+          {
+            action = "adaptive_lighting.apply";
+            data = {
+              entity_id = "switch.adaptive_lighting_living_space";
+              lights = [ "light.essentials_a19_a60_5" ];
+              adapt_color = true;
+              adapt_brightness = true;
+              turn_on_lights = false;
+            };
+          }
         ];
       }
       {
