f6b9fb238b96a61cf47364d007a73e1e3a2824b9
Edmund Miller <git@edmundmiller.dev>
2026-07-01 23:39:56 -0500

    fix(agents): overlay omp 16.3.0

---
+++ overlays/omp/default.nix
@@ -0,0 +1,55 @@
+final: prev:
+let
+  version = "16.3.0";
+  assets = {
+    aarch64-darwin = {
+      name = "omp-darwin-arm64";
+      hash = "sha256-Ebwkjps6Ig+FnrR1ivhgmsZgZLb//GzqhicdzYHhguQ=";
+    };
+    x86_64-darwin = {
+      name = "omp-darwin-x64";
+      hash = "sha256-ObL0cZVotD+J5tfZ2jaJWuqcnrGtW5Ruhbbt3OCoo9E=";
+    };
+    aarch64-linux = {
+      name = "omp-linux-arm64";
+      hash = "sha256-gsYttvqIHWaG8JKeMolSA+/7la8i+BYSxovh3XiH9dQ=";
+    };
+    x86_64-linux = {
+      name = "omp-linux-x64";
+      hash = "sha256-oqI1QDJINVN4Nj33U7GnL0nQsnlMnhonrPUvTOKG20c=";
+    };
+  };
+  asset =
+    assets.${final.stdenv.hostPlatform.system}
+      or (throw "Unsupported OMP system: ${final.stdenv.hostPlatform.system}");
+  omp = final.stdenvNoCC.mkDerivation {
+    pname = "omp";
+    inherit version;
+    dontUnpack = true;
+    nativeBuildInputs = [ final.makeWrapper ];
+    src = final.fetchurl {
+      url = "https://github.com/can1357/oh-my-pi/releases/download/v${version}/${asset.name}";
+      sha256 = asset.hash;
+    };
+    installPhase = ''
+      runHook preInstall
+
+      install -Dm755 "$src" "$out/lib/omp/omp"
+      makeWrapper "$out/lib/omp/omp" "$out/bin/omp" \
+        --set PI_SKIP_VERSION_CHECK 1
+
+      runHook postInstall
+    '';
+    meta = {
+      description = "Oh My Pi coding agent";
+      homepage = "https://omp.sh";
+      mainProgram = "omp";
+      platforms = builtins.attrNames assets;
+    };
+  };
+in
+{
+  llm-agents = (prev.llm-agents or { }) // {
+    inherit omp;
+  };
+}
