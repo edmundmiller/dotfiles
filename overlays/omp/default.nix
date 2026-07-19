6fe0867224008565cab9131d6cd10e9dd7b67c90
Edmund Miller <20095261+edmundmiller@users.noreply.github.com>
2026-07-19 15:41:38 -0500

    chore(omp): update to 17.0.5

--- overlays/omp/default.nix
+++ overlays/omp/default.nix
@@ -16,7 +16,7 @@ let
       cargoDeps = final.rustPlatform.fetchCargoVendor {
         name = "omp-${old.version}-cargo-vendor";
         inherit src;
-        hash = "sha256-Dchz5HANoP+rzeax9+Kd/pfivw0wzv492i0+0J2pO1Q=";
+        hash = "sha256-bnXUqCCaNyovdc+hA6qMrvHagCjamBrsxTqpEH/3dA8=";
       };
       postInstall =
         (old.postInstall or "")
