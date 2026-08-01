948d93634ce2cec4727dc6936f93f92a05fbfd4e
Edmund Miller <git@edmundmiller.dev>
2026-08-01 18:03:26 -0500

    fix(pi-packages): isolate test Git state

--- packages/pi-packages/tests/context-repo.test.ts
+++ packages/pi-packages/tests/context-repo.test.ts
@@ -41,6 +41,7 @@ function setupMemoryDir(): string {
 
   // git init + initial commit so the extension sees an existing repo
   execSync("git init", { cwd: memDir });
+  execSync("git config core.hooksPath /dev/null", { cwd: memDir });
   execSync("git add .", { cwd: memDir });
   execSync('git -c commit.gpgsign=false -c user.name="test" -c user.email="t@t" commit -m "init"', {
     cwd: memDir,
