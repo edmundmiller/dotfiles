3ea0e77ec66cfbb970253549b7662b05fec9a8b8
Edmund Miller <git@edmundmiller.dev>
2026-08-20 19:32:41 -0500

    fix(nuc): exclude unrelated zsh change

--- modules/shell/zsh/default.nix
+++ modules/shell/zsh/default.nix
@@ -63,14 +63,6 @@ in
       users.defaultUserShell = mkForce pkgs.zsh;
     })
 
-    # Darwin-only: macOS Sequoia/Tahoe updates restore stock /etc/zshrc and
-    # abort nix-darwin activation. This is the 2026-08-19 vendor file.
-    (optionalAttrs isDarwin {
-      environment.etc.zshrc.knownSha256Hashes = [
-        "e883d7097b48bd3b2bde8ee40013c7a145abd6bf675cea2a6233d81c748ef970"
-      ];
-    })
-
     {
 
       # Enable zsh at system level - this creates /etc/zshrc that loads nix-darwin environment
@@ -85,6 +77,12 @@ in
         promptInit = "";
 
       };
+
+      # macOS Sequoia/Tahoe updates restore stock /etc/zshrc and abort
+      # nix-darwin activation. This is the 2026-08-19 vendor file.
+      environment.etc.zshrc.knownSha256Hashes = [
+        "e883d7097b48bd3b2bde8ee40013c7a145abd6bf675cea2a6233d81c748ef970"
+      ];
 
       modules.shell.zsh = {
         rcFiles = mkBefore ([ (configDir + "/zsh/prompt.zsh") ] ++ autoRcFiles);
